5fcc5e0fb56ac7de72c2104d0a01ec59f7f8d425
evenstensberg <evenstensberg@gmail.com>
2016-12-18 20:58:42 +0100

    feat: new test suites
    Integrated new test suites and trying to get it work nicely with
    observable sequences.

--- tests/setup-test.js
+++ tests/setup-test.js
@@ -1,7 +1,27 @@
-'use strict';
+const inquirer = require('inquirer')
+const questions = require('../lib/observable-questions');
+
 /*
  * TODO: Write Tests
  */
-test('Runs a test suite', () => {
-	expect('webpack').toBe('webpack');
-});
+
+ jest.useRealTimers()
+
+ describe('Inquirer', function () {
+	 let stdin;
+	 beforeEach(function () {
+		 stdin = require('mock-stdin').stdin();
+	 });
+	 it('Should recieve input and handle answers', function () {
+		 process.nextTick(function mockResponse() {
+			 stdin.send('index.js\n');
+		 });
+		 process.nextTick(function mockResponse() {
+			 stdin.send('output.js\n');
+		 });
+		 inquirer.prompt(questions).ui.process.subscribe(function (response) {
+			 expect(response.entryLogic).toBe('index.js')
+			 expect(response.outputLogic).toBe('output.js')
+		 })
+	 });
+ });
