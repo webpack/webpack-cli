ff9813955ab8c55bc40e31736874e00c5c9d1bcc
Pavithra K <pksjce@gmail.com>
2016-12-20 19:51:46 +0530

    fix - Try to pass the build

--- tests/setup-test.js
+++ tests/setup-test.js
@@ -1,27 +1,29 @@
-const inquirer = require('inquirer')
+'use strict';
+
+const inquirer = require('inquirer');
 const questions = require('../lib/observable-questions');
 
 /*
  * TODO: Write Tests
  */
 
- jest.useRealTimers();
+jest.useRealTimers();
 
- describe('Inquirer', function () {
-	 let stdin;
-	 beforeEach(function () {
-		 stdin = require('mock-stdin').stdin();
-	 });
-	 it('Should recieve input and handle answers', function () {
-		 process.nextTick(function mockResponse() {
-			 stdin.send('index.js\n');
-		 });
-		 process.nextTick(function mockResponse() {
-			 stdin.send('output.js\n');
-		 });
-		 inquirer.prompt(questions).ui.process.subscribe(function (response) {
-			 expect(response.answer).toBe('index.js')
-			 expect(response.answer).toBe('output.js')
-		 })
-	 });
- });
+describe('Inquirer', function () {
+	let stdin;
+	beforeEach(function () {
+		stdin = require('mock-stdin').stdin();
+	});
+	it('Should recieve input and handle answers', function () {
+		process.nextTick(function mockResponse() {
+			stdin.send('index.js\n');
+		});
+		process.nextTick(function mockResponse() {
+			stdin.send('output.js\n');
+		});
+		inquirer.prompt(questions).ui.process.subscribe(function (response) {
+			expect(response.answer).toBe('index.js');
+			expect(response.answer).toBe('output.js');
+		});
+	});
+});
