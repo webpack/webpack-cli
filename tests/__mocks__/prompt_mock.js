330fae886124d10e4e2aa9903c93b4493d434867
Pavithra Kodmad <pksjce@gmail.com>
2016-12-25 23:12:00 +0530

    Feature/config migration[WIP] (#11)
    * Feat[wip] - Adds codemod for loader to rules migration
    
    * Feat[wip] - Cleanup the codemod
    
    * Fix - Adds own testUtil and branch of recast
    
    * Fix - yarn does not support github links
    
    * Fix - Remove failures
    
    * Feat - add support to move pre and postloaders
    
    * Fix - Adds back node 4 support
    
    * Adds babel jest as an explicit dep

--- tests/__mocks__/prompt_mock.js
+++ tests/__mocks__/prompt_mock.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const inquirer = jest.genMockFromModule('inquirer')
+const inquirer = jest.genMockFromModule('inquirer');
 
-function prompt(questions, spec) {
-	 return questions.subscribe(q => {
-			 expect(q.message).toContain('?')
-			 expect(q.name).toContain('Logic')
-			 expect(q.type).toBe('input')
-	 })
+function prompt(questions) {
+	return questions.subscribe(q => {
+		expect(q.message).toContain('?');
+		expect(q.name).toContain('Logic');
+		expect(q.type).toBe('input');
+	});
 }
 
 inquirer.prompt = prompt;
