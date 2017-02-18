7c1c7ddbaa7aef24b74095b981c70ade4e1014e7
Even Stensberg <evenstensberg@gmail.com>
2017-02-18 19:16:34 +0100

    tests: wip on unit tests

--- tests/__mocks__/inquirer/initialize.mock.js
+++ tests/__mocks__/inquirer/initialize.mock.js
@@ -5,9 +5,10 @@ const prompt = require('prompt.mock');
 const initialConfig = jest.genMockFromModule('../../../lib/utils/initial-config');
 const Rx = require('rx');
 const got = require('got');
+
 let mockInitialize = {};
 
-mockInitialize.exists = function(pkg) {
+mockInitialize.exists = function exists(pkg) {
 	const hostname = 'https://www.npmjs.org';
 	const pkgUrl = `${hostname}/package/${pkg}`;
 	return got(pkgUrl, {method: 'HEAD'})
@@ -17,24 +18,24 @@ mockInitialize.exists = function(pkg) {
 			.catch(() => {
 				return false;
 			});
-};
+}
 
-mockInitialize.validateAddon = async (addon) => mockInitialize.exists(addon);
+mockInitialize.validateAddon = async (addon) => mockInitialize.exists(addon)
 
 mockInitialize.validateAddons = async (addon) => {
 	return Object.keys(addon).forEach( (pkg) => {
 		return mockInitialize.exists(addon[pkg]);
 	});
-};
+}
 
-mockInitialize.init = function(pkg, answer) {
+mockInitialize.init = function init(pkg, answer) {
 	// In the regular module, this is automatically an empty array, becomes `pkg.length == 0`
 	// We're adding manually the answers here as an argument for testing
 	if(!pkg) {
 		return prompt(Rx.Observable.from(questions), initialConfig, answer);
 	}
 	else { /* noop for now, manually testing addons */ }
-};
+}
 
 
-module.exports = mockInitialize;
+module.exports = mockInitialize
--- tests/__tests__/inquirer/initialize.test.js
+++ tests/__tests__/inquirer/initialize.test.js
@@ -1,31 +1,45 @@
 /* eslint-disable */
 'use strict';
 
-const inquirer = require('initialize.mock'); // eslint-disable-line
+const inquirer = require('initialize.mock');
+
 describe('Inquirer', () => {
+	let inquirerFn;
+	afterEach(() => {
+  		inquirerFn = null;
+	})
 	it('Should check existence of a module', async () => {
+		inquirerFn = jest.fn(inquirer.exists);
 		let exists = await inquirer.exists(['webpack-addons-ylvis']);
 		expect(exists).toBe(true);
 	});
+
 	it('Should init a check existency of the module if it has any dependencies', async () => {
-		//FIXME: Mock is hacky, as we cannot make an async call using array.map and get the returned promise.
-		let exists = await inquirer.validateAddon(['webpack-addons-ylvis']);
-		expect(exists).toBe(true);
+		inquirerFn = jest.fn(inquirer.validateAddon);
+		let validateAddon = await inquirer.validateAddon(['webpack-addons-ylvis']);
+		expect(validateAddon).toBe(true);
 	});
+
 	it('Should validate multiple packages if supplied', async () => {
-		var	mockAddons = jest.fn(inquirer.validateAddons(['webpack-addons-ylvis']));
-		expect(mockAddons).toHaveBeenCalledTimes(2);
+		//FIXME: Mock is hacky, as we cannot make an async call using array.map and get the returned promise.
+		inquirerFn = jest.fn(inquirer.validateAddons)
+		let validateAddons = inquirerFn(['webpack-addons-ylvis', 'webpack-addons-noop']);
+		expect(inquirerFn).toHaveBeenCalled();
 	});
+
 	it('Should provide with basic options if no argument is supplied to init', async () => {
-		let inqprompt = await inquirer.init(null, {
+	 	inquirerFn = jest.fn(inquirer.init);
+
+		let init = await inquirerFn(null, {
 			entry: '1', output: '2'
 		});
-		var matchObj = {
+		let matchObj = {
 			entry: '1',
 			output: '2'
 		};
-		inqprompt.subscribe(function(x) {
-			expect(x).toMatchObject(matchObj);
+		init.subscribe(function(answers) {
+			expect(answers).toMatchObject(matchObj);
 		});
 	});
+
 });
