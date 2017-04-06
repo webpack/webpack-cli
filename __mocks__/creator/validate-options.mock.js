0adcddf755b1f5a9aca63b4fd759984901ddd025
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2017-04-06 11:57:23 +0200

    style: Fixed all styles accoring to the webpack's style

--- __mocks__/creator/validate-options.mock.js
+++ __mocks__/creator/validate-options.mock.js
@@ -6,11 +6,11 @@ function getPath(part) {
 }
 
 function validateOptions(opts) {
-	return Object.keys(opts).forEach( (location) => {
+	return Object.keys(opts).forEach((location) => {
 		let part = getPath(opts[location]);
 		try {
 			fs.readFileSync(part);
-		} catch (err) {
+		} catch(err) {
 			throw new Error('Did not find the file');
 		}
 	});
