445125fb7e85657c998780026b4db708bae1f236
Daniela Valero <danielavalero255@gmail.com>
2017-07-04 16:22:05 +0200

    style: fix lintin errors and coding style according to webpack settings

--- bin/webpack.js
+++ bin/webpack.js
@@ -3,189 +3,193 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-var path = require('path');
-var resolveCwd = require('resolve-cwd');
+"use strict";
 
-var localCLI = resolveCwd.silent('webpack-cli/bin/webpack');
+var path = require("path");
+var resolveCwd = require("resolve-cwd");
 
-if (localCLI && path.relative(localCLI, __filename) !== '') {
+var localCLI = resolveCwd.silent("webpack-cli/bin/webpack");
+
+if (localCLI && path.relative(localCLI, __filename) !== "") {
 	require(localCLI);
 } else {
 	try {
-		require('./webpack');
+		require("./webpack");
 	} catch (err) {
 		console.error(`\n  ${err.message}`);
 		process.exitCode = 1;
 	}
 }
 
-var yargs = require('yargs')
-	.usage('webpack ' + require('../package.json').version + '\n' +
-		'Usage: https://webpack.js.org/api/cli/\n' +
-		'Usage without config file: webpack <entry> [<entry>] <output>\n' +
-		'Usage with config file: webpack');
-require('./config-yargs')(yargs);
+var yargs = require("yargs").usage(
+	"webpack " +
+		require("../package.json").version +
+		"\n" +
+		"Usage: https://webpack.js.org/api/cli/\n" +
+		"Usage without config file: webpack <entry> [<entry>] <output>\n" +
+		"Usage with config file: webpack"
+);
+require("./config-yargs")(yargs);
 
-
-var DISPLAY_GROUP = 'Stats options:';
-var BASIC_GROUP = 'Basic options:';
+var DISPLAY_GROUP = "Stats options:";
+var BASIC_GROUP = "Basic options:";
 
 yargs.options({
-	'json': {
-		type: 'boolean',
-		alias: 'j',
-		describe: 'Prints the result as JSON.'
+	json: {
+		type: "boolean",
+		alias: "j",
+		describe: "Prints the result as JSON."
 	},
-	'progress': {
-		type: 'boolean',
-		describe: 'Print compilation progress in percentage',
+	progress: {
+		type: "boolean",
+		describe: "Print compilation progress in percentage",
 		group: BASIC_GROUP
 	},
-	'color': {
-		type: 'boolean',
-		alias: 'colors',
+	color: {
+		type: "boolean",
+		alias: "colors",
 		default: function supportsColor() {
-			return require('supports-color');
+			return require("supports-color");
 		},
 		group: DISPLAY_GROUP,
-		describe: 'Enables/Disables colors on the console'
+		describe: "Enables/Disables colors on the console"
 	},
-	'sort-modules-by': {
-		type: 'string',
+	"sort-modules-by": {
+		type: "string",
 		group: DISPLAY_GROUP,
-		describe: 'Sorts the modules list by property in module'
+		describe: "Sorts the modules list by property in module"
 	},
-	'sort-chunks-by': {
-		type: 'string',
+	"sort-chunks-by": {
+		type: "string",
 		group: DISPLAY_GROUP,
-		describe: 'Sorts the chunks list by property in chunk'
+		describe: "Sorts the chunks list by property in chunk"
 	},
-	'sort-assets-by': {
-		type: 'string',
+	"sort-assets-by": {
+		type: "string",
 		group: DISPLAY_GROUP,
-		describe: 'Sorts the assets list by property in asset'
+		describe: "Sorts the assets list by property in asset"
 	},
-	'hide-modules': {
-		type: 'boolean',
+	"hide-modules": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Hides info about modules'
+		describe: "Hides info about modules"
 	},
-	'display-exclude': {
-		type: 'string',
+	"display-exclude": {
+		type: "string",
 		group: DISPLAY_GROUP,
-		describe: 'Exclude modules in the output'
+		describe: "Exclude modules in the output"
 	},
-	'display-modules': {
-		type: 'boolean',
+	"display-modules": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display even excluded modules in the output'
+		describe: "Display even excluded modules in the output"
 	},
-	'display-max-modules': {
-		type: 'number',
+	"display-max-modules": {
+		type: "number",
 		group: DISPLAY_GROUP,
-		describe: 'Sets the maximum number of visible modules in output'
+		describe: "Sets the maximum number of visible modules in output"
 	},
-	'display-chunks': {
-		type: 'boolean',
+	"display-chunks": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display chunks in the output'
+		describe: "Display chunks in the output"
 	},
-	'display-entrypoints': {
-		type: 'boolean',
+	"display-entrypoints": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display entry points in the output'
+		describe: "Display entry points in the output"
 	},
-	'display-origins': {
-		type: 'boolean',
+	"display-origins": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display origins of chunks in the output'
+		describe: "Display origins of chunks in the output"
 	},
-	'display-cached': {
-		type: 'boolean',
+	"display-cached": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display also cached modules in the output'
+		describe: "Display also cached modules in the output"
 	},
-	'display-cached-assets': {
-		type: 'boolean',
+	"display-cached-assets": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display also cached assets in the output'
+		describe: "Display also cached assets in the output"
 	},
-	'display-reasons': {
-		type: 'boolean',
+	"display-reasons": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display reasons about module inclusion in the output'
+		describe: "Display reasons about module inclusion in the output"
 	},
-	'display-depth': {
-		type: 'boolean',
+	"display-depth": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display distance from entry point for each module'
+		describe: "Display distance from entry point for each module"
 	},
-	'display-used-exports': {
-		type: 'boolean',
+	"display-used-exports": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display information about used exports in modules (Tree Shaking)'
+		describe: "Display information about used exports in modules (Tree Shaking)"
 	},
-	'display-provided-exports': {
-		type: 'boolean',
+	"display-provided-exports": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display information about exports provided from modules'
+		describe: "Display information about exports provided from modules"
 	},
-	'display-error-details': {
-		type: 'boolean',
+	"display-error-details": {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Display details about errors'
+		describe: "Display details about errors"
 	},
-	'verbose': {
-		type: 'boolean',
+	verbose: {
+		type: "boolean",
 		group: DISPLAY_GROUP,
-		describe: 'Show more details'
+		describe: "Show more details"
 	}
 });
 
 var argv = yargs.argv;
 
-if(argv.verbose) {
-	argv['display-reasons'] = true;
-	argv['display-depth'] = true;
-	argv['display-entrypoints'] = true;
-	argv['display-used-exports'] = true;
-	argv['display-provided-exports'] = true;
-	argv['display-error-details'] = true;
-	argv['display-modules'] = true;
-	argv['display-cached'] = true;
-	argv['display-cached-assets'] = true;
+if (argv.verbose) {
+	argv["display-reasons"] = true;
+	argv["display-depth"] = true;
+	argv["display-entrypoints"] = true;
+	argv["display-used-exports"] = true;
+	argv["display-provided-exports"] = true;
+	argv["display-error-details"] = true;
+	argv["display-modules"] = true;
+	argv["display-cached"] = true;
+	argv["display-cached-assets"] = true;
 }
-if(argv._.includes('init')) {
+if (argv._.includes("init")) {
 	const initPkgs = argv._.length === 1 ? [] : [argv._.pop()];
 
-	return require('../lib/initialize')(initPkgs);
-} else if(argv._.includes('migrate')) {
+	return require("../lib/initialize")(initPkgs);
+} else if (argv._.includes("migrate")) {
 	const filePaths = argv._.length === 1 ? [] : [argv._.pop()];
 	if (!filePaths.length) {
-		throw new Error('Please specify a path to your webpack config');
+		throw new Error("Please specify a path to your webpack config");
 	}
 	const inputConfigPath = path.resolve(process.cwd(), filePaths[0]);
 
-	return require('../lib/migrate.js')(inputConfigPath, inputConfigPath);
+	return require("../lib/migrate.js")(inputConfigPath, inputConfigPath);
 } else {
-	var options = require('./convert-argv')(yargs, argv);
+	var options = require("./convert-argv")(yargs, argv);
 
 	return processOptions(options);
 }
 
 function ifArg(name, fn, init) {
-	if(Array.isArray(argv[name])) {
-		if(init) init();
+	if (Array.isArray(argv[name])) {
+		if (init) init();
 		argv[name].forEach(fn);
-	} else if(typeof argv[name] !== 'undefined') {
-		if(init) init();
+	} else if (typeof argv[name] !== "undefined") {
+		if (init) init();
 		fn(argv[name], -1);
 	}
 }
 //eslint-disable-next-line
 function processOptions(options) {
 	// process Promise
-	if(typeof options.then === 'function') {
+	if (typeof options.then === "function") {
 		options.then(processOptions).catch(function(err) {
 			console.error(err.stack || err);
 			process.exitCode = -1;
@@ -194,189 +198,194 @@ function processOptions(options) {
 	}
 
 	var firstOptions = [].concat(options)[0];
-	var statsPresetToOptions = require('webpack/lib/Stats.js').presetToOptions;
+	var statsPresetToOptions = require("webpack/lib/Stats.js").presetToOptions;
 
 	var outputOptions = options.stats;
-	if(typeof outputOptions === 'boolean' || typeof outputOptions === 'string') {
+	if (typeof outputOptions === "boolean" || typeof outputOptions === "string") {
 		outputOptions = statsPresetToOptions(outputOptions);
-	} else if(!outputOptions) {
+	} else if (!outputOptions) {
 		outputOptions = {};
 	}
 	outputOptions = Object.create(outputOptions);
-	if(Array.isArray(options) && !outputOptions.children) {
+	if (Array.isArray(options) && !outputOptions.children) {
 		outputOptions.children = options.map(o => o.stats);
 	}
-	if(typeof outputOptions.context === 'undefined')
+	if (typeof outputOptions.context === "undefined")
 		outputOptions.context = firstOptions.context;
 
-	ifArg('json', function(bool) {
-		if(bool)
-			outputOptions.json = bool;
+	ifArg("json", function(bool) {
+		if (bool) outputOptions.json = bool;
 	});
 
-	if(typeof outputOptions.colors === 'undefined')
-		outputOptions.colors = require('supports-color');
+	if (typeof outputOptions.colors === "undefined")
+		outputOptions.colors = require("supports-color");
 
-	ifArg('sort-modules-by', function(value) {
+	ifArg("sort-modules-by", function(value) {
 		outputOptions.modulesSort = value;
 	});
 
-	ifArg('sort-chunks-by', function(value) {
+	ifArg("sort-chunks-by", function(value) {
 		outputOptions.chunksSort = value;
 	});
 
-	ifArg('sort-assets-by', function(value) {
+	ifArg("sort-assets-by", function(value) {
 		outputOptions.assetsSort = value;
 	});
 
-	ifArg('display-exclude', function(value) {
+	ifArg("display-exclude", function(value) {
 		outputOptions.exclude = value;
 	});
 
-	if(!outputOptions.json) {
-		if(typeof outputOptions.cached === 'undefined')
+	if (!outputOptions.json) {
+		if (typeof outputOptions.cached === "undefined")
 			outputOptions.cached = false;
-		if(typeof outputOptions.cachedAssets === 'undefined')
+		if (typeof outputOptions.cachedAssets === "undefined")
 			outputOptions.cachedAssets = false;
 
-		ifArg('display-chunks', function(bool) {
+		ifArg("display-chunks", function(bool) {
 			outputOptions.modules = !bool;
 			outputOptions.chunks = bool;
 		});
 
-		ifArg('display-entrypoints', function(bool) {
+		ifArg("display-entrypoints", function(bool) {
 			outputOptions.entrypoints = bool;
 		});
 
-		ifArg('display-reasons', function(bool) {
+		ifArg("display-reasons", function(bool) {
 			outputOptions.reasons = bool;
 		});
 
-		ifArg('display-depth', function(bool) {
+		ifArg("display-depth", function(bool) {
 			outputOptions.depth = bool;
 		});
 
-		ifArg('display-used-exports', function(bool) {
+		ifArg("display-used-exports", function(bool) {
 			outputOptions.usedExports = bool;
 		});
 
-		ifArg('display-provided-exports', function(bool) {
+		ifArg("display-provided-exports", function(bool) {
 			outputOptions.providedExports = bool;
 		});
 
-		ifArg('display-error-details', function(bool) {
+		ifArg("display-error-details", function(bool) {
 			outputOptions.errorDetails = bool;
 		});
 
-		ifArg('display-origins', function(bool) {
+		ifArg("display-origins", function(bool) {
 			outputOptions.chunkOrigins = bool;
 		});
 
-		ifArg('display-max-modules', function(value) {
+		ifArg("display-max-modules", function(value) {
 			outputOptions.maxModules = value;
 		});
 
-		ifArg('display-cached', function(bool) {
-			if(bool)
-				outputOptions.cached = true;
+		ifArg("display-cached", function(bool) {
+			if (bool) outputOptions.cached = true;
 		});
 
-		ifArg('display-cached-assets', function(bool) {
-			if(bool)
-				outputOptions.cachedAssets = true;
+		ifArg("display-cached-assets", function(bool) {
+			if (bool) outputOptions.cachedAssets = true;
 		});
 
-		if(!outputOptions.exclude)
-			outputOptions.exclude = ['node_modules', 'bower_components', 'components'];
+		if (!outputOptions.exclude)
+			outputOptions.exclude = [
+				"node_modules",
+				"bower_components",
+				"components"
+			];
 
-		if(argv['display-modules']) {
+		if (argv["display-modules"]) {
 			outputOptions.maxModules = Infinity;
 			outputOptions.exclude = undefined;
 		}
 	} else {
-		if(typeof outputOptions.chunks === 'undefined')
+		if (typeof outputOptions.chunks === "undefined")
 			outputOptions.chunks = true;
-		if(typeof outputOptions.entrypoints === 'undefined')
+		if (typeof outputOptions.entrypoints === "undefined")
 			outputOptions.entrypoints = true;
-		if(typeof outputOptions.modules === 'undefined')
+		if (typeof outputOptions.modules === "undefined")
 			outputOptions.modules = true;
-		if(typeof outputOptions.chunkModules === 'undefined')
+		if (typeof outputOptions.chunkModules === "undefined")
 			outputOptions.chunkModules = true;
-		if(typeof outputOptions.reasons === 'undefined')
+		if (typeof outputOptions.reasons === "undefined")
 			outputOptions.reasons = true;
-		if(typeof outputOptions.cached === 'undefined')
+		if (typeof outputOptions.cached === "undefined")
 			outputOptions.cached = true;
-		if(typeof outputOptions.cachedAssets === 'undefined')
+		if (typeof outputOptions.cachedAssets === "undefined")
 			outputOptions.cachedAssets = true;
 	}
 
-	ifArg('hide-modules', function(bool) {
-		if(bool) {
+	ifArg("hide-modules", function(bool) {
+		if (bool) {
 			outputOptions.modules = false;
 			outputOptions.chunkModules = false;
 		}
 	});
 
-	var webpack = require('webpack/lib/webpack.js');
+	var webpack = require("webpack/lib/webpack.js");
 
 	Error.stackTraceLimit = 30;
 	var lastHash = null;
 	var compiler;
 	try {
 		compiler = webpack(options);
-	} catch(e) {
-		var WebpackOptionsValidationError = require('webpack/lib/WebpackOptionsValidationError');
-		if(e instanceof WebpackOptionsValidationError) {
-			if(argv.color)
-				console.error('\u001b[1m\u001b[31m' + e.message + '\u001b[39m\u001b[22m');
-			else
-				console.error(e.message);
+	} catch (e) {
+		var WebpackOptionsValidationError = require("webpack/lib/WebpackOptionsValidationError");
+		if (e instanceof WebpackOptionsValidationError) {
+			if (argv.color)
+				console.error(
+					"\u001b[1m\u001b[31m" + e.message + "\u001b[39m\u001b[22m"
+				);
+			else console.error(e.message);
 			process.exitCode = 1;
 		}
 		throw e;
 	}
 
-	if(argv.progress) {
-		var ProgressPlugin = require('webpack/lib/ProgressPlugin');
-		compiler.apply(new ProgressPlugin({
-			profile: argv.profile
-		}));
+	if (argv.progress) {
+		var ProgressPlugin = require("webpack/lib/ProgressPlugin");
+		compiler.apply(
+			new ProgressPlugin({
+				profile: argv.profile
+			})
+		);
 	}
 
 	function compilerCallback(err, stats) {
-		if(!options.watch || err) {
+		if (!options.watch || err) {
 			// Do not keep cache anymore
 			compiler.purgeInputFileSystem();
 		}
-		if(err) {
+		if (err) {
 			lastHash = null;
 			console.error(err.stack || err);
-			if(err.details) console.error(err.details);
+			if (err.details) console.error(err.details);
 			process.exitCode = 1;
 		}
-		if(outputOptions.json) {
-			process.stdout.write(JSON.stringify(stats.toJson(outputOptions), null, 2) + '\n');
-		} else if(stats.hash !== lastHash) {
+		if (outputOptions.json) {
+			process.stdout.write(
+				JSON.stringify(stats.toJson(outputOptions), null, 2) + "\n"
+			);
+		} else if (stats.hash !== lastHash) {
 			lastHash = stats.hash;
-			process.stdout.write(stats.toString(outputOptions) + '\n');
+			process.stdout.write(stats.toString(outputOptions) + "\n");
 		}
-		if(!options.watch && stats.hasErrors()) {
-			process.on('exit', function() {
+		if (!options.watch && stats.hasErrors()) {
+			process.on("exit", function() {
 				process.exitCode = 2;
 			});
 		}
 	}
-	if(firstOptions.watch || options.watch) {
-		var watchOptions = firstOptions.watchOptions || firstOptions.watch || options.watch || {};
-		if(watchOptions.stdin) {
-			process.stdin.on('end', function() {
+	if (firstOptions.watch || options.watch) {
+		var watchOptions =
+			firstOptions.watchOptions || firstOptions.watch || options.watch || {};
+		if (watchOptions.stdin) {
+			process.stdin.on("end", function() {
 				process.exitCode = 0;
 			});
 			process.stdin.resume();
 		}
 		compiler.watch(watchOptions, compilerCallback);
-		console.log('\nWebpack is watching the files…\n');
-	} else
-		compiler.run(compilerCallback);
-
+		console.log("\nWebpack is watching the files…\n");
+	} else compiler.run(compilerCallback);
 }
