c8633c0475cc1860f025933b43a8e32915bb7376
Even Stensberg <evenstensberg@gmail.com>
2017-10-05 22:57:26 +0200

    Add unit tests for CLI and add flow compilation (#186)
    * add unit tests for everything and flow compilation
    
    * add travis builds
    
    * add cli to signature & add ignore to trash code
    
    * port to jest
    
    * remove redundant cli command
    
    * Feature/generate loader (#183)
    
    * Add template files for loader yeoman generator
    
    * Create yeoman generator for a webpack loader project
    
    * Add tests for loader-generator
    
    * Add `mkdirp` dependency for loader-generator
    
    * Add function to create yeoman env and run loader-generator
    
    * Add `generate-loader` command to webpack-cli
    
    * Copy loader templates from proper directory
    
    * Add template files for plugin generator
    
    * Create yeoman generator for webpack plugins
    
    * Add function to create yeoman env and run plugin generator
    
    * Add cli command to generate plugin
    
    * Register generate- commands in yargs
    
    * Add template files for loader examples and tests
    
    * Copy loader test and example template files in generator
    
    * Add template files for plugin examples and tests
    
    * Copy plugin test and example template files in generator
    
    * Refactor generator file copying, switch .includes with .indexOf in CLI arg parsing
    
    * Change `indexOf('foo') > -1` to `indexOf('foo') >= 0`
    
    * Factor out generator copy utilities into separate module
    
    * Rewrite generators using a function that returns a customized generator class
    
    * Fix linting errors
    
    * Remove //eslint-disable lines from template files
    
    * add unit tests for everything and flow compilation
    
    * add travis builds
    
    * add cli to signature & add ignore to trash code
    
    * port to jest
    
    * remove redundant cli command
    
    * rebase against master

---
+++ test/binCases/config-name/found-one/stdin.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = function testAssertions(code, stdout, stderr) {
+	expect(code).toBe(0);
+	expect(stdout).toEqual(expect.anything());
+
+	expect(stdout[5]).toContain("./index2.js");
+	expect(stderr).toHaveLength(0);
+};
---
+++ test/binCases/watch/single-config-watch-opt/stdin.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = function testAssertions(stdout, stderr, done) {
+	expect(stdout).toEqual(expect.anything());
+	expect(stdout[0]).toContain("");
+	expect(stdout[1]).toContain("Webpack is watching the files…");
+
+	expect(stderr).toHaveLength(0);
+	done();
+};
