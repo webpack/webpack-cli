2e6774ec699ea660177cfa3fed417a14e30ee043
Even Stensberg <evenstensberg@gmail.com>
2017-12-13 14:02:43 +0100

    remove validate schema & some mocks

--- dist/utils/resolve-packages.spec.js
+++ dist/utils/resolve-packages.spec.js
@@ -1,6 +1,39 @@
 "use strict";
+const path = require("path");
 
-const getLoc = require("../../__mocks__/inquirer/resolve.mock");
+function mockPromise(value) {
+	const isValueAPromise = (value || {}).then;
+	const mockedPromise = {
+		then: function(callback) {
+			return mockPromise(callback(value));
+		}
+	};
+
+	return isValueAPromise ? value : mockedPromise;
+}
+function spawnChild(pkg) {
+	return pkg;
+}
+
+function getLoc(option) {
+	let packageModule = [];
+	option.filter(pkg => {
+		mockPromise(spawnChild(pkg)).then(() => {
+			try {
+				let loc = path.join("..", "..", "node_modules", pkg);
+				packageModule.push(loc);
+			} catch (err) {
+				throw new Error(
+					"Package wasn't validated correctly.." + "Submit an issue for",
+					pkg,
+					"if this persists"
+				);
+			}
+		});
+		return packageModule;
+	});
+	return packageModule;
+}
 
 describe("resolve-packages", () => {
 	let moduleLoc;
