3a49e168e48a32359dec8744fb7610135f31714f
Even Stensberg <evenstensberg@gmail.com>
2017-12-15 20:15:01 +0100

    add support for enum props too

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const webpack = require('webpack');
 const webpack = require('webpack');
+const webpack = require('webpack');
+const webpack = require('webpack');
 module.exports = {
 	externals: {
 		undefined: '',
 		undefined: 'a'
+	},
+
+	performance: {
+		0: ''
 	}
 };
