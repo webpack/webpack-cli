285846a4cb1f976edcdb36629cf247d8017ff956
Even Stensberg <evenstensberg@gmail.com>
2017-12-18 13:19:58 +0100

    add JSdoc & documentation

--- test/binCases/errors/issue-5576/stdin.js
+++ test/binCases/errors/issue-5576/stdin.js
@@ -5,9 +5,7 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	expect(stdout[0]).toContain("Hash: ");
 	expect(stdout[1]).toContain("Version: ");
 	expect(stdout[2]).toContain("Time: ");
-	expect(stdout[4]).toContain(
-		"bundle.js"
-	);
+	expect(stdout[4]).toContain("bundle.js");
 
 	expect(stderr).toHaveLength(0);
 };
