fee29ff5a80b48503a960f6a071638a23c756129
Even Stensberg <evenstensberg@gmail.com>
2017-12-19 17:40:49 +0100

    adds schema for plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,7 @@
-module.exports = {};
+const webpack = require('webpack');
+module.exports = {
+	plugins: [
+		new webpack.CommonJsPlugin(),
+		new webpack.DefinePlugin('/* Add your arguments here */')
+	]
+};
