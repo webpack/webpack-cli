4f21d49a6f0af6aec09636ea2df1f641bb9b273d
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-25 20:35:29 +0530

    test(env): improve complex test check

--- test/binCases/env/complex/stdin.js
+++ test/binCases/env/complex/stdin.js
@@ -8,6 +8,8 @@ module.exports = function testAssertions(code, stdout, stderr) {
 	expect(stdout[2]).toContain("Time: ");
 	expect(stdout[4]).toContain("Environment (--env): {");
 	expect(stdout[5]).toContain("\"prod\": [");
+	expect(stdout[6]).toContain("\"foo\",");
+	expect(stdout[7]).toContain("\"bar\"");
 	expect(stdout[8]).toContain("],");
 	expect(stdout[9]).toContain("\"baz\": true");
 	expect(stdout[10]).toContain("}");
