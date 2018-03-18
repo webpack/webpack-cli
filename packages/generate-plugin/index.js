ec6cc38c49b548aa007f5dea45ea790a9d13ed3d
Sendil Kumar <sendilkumarn@live.com>
2018-03-18 21:53:08 +0100

    chore(monorepo): move to lerna

---
+++ packages/generate-plugin/index.js
@@ -0,0 +1,17 @@
+const yeoman = require("yeoman-environment");
+const PluginGenerator = require("webpack-cli-generators/plugin-generator").PluginGenerator;
+
+/**
+ * Runs a yeoman generator to create a new webpack plugin project
+ * @returns {void}
+ */
+function pluginCreator() {
+	const env = yeoman.createEnv();
+	const generatorName = "webpack-plugin-generator";
+
+	env.registerStub(PluginGenerator, generatorName);
+
+	env.run(generatorName);
+}
+
+module.exports = pluginCreator;
