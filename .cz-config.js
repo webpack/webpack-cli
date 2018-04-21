e3d3a2df941ce4485909772fd511c1bf32447ec0
Emanuele Stoppa <my.burning@gmail.com>
2018-04-21 18:16:26 +0100

    fix(commit): rollback

--- .cz-config.js
+++ .cz-config.js
@@ -26,7 +26,6 @@ module.exports = {
 		{ value: "feat", name: "feat:		A new feature" },
 		{ value: "fix", name: "fix:		Bugs, typos, etc" },
 		{ value: "misc", name: "misc:		Other formats like tweaks and such" },
-		{ value: "tests", name: "tests:	Tests, jest, binTestCases, etc" },
-		{ value: "break", name: "break: changes that break the behaviour of the cli" }
+		{ value: "tests", name: "tests:	Tests, jest, binTestCases, etc" }
 	]
 };
