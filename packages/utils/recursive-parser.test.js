eb0d74d6a92b492ec6b7d6bc27095df70a2b9d50
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-05-22 00:42:21 +0530

    tests(add): resursive add ast

--- packages/utils/recursive-parser.test.js
+++ packages/utils/recursive-parser.test.js
@@ -25,3 +25,55 @@ defineTest(
 		man: "() => duper"
 	}
 );
+
+// Tests for _add_ command
+defineTest(
+	__dirname,
+	"entry",
+	"fixture-2",
+	{
+		objects: "are",
+		super: [
+			"yeah",
+			{
+				test: new RegExp(/\.(js|vue)$/),
+				loader: "'eslint-loader'",
+				enforce: "'pre'",
+				include: ["customObj", "'Stringy'"],
+				options: {
+					formatter: "'someOption'"
+				}
+			}
+		],
+		nice: "':)'",
+		man: "() => duper"
+	},
+	"add"
+);
+
+defineTest(
+	__dirname,
+	"mode",
+	"fixture-2",
+	"'development'",
+	"add"
+);
+
+defineTest(
+	__dirname,
+	"entry",
+	"fixture-2",
+	"'src/index.js'",
+	"add"
+);
+
+defineTest(
+	__dirname,
+	"entry",
+	"fixture-2",
+	{
+		a: "aaaa.js",
+		b: "bbbb.js"
+	},
+	"add"
+);
