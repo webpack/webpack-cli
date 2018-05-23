990b170539e31d0089dd7c9c50afa9ced857296e
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-05-23 23:37:28 +0530

    cli(generator): add remove generator

--- packages/generators/remove-generator.js
+++ packages/generators/remove-generator.js
@@ -1,3 +1,46 @@
 const Generator = require("yeoman-generator");
+const { List } = require("@webpack-cli/webpack-scaffold");
 
-module.exports = class RemoveGenerator extends Generator {};
+const PROP_TYPES = require("@webpack-cli/utils/prop-types");
+
+/**
+ *
+ * Generator for removing properties
+ * @class	RemoveGenerator
+ * @extends	Generator
+ * @returns	{Void} After execution, transforms are triggered
+ *
+ */
+
+module.exports = class RemoveGenerator extends Generator {
+	constructor(args, opts) {
+		super(args, opts);
+		this.configuration = {
+			config: {
+				webpackOptions: {},
+			}
+		};
+	}
+
+	prompting() {
+		const done = this.async();
+
+		return this.prompt([
+			List(
+				"actionType",
+				"What property do you want to remove?",
+				Array.from(PROP_TYPES.keys())
+			)
+		])
+			.then(actionTypeAnswer => {
+				this.configuration.config.webpackOptions[
+					actionTypeAnswer.actionType
+				] = null;
+				done();
+			});
+	}
+
+	writing() {
+		this.config.set("configuration", this.configuration);
+	}
+};
