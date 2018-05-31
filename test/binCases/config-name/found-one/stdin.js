3442865c7c9c7804356419477caa155000b66ebb
Even Stensberg <evenstensberg@gmail.com>
2018-05-31 18:53:14 +0200

    tests(binTestCases): refactor to make tests pass (#472)

--- test/binCases/config-name/found-one/stdin.js
+++ test/binCases/config-name/found-one/stdin.js
@@ -3,7 +3,6 @@
 module.exports = function testAssertions(code, stdout, stderr) {
 	expect(code).toBe(0);
 	expect(stdout).toEqual(expect.anything());
-
-	expect(stdout[7]).toContain("./index2.js");
+	expect(stdout[6]).toContain("./index2.js");
 	expect(stderr).toHaveLength(0);
 };
