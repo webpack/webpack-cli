295d1e2169dc0e01905a82f89a80a9fd4f56469c
Dhruvdutt Jadhav <dhruvdutt.jadhav@gmail.com>
2018-06-12 16:56:45 +0530

    cli(add): support for webpack config file (#501)
    * cli(add): add support for webpack config
    
    * cli(utils): update signature for config helper
    
    * chore(deps): add log-symbols
    
    * cli: update modify-config-helper usage

--- packages/add/index.js
+++ packages/add/index.js
@@ -1,16 +1,23 @@
 "use strict";
 
 const defaultGenerator = require("@webpack-cli/generators/add-generator");
-const modifyHelper = require("@webpack-cli/utils/modify-config-helper");
+const modifyConfigHelper = require("@webpack-cli/utils/modify-config-helper");
 
 /**
  * Is called and returns a scaffolding instance, adding properties
  *
- * @returns {Function} modifyHelper - A helper function that uses the action
+ * @returns {Function} modifyConfigHelper - A helper function that uses the action
  * 	we're given on a generator
  *
  */
 
-module.exports = function add() {
-	return modifyHelper("add", defaultGenerator);
+module.exports = function add(...args) {
+	const DEFAULT_WEBPACK_CONFIG_FILENAME = "webpack.config.js";
+
+	const filePaths = args.slice(3);
+	let configFile = DEFAULT_WEBPACK_CONFIG_FILENAME;
+	if (filePaths.length) {
+		configFile = filePaths[0];
+	}
+	return modifyConfigHelper("add", defaultGenerator, configFile);
 };
