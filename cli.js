470ee0fdf312f5a743be939ccd18c493c9d656c0
Emanuele Stoppa <my.burning@gmail.com>
2018-09-11 18:29:42 +0100

    Fixed eslint errors

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 const importLocal = require("import-local");
 
@@ -8,27 +8,23 @@ require("v8-compile-cache");
 
 // Prefer the local installation of webpack-cli
 if (importLocal(__filename)) {
-    return;
 }
 process.title = "webpack";
 
-const updateNotifier = require('update-notifier');
-const packageJson = require('./package.json');
+const updateNotifier = require("update-notifier");
+const packageJson = require("./package.json");
 
-updateNotifier({pkg: packageJson}).notify();
+updateNotifier({ pkg: packageJson }).notify();
 
-const semver = require('semver');
+const semver = require("semver");
 
 const version = packageJson.engines.node;
 
 if (!semver.satisfies(process.version, version)) {
-  const rawVersion = version.replace(/[^\d\.]*/, '');
-  console.log(
-      'webpack CLI requires at least Node v' + rawVersion + '. ' +
-      'You have ' + process.version + '.\n' +
-      'See https://webpack.js.org/ ' +
-      'for migration help and similar.');
-  process.exit(1);
+	const rawVersion = version.replace(/[^\d\.]*/, "");
+	console.log("webpack CLI requires at least Node v" + rawVersion + ". " + "You have " + process.version + ".\n" + "See https://webpack.js.org/ " + "for migration help and similar.");
+	process.exit(1);
 }
 
-require('./lib/run');
+const run = require("./lib/run"); // eslint-disable-line
+run();
