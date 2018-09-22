91cc499116d40be7bb0ccd64c8fedef0cccd8db9
Emanuele <my.burning@gmail.com>
2018-09-22 15:41:46 +0100

    chore(tests): added first ts test for info package (#584)

--- packages/info/index.ts
+++ packages/info/index.ts
@@ -5,14 +5,16 @@ import * as process from "process";
  * Prints debugging information for webpack issue reporting
  */
 
+export function information() {
+	return {
+		Binaries: ["Node", "Yarn", "npm"],
+		Browsers: ["Chrome", "Firefox", "Safari"],
+		System: ["OS", "CPU"],
+		npmGlobalPackages: ["webpack", "webpack-cli"],
+		npmPackages: "*webpack*",
+	};
+}
+
 export default async function info() {
-	process.stdout.write(
-		await envinfo.run({
-			Binaries: ["Node", "Yarn", "npm"],
-			Browsers: ["Chrome", "Firefox", "Safari"],
-			System: ["OS", "CPU"],
-			npmGlobalPackages: ["webpack", "webpack-cli"],
-			npmPackages: "*webpack*",
-		}),
-	);
+	process.stdout.write(await envinfo.run(information()));
 }
