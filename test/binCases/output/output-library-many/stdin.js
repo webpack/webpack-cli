d4cd76ab102895db0829e02570a9cff937a74eba
ev1stensberg <evenstensberg@gmail.com>
2018-09-29 18:20:50 +0200

    chore: revise long lines

--- test/binCases/output/output-library-many/stdin.js
+++ test/binCases/output/output-library-many/stdin.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
 
 module.exports = function testAssertions(code, stdout, stderr) {
 	expect(code).toBe(0);
 	expect(stdout).toEqual(expect.anything());
 	expect(stdout[7]).toMatch(/index\.js.*\{0\}/);
 	expect(stderr).toHaveLength(0);
-
-	const output = fs.readFileSync(path.join(__dirname, "../../../js/bin/output/output-library-many/main.js"), "utf-8");
+	const outputPath = resolve("test", "js", "bin", "output", "output-library-many", "main.js");
+	const output = readFileSync(outputPath, "utf-8");
 	expect(output).toContain("window.key1=window.key1||{},window.key1.key2=function");
 };
