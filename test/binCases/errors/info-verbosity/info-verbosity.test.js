e6c0366cd9fb4202f929f4ff1ea5fb9f88490860
Emanuele Stoppa <my.burning@gmail.com>
2018-10-09 20:16:21 +0100

    tests(refactoring): refactored errors

---
+++ test/binCases/errors/info-verbosity/info-verbosity.test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const run = require("../../../testUtils");
+
+test("info-verbosity", () => {
+	const { stderr } = run(__dirname, [
+		"--entry",
+		"./index.js",
+		"--config",
+		"./webpack.config.js",
+		"--output-filename",
+		"[name].js",
+		"--output-chunk-filename",
+		"[id].chunk.js",
+		"--info-verbosity",
+		"false"
+	]);
+
+	expect(stderr).toContain("Invalid values:");
+	expect(stderr).toContain("Argument: info-verbosity, Given: \"false\", Choices: \"none\", \"info\", \"verbose\"");
+	// snapshot not needed
+});
---
+++ test/binCases/errors/issue-5576/issue-5576.test.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const run = require("../../../testUtils");
+
+test("info-verbosity", () => {
+	const { stderr, stdout, code } = run(__dirname, ["a", "bundle.js", "--mode", "production"]);
+
+	expect(code).toBe(2);
+	expect(stdout).toContain("bundle.js");
+
+	expect(stderr).toHaveLength(0);
+});
