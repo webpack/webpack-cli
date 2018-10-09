c37da12a6f18fa8156c859548c7fe78632abd2ad
Emanuele Stoppa <my.burning@gmail.com>
2018-10-09 15:28:40 +0100

    tests(refactor): started moving to a better test suite case

--- test/binCases/config-name/not-found/stdin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = function testAssertions(code, stdout, stderr) {
-	expect(code).not.toBe(0);
-	expect(stdout).toHaveLength(0);
-	expect(stderr[0]).toContain("Configuration with name 'foo' was not found.");
-};
---
+++ test/testUtils.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const path = require("path");
+const { sync: spawnSync } = require("execa");
+
+const WEBPACK_PATH = path.resolve(__dirname, "../bin/cli.js");
+
+/**
+ * Description
+ *
+ * @param {*} testCase The path to folder that contains the webpack.config.js
+ * @param {*} args Array of arguments to pass to webpack
+ * @returns {Object} The webpack output
+ */
+function runWebpack(testCase, args) {
+	// const cwd = path.resolve(testCase);
+	const cwd = path.resolve(testCase);
+
+	const outputPath = path.resolve(__dirname, "js/bin", testCase);
+	const argsWithOutput = args.concat("--output-path", outputPath);
+
+	const result = spawnSync(WEBPACK_PATH, argsWithOutput, {
+		cwd,
+		reject: false
+	});
+
+	// TODO: STRIP OUT THE OUTPUT
+	result.stdout = removeTimeStrings(result.stdout);
+
+	return result;
+}
+
+function removeTimeStrings(stdout) {
+	if (stdout === "") {
+		return "";
+	}
+	return stdout
+		.split("\n")
+		.filter(line => !line.includes("Hash"))
+		.filter(line => !line.includes("Version"))
+		.filter(line => !line.includes("Time"))
+		.filter(line => !line.includes("Built at"))
+		.join("\n");
+}
+
+module.exports = runWebpack;
