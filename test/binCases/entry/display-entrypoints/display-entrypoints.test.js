43a09c49a3be60127e0e12c5b66d253d694af049
Emanuele Stoppa <my.burning@gmail.com>
2018-10-09 19:40:46 +0100

    tests(refactoring): refactored entry points tests

---
+++ test/binCases/entry/display-entrypoints/display-entrypoints.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const run = require("../../../testUtils");
+
+test("display-entrypoints", () => {
+	const { stdout, stderr } = run(__dirname, ["--display-entrypoints", "false"]);
+
+	expect(stdout).not.toContain("Entrypoint");
+	expect(stderr).toHaveLength(0);
+	expect(stdout).toMatchSnapshot();
+});
