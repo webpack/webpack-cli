7d8470df9100fb9d93d98450e43409455c769d50
Emanuele Stoppa <my.burning@gmail.com>
2018-10-09 20:04:13 +0100

    tests(refactoring): refactored entry point tests

---
+++ test/binCases/configFile/profile/profile.test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const run = require("../../../testUtils");
+
+test("profile", () => {
+	const { code, stdout, stderr } = run(__dirname, []);
+
+	expect(code).toBe(0);
+	expect(stdout).toEqual(expect.anything());
+	expect(stdout).toContain("factory:");
+	expect(stderr).toHaveLength(0);
+	// no snapshot here
+});
