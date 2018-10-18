35e8c3fa60320f802c7647711f63e2c6a1ea3fa7
Rishabh Chawla <rishabh31121999@gmail.com>
2018-10-18 10:22:47 +0530

    feat(gui): setup foundation for UI

---
+++ packages/ui/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public/build")));
+
+app.use("/", indexRouter);
+
+module.exports = app;
