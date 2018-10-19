68b94ed739955f83563646dfdeed5e16f8c1536a
Rishabh Chawla <rishabh31121999@gmail.com>
2018-10-19 12:00:16 +0530

    feat(ui): update files and travis

--- packages/ui/app.js
+++ packages/ui/app.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
