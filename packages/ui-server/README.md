72ec643b8b2fe9c8f77d3325ed693ff6390803a7
Rishabh Chawla <rishabh31121999@gmail.com>
2018-10-21 09:21:48 +0530

    feat(ui): update folder structure ⭐

---
+++ packages/ui-server/README.md
@@ -0,0 +1,16 @@
+# webpack-cli ui-server
+Express server for `webpack-cli` packages
+
+#### Package Structure
+1. `./app.js` : main entry for the package and server
+2. `./api/`: will contain endpoints for features like
+	- `init`
+	- `add`
+	- `remove`
+	- `migrate`
+3. `./routes/` : will contain logic for serving html files
+
+## Setup development
+1. For backend: run `npm install`
+
+Lets get coding 🙌
---
+++ packages/ui-server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+
+module.exports = function(staticFolder) {
+	const app = express();
+	app.use(logger("dev"));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	app.use(express.static(staticFolder));
+	app.use("/", indexRouter);
+
+	return app;
+};
