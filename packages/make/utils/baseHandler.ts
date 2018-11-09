7ae72424c2a736fd2cf334809447bf163390d7cb
Rishabh Chawla <rishabh31121999@gmail.com>
2018-11-09 20:24:26 +0530

    feat(make): update storeBase()

--- packages/make/utils/baseHandler.ts
+++ packages/make/utils/baseHandler.ts
@@ -1,31 +1,34 @@
 import * as fs from "fs";
+import * as dir from "node-dir";
 /**
  * @returns storeBase() , getDependenencyTree(), getChangedTree(), builder()
  *
  */
+
+const log = (text) => { process.stdout.write(text); };
 
 export function storeBase(userDirectory: fs.PathLike) {
 	/**
 	 * 	Stores base state at ./base/ from userDirectory
 	 * 	@returns void
 	 */
-	fs.readdir(userDirectory, (err, files) => {
+	dir.files(userDirectory.toString(),
+	(err, files) => {
 		if (err) {
-			process.stdout.write(`
-			[${err.name}]
-			${err.message}
-			`);
-			return;
+			log(`[make] Error: ${err}`);
+		} else {
+			files.forEach( (file) => {
+				file = file.slice( userDirectory.toString().length);
+				const baseDir: fs.PathLike = file.split("/").splice(-1, 1).join("/");
+				if (file.match(/.*(.js|.ts|.scss)/g)) {
+					if (!fs.existsSync(baseDir)) {
+						fs.mkdirSync(__dirname + "/base" + baseDir);
+					}
+					fs.copyFileSync(userDirectory + file, __dirname + "/base" + file);
+					log(`Stored: ${file}\n`);
+				}
+			});
 		}
-
-		files.forEach((file) => {
-			if (file.match(/.*(.js|.ts|.scss)/g)) {
-				process.stdout.write(`
-				[${err.name}]
-				${err.message}
-				`);
-			}
-		});
 	});
 	return;
 }
