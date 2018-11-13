7295fe880ddcb13ba7f92ce8ac7a3b16eddab7db
Hemal Patel <hemal.patel7735@outlook.com>
2018-11-13 16:33:37 +0530

    feat(tests): add extractHash support in test-infra (#683)
    <!-- Thanks for submitting a pull request! Please provide enough information so that others can review your pull request. -->
    
    **What kind of change does this PR introduce?**
    <!-- E.g. a bugfix, feature, refactoring, build related change, etc… -->
    This PR adds support for extracting hash after running test. [Jest](https://github.com/facebook/jest/blob/master/e2e/__tests__/test_name_pattern_skipped.test.js#L19) is already doing it. We are trying to mimic it here, for our needs.
    **Did you add tests for your changes?** Yes
    
    **If relevant, did you update the documentation?** NA
    
    **Summary**
    
    <!-- Explain the **motivation** for making this change. What existing problem does the pull request solve? -->
    <!-- Try to link to an open issue for more information. -->
    This PR tries to address the feature-request issue #672 
    Closes #672 
    
    **Does this PR introduce a breaking change?** No
    <!-- If this PR introduces a breaking change, please describe the impact and a migration path for existing applications. -->

--- test/testUtils.js
+++ test/testUtils.js
@@ -75,4 +75,81 @@ function removeTimeStrings(stdout) {
 		.filter(line => !line.includes("Built at"))
 		.join("\n");
 }
-module.exports = { run, runWatch };
+
+/**
+ * @typedef {Object} Config
+ * @property {string} name - name of config
+ * @property {string} hash - hash of config
+ *
+ * @typedef {Object} HashInfo
+ * @property {string} hash - global hash value
+ * @property {Array.<Config>} config
+ */
+
+/**
+ * Description
+ *
+ * @param {string} stdout stdout of generic webpack output
+ * @returns {HashInfo} - an object containing hash-info
+ * @throws Will throw an error if {@link stdout}
+ * 		- is empty
+ * 		- does not contain Hash
+ *      - does not contain Hash
+ * 		- if multiple configs then, count(hash) !== count(Child) + 1 (+1 is for global hash)
+ *
+ */
+function extractHash(stdout) {
+	if (stdout === "") {
+		throw new Error("stdout is empty");
+	}
+
+	let hashArray = stdout.match(/Hash.*/gm);
+
+	// If logs are full of errors and we don't find hash
+	if (!hashArray) {
+		throw new Error(
+			`could not find hash in the stdout
+			OUTPUT: ${stdout}`
+		);
+	}
+
+	hashArray = hashArray.map(hashLine => hashLine.replace(/Hash:(.*)/, "$1").trim());
+
+	const hashInfo = {
+		hash: hashArray[0],
+		config: []
+	};
+
+	// Multiple config were found
+	if (hashArray.length > 1) {
+		let childArray = stdout.match(/Child.*/gm);
+
+		if (!childArray) {
+			throw new Error(
+				`could not find config in the stdout
+				OUTPUT: ${stdout}`
+			);
+		}
+
+		childArray = childArray.map(childLine => childLine.replace(/Child(.*):/, "$1").trim());
+
+		// We don't need global hash anymore
+		// so we'll remove it to maintain 1:1 parity between config and hash
+		hashArray.shift();
+		if (hashArray.length !== childArray.length) {
+			throw new Error(`The stdout is corrupted. Hash count and config count do not match.
+			OUTPUT: ${stdout}`);
+		}
+		const configCount = childArray.length;
+		for (let configIdx = 0; configIdx < configCount; configIdx++) {
+			hashInfo["config"].push({
+				name: childArray[configIdx],
+				hash: hashArray[configIdx]
+			});
+		}
+	}
+
+	return hashInfo;
+}
+
+module.exports = { run, runWatch, extractHash };
