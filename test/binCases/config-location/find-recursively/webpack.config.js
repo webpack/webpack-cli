e941b49bbb1eed85d5a3fb26e1df0ee076e49586
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-18 18:02:35 +0100

    tests(config): add config-location tests (#689)

---
+++ test/binCases/config-location/find-recursively/webpack.config.js
@@ -0,0 +1,7 @@
+var path = require("path");
+
+module.exports = [
+	{
+		entry: path.resolve(__dirname, "./index2")
+	},
+];
---
+++ test/binCases/config-location/set-implicitly/webpack.config.js
@@ -0,0 +1,8 @@
+var path = require("path");
+
+module.exports = [
+	{
+		name: "foo",
+		entry: path.resolve(__dirname, "./index2")
+	},
+];
