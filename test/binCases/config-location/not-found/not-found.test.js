1f2b7e238e33f052c72d6e158aa2f4d0387b3d61
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-19 21:57:33 +0100

    tests(config): add negative test-cases to config-location (#689)

---
+++ test/binCases/config-location/not-found/not-found.test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { run } = require("../../../testUtils");
+
+test("not-found", () => {
+	const { code, stdout, stderr } = run(__dirname, [
+		"--output-filename",
+		"[name].js",
+		"--output-chunk-filename",
+		"[id].chunk.js",
+		"--target",
+		"async-node",
+		"--mode",
+		"production",
+	]);
+	expect(code).toBe(0);
+	expect(stdout).toEqual(expect.anything());
+	expect(stdout).toContain("./src/index.js");
+	expect(stderr).toHaveLength(0);
+});
