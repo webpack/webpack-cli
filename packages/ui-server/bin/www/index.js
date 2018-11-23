e913f38a6168ca495a1686f6ecb58b40fbb523f3
Laksh Singla <lakshsingla@gmail.com>
2018-11-24 00:26:41 +0530

    (refactor) Refactored the static folder path and added webpack logger

--- packages/ui-server/bin/www/index.js
+++ packages/ui-server/bin/www/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const weblog = require('webpack-log');
+
 const app = require('../../app.js');
 
+const log = weblog({ name: 'uis'});
 const port = process.env.PORT || 8000;
+const staticFolder = path.join('..', '..', 'static');
 
-console.log(port);
-
-app(path.join('..', '..', 'static')).listen(port, () => {
-	console.log(`server started on 127.0.0.1:${port}`);
+app(staticFolder).listen(port, () => {
+	log.info(`server started on 127.0.0.1:${port}`);
 });
