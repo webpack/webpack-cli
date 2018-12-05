b6c6484300859f3d5db04f955c808af9291acecb
Abenezer Abebe <abenezerabebe@gmail.com>
2018-12-05 19:19:35 +0100

    chore: align file in the same way as other terserPlugin test fixtures

--- packages/migrate/terserPlugin/__testfixtures__/terserPlugin-4.input.js
+++ packages/migrate/terserPlugin/__testfixtures__/terserPlugin-4.input.js
@@ -1,22 +1,22 @@
 module.exports = {
-	devtool: "eval",
-	entry: ["./src/index"],
+	devtool: 'eval',
+	entry: [
+		'./src/index'
+	],
 	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "index.js"
+		path: path.join(__dirname, 'dist'),
+		filename: 'index.js'
 	},
 	module: {
-		loaders: [
-			{
-				test: /.js$/,
-				loaders: ["babel"],
-				include: path.join(__dirname, "src")
-			}
-		]
+		loaders: [{
+			test: /.js$/,
+			loaders: ['babel'],
+			include: path.join(__dirname, 'src')
+		}]
 	},
 	resolve: {
-		root: path.resolve("/src"),
-		modules: ["node_modules"]
+		root: path.resolve('/src'),
+		modules: ['node_modules']
 	},
 	plugins: [
 		new webpack.optimize.TerserPlugin({
