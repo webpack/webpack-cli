0817c5085884421214653a8cf8627ce3954039b0
Hemal Patel <hemal.patel7735@outlook.com>
2018-12-09 15:08:00 +0530

    tests(config-type): use extractSummary

--- test/binCases/config-type/object/object.test.js
+++ test/binCases/config-type/object/object.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { run } = require("../../../testUtils");
+const { run, extractSummary } = require("../../../testUtils");
 
 test("object", () => {
 	const { code, stdout, stderr } = run(__dirname, [
@@ -12,9 +12,11 @@ test("object", () => {
 		"production"
 	]);
 
+	const summary = extractSummary(stdout);
+
 	expect(code).toBe(0);
-	expect(stdout).toEqual(expect.anything());
-	expect(stdout).toContain("entry.bundle.js");
+	expect(summary).toEqual(expect.anything());
+	expect(summary).toContain("entry.bundle.js");
 	expect(stderr).toHaveLength(0);
-	expect(stdout).toMatchSnapshot();
+	expect(summary).toMatchSnapshot();
 });
