3e30a57278431d617dfe9e77d83379a8a0e2f888
Hemal Patel <hemal.patel7735@outlook.com>
2018-12-09 14:47:42 +0530

    tests(config-name): use extractSummary

--- test/binCases/config-name/not-found/not-found.test.js
+++ test/binCases/config-name/not-found/not-found.test.js
@@ -1,11 +1,14 @@
 "use strict";
 
-const { run } = require("../../../testUtils");
+const { run, extractSummary } = require("../../../testUtils");
 
 test("not-found", () => {
 	const { code, stdout, stderr } = run(__dirname, ["--config", "./webpack.config.js", "--config-name", "foo"]);
+
+	const summary = extractSummary(stdout);
+
 	expect(code).not.toBe(0);
-	expect(stdout).toHaveLength(0);
+	expect(summary).toHaveLength(0);
 	expect(stderr).toContain("Configuration with name 'foo' was not found.");
-	expect(stdout).toMatchSnapshot();
+	expect(summary).toMatchSnapshot();
 });
