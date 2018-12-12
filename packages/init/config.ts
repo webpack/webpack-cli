990fdf2c5aa57a3338c1f0d848e3b7663a83aa6b
Rishabh Chawla <rishabh31121999@gmail.com>
2018-12-12 20:43:53 +0530

    feat(ui-init): documentation of functions

--- packages/init/config.ts
+++ packages/init/config.ts
@@ -1,7 +1,7 @@
 // Interface from @webpack-cli/generators/types
 import {IWebpackOptions} from "@webpack-cli/generators/types";
 import {ITransformConfig} from "@webpack-cli/utils/modify-config-helper";
-import { PathLike } from "fs";
+
 /**
  * Config class responsible for handling config generation
  */
@@ -12,6 +12,11 @@ export class Config {
 	private webpackOptions: IWebpackOptions;
 	private merge: object;
 	private configPath: string;
+
+	/**
+	 * Contructor for config class
+	 * @summary sets default values of each Data member
+	 */
 	constructor() {
 		this.configName = "";
 		this.topScope = [""];
@@ -20,20 +25,39 @@ export class Config {
 		this.configPath = process.cwd();
 	}
 
+	/**
+	 * pushToTopScope
+	 * @summary push lines to topScope of config to be scaffolded
+	 * @param args : string[] ( An array of lines to be pushed to topScope )
+	 */
 	public pushToTopScope(...args: string[]): void {
 		args.forEach((el) => {
 			this.topScope.push(el);
 		});
 	}
 
+	/**
+	 * setConfigName
+	 * @summary set name of config to be scaffolded to configName
+	 * @param configName : string (The name of config to be scaffolded)
+	 */
 	public setConfigName(configName: string): void {
 		this.configName = configName;
 	}
 
+	/**
+	 * setWebpackOption
+	 * @param optionName A option Name in accordance to interface IWebpackOptions
+	 * @param value The vaule of the optionName
+	 */
 	public setWebpackOption(optionName: string, value: any): void {
 		this.webpackOptions[optionName] = value;
 	}
 
+	/**
+	 * exportConfig()
+	 * @returns outputConfig The Config to be scaffolded ( passed to runTransform() )
+	 */
 	public exportConfig(): ITransformConfig {
 		const outputConfig = {
 			config: {
