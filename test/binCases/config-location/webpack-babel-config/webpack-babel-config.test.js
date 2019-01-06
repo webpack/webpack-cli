12e602d36feb0d04469a81fb29cf8096e1ffeb97
lakatostamas <mail.tamaslakatos@gmail.com>
2019-01-06 19:04:42 +0100

    tests(bin): add `webpack.config.babel.js` related test (#724)

---
+++ test/binCases/config-location/webpack-babel-config/webpack-babel-config.test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const { run } = require("../../../testUtils");
+
+test("webpack-babel-config", () => {
+	const { code, stdout, stderr } = run(__dirname, [
+		"--output-filename",
+		"[name].js",
+		"--output-chunk-filename",
+		"[id].chunk.js",
+		"--target",
+		"async-node",
+	]);
+	expect(code).toBe(0);
+	expect(stdout).toContain("./index2.js");
+	expect(stderr).toHaveLength(0);
+});
