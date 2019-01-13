42e5ee84f16c4d7012819f44913c05c893e715c4
Hemal Patel <hemal.patel7735@outlook.com>
2019-01-13 22:46:30 +0530

    tests(watch): hash assertion info-verbosity-verbose

--- test/binCases/watch/info-verbosity-verbose/info-verbosity-verbose.test.js
+++ test/binCases/watch/info-verbosity-verbose/info-verbosity-verbose.test.js
@@ -4,10 +4,36 @@ jest.setTimeout(10E6);
 /* eslint-disable node/no-unsupported-features  */
 /* eslint-disable node/no-unsupported-features/es-syntax  */
 
-const { runWatch, extractSummary } = require("../../../testUtils");
+const fs = require("fs");
+const path = require("path");
+const { extractSummary, extractHash, appendDataIfFileExists, runAndGetWatchProc } = require("../../../testUtils");
+
+const fileToChange = "index.js";
+const copyFile = "index_copy.js";
+const fileToChangePath = path.resolve(__dirname, fileToChange);
+const copyFilePath = path.resolve(__dirname, copyFile);
+
+// create copy of "index.js" => "index_copy.js"
+beforeEach(() => {
+	// fs.copyFileSync was added in Added in: v8.5.0
+	// We should refactor the below code once our minimal supported version is v8.5.0
+	fs.createReadStream(fileToChangePath).pipe(fs.createWriteStream(copyFilePath));
+});
+
+afterEach(() => {
+	try {
+		// subsequent test-case runs won't pass as snapshot is not matched
+		// hence, deleting the file as it is modified by the test
+		fs.unlinkSync(fileToChangePath);
+	} catch (e) {
+		console.warn("could not remove the file:" + fileToChangePath + "\n" + e.message);
+	} finally {
+		fs.renameSync(copyFilePath, fileToChangePath);
+	}
+});
 
 test("info-verbosity-verbose", async done => {
-	const result = await runWatch(__dirname, [
+	const webpackProc = runAndGetWatchProc(__dirname, [
 		"--entry ",
 		"./index.js",
 		"--config",
@@ -22,15 +48,62 @@ test("info-verbosity-verbose", async done => {
 		"--info-verbosity",
 		"verbose"
 	]);
-	const { stdout, stderr } = result;
 
-	const summary = extractSummary(stdout);
+	// Watch mode with --info-verbosity verbose does not spit the output in one go.
+	// So we need to keep a track of chunks output order
+	// 1. Compilation starting...
+	// 2. webpack is watching the files...
+	// 3. Compilation  finished
+	// 4. Hash and other info
+	// 5. Compilation starting...
+	// 6. Compilation  finished
+	// 7. Hash and other info
+	var chunkNumber = 0;
+	var hash1, hash2;
 
-	expect(summary).toEqual(expect.anything());
-	expect(summary).toContain("Compilation");
-	expect(summary).toContain("webpack is watching the files…");
+	webpackProc.stdout.on("data", data => {
+		data = data.toString();
+		chunkNumber++;
 
-	expect(stderr).toHaveLength(0);
-	expect(summary).toMatchSnapshot();
-	done();
+		switch (chunkNumber) {
+			case 1:
+			case 5:
+				expect(data).toContain("Compilation  starting");
+				break;
+			case 2:
+				expect(data).toContain("webpack is watching the files");
+				break;
+			case 3:
+			case 6:
+				expect(data).toContain("Compilation  finished");
+				break;
+			case 4:
+				expect(extractSummary(data)).toMatchSnapshot();
+
+				hash1 = extractHash(data);
+
+				// We get webpack output after running test
+				// Since we are running the webpack in watch mode, changing file will generate additional output
+				// First time output will be validated fully
+				// Hash of the The subsequent output will be tested against that of first time output
+				appendDataIfFileExists(__dirname, fileToChange, "//junk-comment");
+
+				break;
+			case 7:
+				hash2 = extractHash(data);
+
+				expect(hash2.hash).not.toBe(hash1.hash);
+
+				webpackProc.kill();
+				done();
+				break;
+			default:
+				break;
+		}
+	});
+
+	webpackProc.stderr.on("data", error => {
+		// fail test case if there is any error
+		done(error.toString());
+	});
 });
