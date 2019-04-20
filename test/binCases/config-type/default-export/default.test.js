fc9bc8dd92ddbceb20fce18762b027a883debf9d
Tomasz Sodzawiczny <tomasz.sodzawiczny@brainly.com>
2019-04-20 12:17:10 +0200

    tests: add default export handling test

---
+++ test/binCases/config-type/default-export/default.test.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const { run, extractSummary } = require("../../../testUtils");
+
+test("object", () => {
+	const { code, stdout, stderr } = run(__dirname, [
+		"--config",
+		"./webpack.config.js",
+		"--target",
+		"async-node",
+		"--mode",
+		"production"
+	]);
+
+	const summary = extractSummary(stdout);
+
+	expect(code).toBe(0);
+	expect(summary).toEqual(expect.anything());
+	expect(summary).toContain("entry.bundle.js");
+	expect(stderr).toHaveLength(0);
+});
