b3c98ad3cf20d1dc7767465ffc4d86227a403868
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-06-14 00:17:33 +0530

    chore(comments): removes unwanted comments
    removed comments

--- packages/info/renderTable.ts
+++ packages/info/renderTable.ts
@@ -1,12 +1,10 @@
 import * as Table from "cli-table3";
 
 export function renderTable(data, fileName): string {
-	// instantiate
 	let table = new Table({
 		head: ["Config", fileName]
 	});
 
-	// table is an Array, so you can `push`, `unshift`, `splice` and friends
 	data.map((elm: Table.Cell[] & Table.VerticalTableRow & Table.CrossTableRow): void => {
 		table.push(elm);
 	});
