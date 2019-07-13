b9c4260c40d50a3ae68a4a9a4b7f02554a3d162d
anikethsaha <anik220798@gmail.com>
2019-07-14 01:11:37 +0530

    chore: added more tests for path-utils

--- packages/utils/__tests__/path-utils.test.ts
+++ packages/utils/__tests__/path-utils.test.ts
@@ -2,12 +2,37 @@ import { join } from "path";
 import { isLocalPath, findProjectRoot } from "../path-utils";
 
 describe("path-utils tests", () => {
-  it("isLocalPath Testing : __dirname", () => {
-    const answer = isLocalPath(join(__dirname, "../../"));
-    expect(answer).toBe(true);
-  });
-  it("findProjectRoot testing", () => {
-    const answer = findProjectRoot();
-    expect(answer).toBe(process.cwd());
-  });
+	it("isLocalPath Testing : __dirname", () => {
+		const answer = isLocalPath(join(__dirname, "../../"));
+		expect(answer).toBe(true);
+	});
+	it("findProjectRoot testing", () => {
+		const answer = findProjectRoot();
+		expect(answer).toBe(process.cwd());
+	});
+	it("isLocalPath Testing : ../types", () => {
+		const answer = isLocalPath(join(__dirname, "../types"));
+		expect(answer).toBe(true);
+	});
+	it("isLocalPath Testing : ../notLocalPath", () => {
+		const answer = isLocalPath("../notLocalPath");
+		expect(answer).toBe(false);
+	});
+	it("isLocalPath Testing : ../notLocalPath using path.join", () => {
+		const answer = isLocalPath(join(__dirname, "../notLocalPath"));
+		expect(answer).toBe(false);
+	});
+	it("isLocalPath Testing : ./_snapshots (./path)", () => {
+		const answer = isLocalPath(join(__dirname, "./__snapshots__"));
+		expect(answer).toBe(true);
+	});
+
+	it("isLocalPath Testing : /_snapshots/ast-utils.test.ts.snap (path/path)", () => {
+		const answer = isLocalPath(join(__dirname, "/__snapshots__/ast-utils.test.ts.snap"));
+		expect(answer).toBe(true);
+	});
+	it("isLocalPath Testing : ..\\ win", () => {
+		const answer = isLocalPath("..\\");
+		expect(answer).toBe(true);
+	});
 });
