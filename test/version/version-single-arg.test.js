9792c8183cf8d7628d3e18b09101390a558079ca
Rishabh Chawla <rishabh31121999@gmail.com>
2019-08-17 17:36:32 +0530

    chore: update variable to be understandable

--- test/version/version-single-arg.test.js
+++ test/version/version-single-arg.test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const { run } = require('../utils/test-utils');
-const pkgLock = require('../../package.json');
+const pkgJSON = require('../../package.json');
 
 describe('single version flag', () => {
     it('outputs versions with command syntax', () => {
         const { stdout, stderr } = run(__dirname, ['version']);
-        expect(stdout).toContain(pkgLock.version);
+        expect(stdout).toContain(pkgJSON.version);
         expect(stderr).toHaveLength(0);
     });
 
     it('outputs versions with dashed syntax', () => {
         const { stdout, stderr } = run(__dirname, ['--version']);
-        expect(stdout).toContain(pkgLock.version);
+        expect(stdout).toContain(pkgJSON.version);
         expect(stderr).toHaveLength(0);
     });
 });
