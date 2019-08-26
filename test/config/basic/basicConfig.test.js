ab6a65085711b8ce2de793b06c0554ed9dad7eb5
anikethsaha <anik220798@gmail.com>
2019-08-26 13:58:50 +0530

    chore: change the directory splitting and structured the tests

--- test/config/basic/basicConfig.test.js
+++ test/config/basic/basicConfig.test.js
@@ -1,22 +1,29 @@
 'use strict';
 const { stat } = require('fs');
-const { resolve } = require('path');
+const { resolve, sep } = require('path');
 const { run, extractSummary } = require('../../utils/test-utils');
 
 describe('config flag test : Basic config file', () => {
-    it('Should not throw any module not found', done => {
-        const { stdout } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js')]);
-        const summary = extractSummary(stdout);
-        const outputDir = 'basic/bin';
-        const outDirectoryFromCompiler = summary['Output Directory'];
-        const outDirToMatch = outDirectoryFromCompiler
-            .split('\\')
-            .slice(outDirectoryFromCompiler.split('\\').length - 2, outDirectoryFromCompiler.split('\\').length)
-            .join('/');
+    const { stdout } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js')]);
+    const summary = extractSummary(stdout);
+    const outputDir = 'basic/bin';
+    it('should log Output Directory as basic/bin', done => {
+        const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
+        const outDirToMatch = outDirectoryFromCompiler.slice(outDirectoryFromCompiler.length - 2, outDirectoryFromCompiler.length).join('/');
         expect(outDirToMatch).toContain(outputDir);
+        done();
+    });
+    it('should create a bin Directory', done => {
         stat(resolve(__dirname, './bin'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isDirectory()).toBe(true);
+            done();
+        });
+    });
+    it('should create a file name bundle.js inside bin', done => {
+        stat(resolve(__dirname, './bin/bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
             done();
         });
     });
--- test/config/empty/emptyConfig.test.js
+++ test/config/empty/emptyConfig.test.js
@@ -1,20 +1,23 @@
 'use strict';
 const { stat } = require('fs');
-const { resolve } = require('path');
+const { resolve, sep } = require('path');
 const { run, extractSummary } = require('../../utils/test-utils');
 
 describe('config flag test : Empty config file', () => {
-    it('compile but throw missing entry module error', done => {
-        const { stdout, stderr } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js')]);
-        const summary = extractSummary(stdout);
-        const outputDir = 'empty/bin';
-        const outDirectoryFromCompiler = summary['Output Directory'];
-        const outDirToMatch = outDirectoryFromCompiler
-            .split('\\')
-            .slice(outDirectoryFromCompiler.split('\\').length - 2, outDirectoryFromCompiler.split('\\').length)
-            .join('/');
+    const { stdout, stderr } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js')]);
+    const summary = extractSummary(stdout);
+    const outputDir = 'empty/bin';
+    it('should log Output Directory as basic/bin', done => {
+        const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
+        const outDirToMatch = outDirectoryFromCompiler.slice(outDirectoryFromCompiler.length - 2, outDirectoryFromCompiler.length).join('/');
         expect(outDirToMatch).toContain(outputDir);
+        done();
+    });
+    it('compile but throw missing entry module error', done => {
         expect(stderr).toContain('Entry module not found');
+        done();
+    });
+    it('should create a bin Directory', done => {
         stat(resolve(__dirname, './bin'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isDirectory()).toBe(true);
