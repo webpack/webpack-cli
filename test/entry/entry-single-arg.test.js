078a1e4bbe8a6515ab8239859110d8a4967a1154
anikethsaha <anik220798@gmail.com>
2019-08-26 14:06:39 +0530

    chore:  changed the split seperator

--- test/entry/entry-single-arg.test.js
+++ test/entry/entry-single-arg.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { stat } = require('fs');
-const { resolve } = require('path');
+const { resolve, sep } = require('path');
 
 const { run, extractSummary } = require('../utils/test-utils');
 
@@ -10,11 +10,8 @@ describe('single entry flag', () => {
         const { stdout, stderr } = run(__dirname);
         const summary = extractSummary(stdout);
         const outputDir = 'entry/bin';
-        const outDirectoryFromCompiler = summary['Output Directory'];
-        const outDirToMatch = outDirectoryFromCompiler
-            .split('\\')
-            .slice(outDirectoryFromCompiler.split('\\').length - 2, outDirectoryFromCompiler.split('\\').length)
-            .join('/');
+        const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
+        const outDirToMatch = outDirectoryFromCompiler.slice(outDirectoryFromCompiler.length - 2, outDirectoryFromCompiler.length).join('/');
         expect(outDirToMatch).toContain(outputDir);
         expect(stderr).toContain('Entry module not found');
         stat(resolve(__dirname, './bin'), (err, stats) => {
