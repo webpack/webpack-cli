79137d0800a161cb810236f384be48b5365e1a77
Even Stensberg <evenstensberg@gmail.com>
2019-09-17 09:25:06 +0200

    chore: rebase

--- test/entry/defaults-empty/entry-single-arg.test.js
+++ test/entry/defaults-empty/entry-single-arg.test.js
@@ -9,7 +9,7 @@ describe('single entry flag empty project', () => {
     it('sets default entry, compiles but throw missing entry module error', done => {
         const { stdout, stderr } = run(__dirname);
         const summary = extractSummary(stdout);
-        const outputDir = 'entry/bin';
+        const outputDir = 'defaults-empty/bin';
         const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
         const outDirToMatch = outDirectoryFromCompiler.slice(outDirectoryFromCompiler.length - 2, outDirectoryFromCompiler.length).join('/');
         expect(outDirToMatch).toContain(outputDir);
