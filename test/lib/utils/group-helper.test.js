386ee1c44a1bddd5fd10b2852460f1c76d232c76
anikethsaha <anik220798@gmail.com>
2019-09-21 19:22:46 +0530

    chore: moved the group-helperlib test to test/lib/utils

--- test/lib/utils/group-helper.test.js
+++ test/lib/utils/group-helper.test.js
@@ -1,10 +1,10 @@
 const { parse, sep } = require('path');
-const GroupHelper = require('../lib/utils/group-helper');
+const GroupHelper = require('../../../lib/utils/group-helper');
 
 describe('group-helper tests', () => {
     describe('resolveFilePath', () => {
         it('should return correct path : sample/entry.js', done => {
-            const GroupHelperOptions = [{ entry: 'test/sample/entry.js' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
+            const GroupHelperOptions = [{ entry: 'test/lib/utils/sample/entry.js' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
             const GroupHelperObj = new GroupHelper(GroupHelperOptions);
             const entryPath = GroupHelperObj.resolveFilePath(GroupHelperOptions[0].entry);
             const { dir, base } = parse(entryPath);
@@ -13,7 +13,7 @@ describe('group-helper tests', () => {
             done();
         });
         it('should return correct path : sample/entry', done => {
-            const GroupHelperOptions = [{ entry: 'test/sample/entry' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
+            const GroupHelperOptions = [{ entry: 'test/lib/utils/sample/entry' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
             const GroupHelperObj = new GroupHelper(GroupHelperOptions);
             const entryPath = GroupHelperObj.resolveFilePath(GroupHelperOptions[0].entry);
             const { dir, base } = parse(entryPath);
@@ -22,7 +22,7 @@ describe('group-helper tests', () => {
             done();
         });
         it('should return correct path : sample', done => {
-            const GroupHelperOptions = [{ entry: 'test/sample' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
+            const GroupHelperOptions = [{ entry: 'test/lib/utils/sample' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
             const GroupHelperObj = new GroupHelper(GroupHelperOptions);
             const entryPath = GroupHelperObj.resolveFilePath(GroupHelperOptions[0].entry);
             const { dir, base } = parse(entryPath);
@@ -31,7 +31,7 @@ describe('group-helper tests', () => {
             done();
         });
         it('should return correct path : sample/index.js', done => {
-            const GroupHelperOptions = [{ entry: 'test/sample/index.js' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
+            const GroupHelperOptions = [{ entry: 'test/lib/utils/sample/index.js' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
             const GroupHelperObj = new GroupHelper(GroupHelperOptions);
             const entryPath = GroupHelperObj.resolveFilePath(GroupHelperOptions[0].entry);
             const { dir, base } = parse(entryPath);
@@ -40,7 +40,7 @@ describe('group-helper tests', () => {
             done();
         });
         it('should return correct path : sample/index', done => {
-            const GroupHelperOptions = [{ entry: 'test/sample/index' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
+            const GroupHelperOptions = [{ entry: 'test/lib/utils/sample/index' }, { progress: 'bar' }, { sourcemap: 'eval' }, { dev: false }, { prod: true }];
             const GroupHelperObj = new GroupHelper(GroupHelperOptions);
             const entryPath = GroupHelperObj.resolveFilePath(GroupHelperOptions[0].entry);
             const { dir, base } = parse(entryPath);
