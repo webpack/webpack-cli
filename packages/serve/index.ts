656898408aba7aac324a9e11fcfccbd4d7e4635c
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-09-26 16:13:24 -0500

    style(serve): make args to camel case helper hyphen based filename

--- packages/serve/index.ts
+++ packages/serve/index.ts
@@ -1,7 +1,7 @@
 import * as cmdArgs from "command-line-args";
 import * as WebpackCLI from "../../lib/webpack-cli";
 import * as startDevServer from "./startDevServer";
-import * as argsToCamelCase from "./argsToCamelCase";
+import * as argsToCamelCase from "./args-to-camel-case";
 import { devServer } from "../../lib/utils/dev-server-flags";
 import { core } from "../../lib/utils/cli-flags";
 
