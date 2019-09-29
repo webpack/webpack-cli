d0ac2e336be784eec1be958958947c7fa140f519
Even Stensberg <evenstensberg@gmail.com>
2019-09-29 12:09:09 +0200

    tests: add env tests

---
+++ test/env/array/array-env.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path');
+const execa = require('execa');
+
+const { sync: spawnSync } = execa;
+const { run } = require('../../utils/test-utils');
+
+describe('env array', () => {
+    it('is able to set two different environments for an array configuration', () => {
+        run(__dirname);
+        const devFile = path.join(__dirname, './bin/dev.js');
+        const prodFile = path.join(__dirname, './bin/prod.js');
+
+        const devScript = spawnSync('node', [devFile]);
+        const prodScript = spawnSync('node', [prodFile]);
+
+        expect(devScript.stdout).toBe('environment is development');
+        expect(prodScript.stdout).toBe('environment is production');
+    });
+});
