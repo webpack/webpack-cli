e02eac4f6a1ec2f7d9b0736dccbf860c996b577f
Even Stensberg <evenstensberg@gmail.com>
2019-10-31 06:03:52 +0100

    chore: add more detailed test

--- test/config-lookup/dotfiles/dotfiles-lookup.test.js
+++ test/config-lookup/dotfiles/dotfiles-lookup.test.js
@@ -1,10 +1,25 @@
 'use strict';
-const { run } = require('../../utils/test-utils');
+
+const { stat } = require('fs');
+const { resolve } = require('path');
+
+const { run, extractSummary } = require('../../utils/test-utils');
 
 describe('config lookup test : dotfolder', () => {
-    it('should find a webpack configuration in a dotfolder', () => {
+    it('should find a webpack configuration in a dotfolder', done => {
         const { stdout, stderr } = run(__dirname, [], false);
         expect(stderr).not.toBeUndefined();
         expect(stdout).not.toBeUndefined();
+
+        const summary = extractSummary(stdout);
+        const outputDir = 'config-lookup/dotfiles/dist';
+
+        expect(summary['Output Directory']).toContain(outputDir);
+        expect(stderr).not.toContain('Entry module not found');
+        stat(resolve(__dirname, './dist/bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
     });
 });
