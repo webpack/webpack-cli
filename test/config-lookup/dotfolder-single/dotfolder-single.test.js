e83f35e9a8584124618a6de7d574586ddfde5335
Even Stensberg <evenstensberg@gmail.com>
2019-11-09 13:49:20 +0100

    Revert "Revert "Merge remote-tracking branch 'upstream/next' into next""
    This reverts commit 9db30788e7c3a7fcd2babf63d63d4ec0de940c60.

---
+++ test/config-lookup/dotfolder-single/dotfolder-single.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { stat } = require('fs');
+const { resolve } = require('path');
+
+const { run, extractSummary } = require('../../utils/test-utils');
+
+describe('dotfolder single config lookup', () => {
+    it('should find a webpack configuration in a dotfolder', done => {
+        const { stdout, stderr } = run(__dirname, [], false);
+        expect(stderr).not.toBeUndefined();
+        expect(stdout).not.toBeUndefined();
+
+        const summary = extractSummary(stdout);
+        const outputDir = 'config-lookup/dotfolder-single/dist';
+
+        expect(summary['Output Directory']).toContain(outputDir);
+        expect(stderr).not.toContain('Entry module not found');
+        stat(resolve(__dirname, './dist/bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+});
---
+++ test/output/defaults/output-defaults.test.js
@@ -0,0 +1,34 @@
+'use strict';
+const { stat } = require('fs');
+const { resolve } = require('path');
+const { run, extractSummary } = require('../../utils/test-utils');
+
+describe('output flag defaults', () => {
+    it('should create default file for a given directory', done => {
+        const { stdout } = run(__dirname, ['--entry', './a.js', '--output', './binary'], false);
+        const summary = extractSummary(stdout);
+        const outputDir = 'defaults/binary';
+
+        expect(summary['Output Directory']).toContain(outputDir);
+
+        stat(resolve(__dirname, './binary/bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+    it('set default output directory on empty flag', done => {
+        const { stdout, stderr } = run(__dirname, ['--entry', './a.js', '--output'], false);
+        expect(stderr).toContain('option has not been set, webpack will fallback to');
+        const summary = extractSummary(stdout);
+
+        const outputDir = 'defaults/dist';
+
+        expect(summary['Output Directory']).toContain(outputDir);
+        stat(resolve(__dirname, './dist/bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+});
