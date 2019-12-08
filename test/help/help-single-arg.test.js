294413385e094f75f79d7ddc1f19053ffbb87205
Rishabh Chawla <rishabh31121999@gmail.com>
2019-12-08 22:12:18 +0530

    tests: remove output flags from child process

--- test/help/help-single-arg.test.js
+++ test/help/help-single-arg.test.js
@@ -6,7 +6,7 @@ const helpHeader = 'The build tool for modern web applications';
 
 describe('single help flag', () => {
     it('respects --color flag as false', () => {
-        const { stdout, stderr } = run(__dirname, ['--help', '--color=false']);
+        const { stdout, stderr } = run(__dirname, ['--help', '--color=false'], false);
         const usage = 'webpack [...options] | <command>';
         const example = 'webpack help --flag | <command>';
         chalk.enabled = true;
@@ -19,19 +19,19 @@ describe('single help flag', () => {
         expect(stderr).toHaveLength(0);
     });
     it('outputs help info with command syntax', () => {
-        const { stdout, stderr } = run(__dirname, ['help']);
+        const { stdout, stderr } = run(__dirname, ['help'], false);
         expect(stdout).toContain(helpHeader);
         expect(stderr).toHaveLength(0);
     });
 
     it('outputs help info with dashed syntax', () => {
-        const { stdout, stderr } = run(__dirname, ['--help']);
+        const { stdout, stderr } = run(__dirname, ['--help'], false);
         expect(stdout).toContain(helpHeader);
         expect(stderr).toHaveLength(0);
     });
 
     it('creates a readable snapshot', () => {
-        const { stdout, stderr } = run(__dirname, ['--help']);
+        const { stdout, stderr } = run(__dirname, ['--help'], false);
 
         const serializer = require('jest-serializer-ansi');
         expect.addSnapshotSerializer(serializer);
