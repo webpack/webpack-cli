72490b446e706719a25a28ade2db38599c8853c8
emanuele <my.burning@gmail.com>
2020-01-17 09:24:57 +0000

    fix: merge config test

--- test/merge/config/merge-config.test.js
+++ test/merge/config/merge-config.test.js
@@ -9,7 +9,7 @@ describe('merge flag configuration', () => {
     it('merges two configurations together', done => {
         const { stderr } = run(__dirname, ['--config', './1.js', '--merge', './2.js'], false);
         expect(stderr).toContain('option has not been set, webpack will fallback to');
-        stat(resolve(__dirname, './dist/main.js'), (err, stats) => {
+        stat(resolve(__dirname, './dist/merged.js'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isFile()).toBe(true);
             done();
