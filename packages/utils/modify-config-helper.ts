aa6d82b649c9a1f4c54566b80597576d9bb554b4
Anshuman Verma <anshu.av97@gmail.com>
2020-01-25 11:43:26 +0530

    fix(create): handle create package errors gracefully (#1159)
    * fix(create): handle create package errors gracefully
    
    handle package creation errors
    
    ISSUES CLOSED: #1151
    
    * chore(create): update create package err message
    
    update create package err message
    
    ISSUES CLOSED: #1151
    
    * chore(create): update package present comment
    
    update comment to something
    
    ISSUES CLOSED: #1151
    
    * chore(create): update err message and exit code
    
    update err message and exit code
    
    ISSUES CLOSED: #1151

--- packages/utils/modify-config-helper.ts
+++ packages/utils/modify-config-helper.ts
@@ -64,6 +64,13 @@ export default function modifyHelperUtil(action: string, generator: typeof Gener
         };
     }
 
+    // Presently only one package can be scaffolded in a directory.
+    // TODO - Remove this once we support multiple scaffolds in the same directory.
+    if (fs.existsSync('package.json')) {
+        console.error(`${chalk.bold.red('Error: ')}You already have a ${chalk.blue('package.json')} file in this directory. Scaffolds cannot be created on top of existing projects.`);
+        process.exit(1);
+    }
+
     env.registerStub(generator, generatorName);
     env.run(generatorName, {
         configFile,
@@ -76,7 +83,7 @@ export default function modifyHelperUtil(action: string, generator: typeof Gener
                 configModule = require(confPath);
                 // Change structure of the config to be transformed
                 const tmpConfig: WebpackScaffoldObject = {
-                    config: {}
+                    config: {},
                 };
                 Object.keys(configModule)
                     .filter(config => {
@@ -103,10 +110,9 @@ export default function modifyHelperUtil(action: string, generator: typeof Gener
                 configModule,
             );
             if (configModule.usingDefaults && configModule.usingDefaults === true) {
-                const runCommand = getPackageManager() === "yarn" ? "yarn build" : "npm run build";
+                const runCommand = getPackageManager() === 'yarn' ? 'yarn build' : 'npm run build';
 
-                const successMessage =
-                    `\nYou can now run ${chalk.green(runCommand)} to bundle your application!\n\n`;
+                const successMessage = `\nYou can now run ${chalk.green(runCommand)} to bundle your application!\n\n`;
                 process.stdout.write(`\n${successMessage}`);
                 return;
             }
