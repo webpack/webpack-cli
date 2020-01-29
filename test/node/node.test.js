49db9de1e357dc1028baba8576df0d27b5893c28
Sergey Melyukov <s.melukov@gmail.com>
2020-01-29 17:07:34 +0300

    feat(cli): allow to pass flags to node.js closes #1084 closes #289

---
+++ test/node/node.test.js
@@ -0,0 +1,23 @@
+'use strict';
+const { stat } = require('fs');
+const { resolve, sep } = require('path');
+const { run, extractSummary } = require('../utils/test-utils');
+
+describe('node flags', () => {
+    it('is able to options flags to node js', done => {
+        const { stdout, stderr } = run(__dirname, ['--node-args', `--require=${resolve(__dirname, 'bootstrap.js')}`, '--node-args', `-r ${resolve(__dirname, 'bootstrap2.js')}`, '--output', './bin/[name].bundle.js'], false);
+        expect(stderr).toContain('');
+        expect(stdout).toContain('---from bootstrap.js---');
+        expect(stdout).toContain('---from bootstrap2.js---');
+        const summary = extractSummary(stdout);
+        const outputDir = 'node/bin';
+        const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
+        const outDirToMatch = outDirectoryFromCompiler.slice(outDirectoryFromCompiler.length - 2, outDirectoryFromCompiler.length).join('/');
+        expect(outDirToMatch).toContain(outputDir);
+        stat(resolve(__dirname, './bin/main.bundle.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+});
