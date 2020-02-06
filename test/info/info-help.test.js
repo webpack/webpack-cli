000cc0437dc77f5ebc34def9c79b232be372d835
Anshuman Verma <anshu.av97@gmail.com>
2020-02-06 23:57:29 +0530

    tests(info): fix info help tests
    fix info help tests

--- test/info/info-help.test.js
+++ test/info/info-help.test.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const chalk = require('chalk');
+const path = require('path');
 const { run } = require('../utils/test-utils');
 const { commands } = require('../../lib/utils/cli-flags');
+
+const runInfo = args => {
+    return run(path.resolve(__dirname, './shim'), args, false);
+};
 
 const infoFlags = commands.find(c => c.name === 'info').flags;
 
@@ -11,21 +16,21 @@ const descriptionText = 'Outputs information about your system and dependencies'
 
 describe('should print help for info command', () => {
     it('help flag supplied after info', () => {
-        const { stdout, stderr } = run(__dirname, ['info', 'help']);
+        const { stdout, stderr } = runInfo(['info', 'help']);
         expect(stdout).toContain(usageText);
         expect(stdout).toContain(descriptionText);
         expect(stderr).toHaveLength(0);
     });
 
     it('dashed help flag supplied before info', () => {
-        const { stdout, stderr } = run(__dirname, ['--help', 'info']);
+        const { stdout, stderr } = runInfo(['--help', 'info']);
         expect(stdout).toContain(usageText);
         expect(stdout).toContain(descriptionText);
         expect(stderr).toHaveLength(0);
     });
 
     it('should respect the --color flag', () => {
-        const { stdout, stderr } = run(__dirname, ['info', 'help', '--color=true']);
+        const { stdout, stderr } = runInfo(['info', 'help', '--color=true']);
         chalk.enabled = true;
         chalk.level = 3;
         const orange = chalk.keyword('orange');
@@ -35,7 +40,7 @@ describe('should print help for info command', () => {
     });
 
     it('should output all cli flags', () => {
-        const { stdout, stderr } = run(__dirname, ['info', 'help']);
+        const { stdout, stderr } = runInfo(['info', 'help']);
         infoFlags.forEach(flag => expect(stdout).toContain(`--${flag.name}`));
         expect(stderr).toHaveLength(0);
     });
