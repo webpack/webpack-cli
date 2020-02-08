a6804d625cb206bd95cc9c850a4e155f97cb4678
Anshuman Verma <anshu.av97@gmail.com>
2020-02-08 22:40:10 +0530

    tests(info): add tests for info package arguments/flags (#1147)
    * tests(info): add flag tests for info
    
    add tests for flags in info
    
    * tests(info): add another test to print all available flags
    
    add test to print flags in info
    
    * tests(info): fix info help tests
    
    fix info help tests
    
    * tests(info): fix info-help tests
    
    fix info-help tests
    
    * tests(info): add more tests for info flags
    
    add more info flag tests
    
    * tests(info): fix file name in info tests
    
    fix file name in info tests

---
+++ test/info/info-help.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const chalk = require('chalk');
+const path = require('path');
+const { run } = require('../utils/test-utils');
+const { commands } = require('../../lib/utils/cli-flags');
+
+const runInfo = args => {
+    return run(path.resolve(__dirname, './shim'), args, false);
+};
+
+const infoFlags = commands.find(c => c.name === 'info').flags;
+
+const usageText = 'webpack info [options] [output-format]';
+const descriptionText = 'Outputs information about your system and dependencies';
+
+describe('should print help for info command', () => {
+    it('help flag supplied after info', () => {
+        const { stdout, stderr } = runInfo(['info', 'help']);
+        expect(stdout).toContain(usageText);
+        expect(stdout).toContain(descriptionText);
+        expect(stderr).toHaveLength(0);
+    });
+
+    it('dashed help flag supplied before info', () => {
+        const { stdout, stderr } = runInfo(['--help', 'info']);
+        expect(stdout).toContain(usageText);
+        expect(stdout).toContain(descriptionText);
+        expect(stderr).toHaveLength(0);
+    });
+    it('should output help even if other flags are supplied with help', () => {
+        const { stdout, stderr } = runInfo(['info', '--help', '--system']);
+        expect(stdout).toContain(usageText);
+        expect(stdout).toContain(descriptionText);
+        expect(stdout).not.toContain('System:');
+        expect(stderr).toHaveLength(0);
+    });
+
+    it('should respect the --color=false flag', () => {
+        const { stdout, stderr } = runInfo(['info', 'help', '--color=false']);
+        chalk.enabled = true;
+        chalk.level = 3;
+        const orange = chalk.keyword('orange');
+        expect(stdout).not.toContain(orange(usageText));
+        expect(stdout).toContain(descriptionText);
+        expect(stderr).toHaveLength(0);
+    });
+
+    it('should output all cli flags', () => {
+        const { stdout, stderr } = runInfo(['info', 'help']);
+        infoFlags.forEach(flag => expect(stdout).toContain(`--${flag.name}`));
+        expect(stderr).toHaveLength(0);
+    });
+});
