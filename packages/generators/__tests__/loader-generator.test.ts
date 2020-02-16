a5af8eee1611d57038ba89400ab785037478ad02
Anshuman Verma <anshu.av97@gmail.com>
2020-02-17 01:00:12 +0530

    tests(loader-generator): add tests for loader generator
    add tests for loader generator

--- packages/generators/__tests__/loader-generator.test.ts
+++ packages/generators/__tests__/loader-generator.test.ts
@@ -1,13 +1,44 @@
-import { makeLoaderName } from "../loader-generator";
+import { makeLoaderName } from '../loader-generator';
+import { join } from 'path';
+import { run } from 'yeoman-test';
+const assert = require('yeoman-assert');
 
-describe("makeLoaderName", () => {
-	it("should kebab-case loader name and append '-loader'", () => {
-		const loaderName = makeLoaderName("This is a test");
-		expect(loaderName).toEqual("this-is-a-test-loader");
-	});
+describe('loader generator', () => {
+    it('generates a default loader', async () => {
+        const outputDir = await run(join(__dirname, '../loader-generator'));
+        const loaderDir = `${outputDir}/my-loader`;
+        const srcFiles = ['cjs.js', 'index.js'];
+        const testFiles = ['functional.test.js', 'test-utils.js', 'unit.test.js', 'fixtures/simple-file.js'];
+        const exampleFiles = ['webpack.config.js', 'src/index.js', 'src/lazy-module.js', 'src/static-esm-module.js'];
 
-	it("should not modify a properly formatted loader name", () => {
-		const loaderName = makeLoaderName("properly-named-loader");
-		expect(loaderName).toEqual("properly-named-loader");
-	});
+        // Check that files in all folders are scaffolded. Checking them separately so we know which directory has the problem
+        // assert for src files
+        assert.file([...srcFiles.map(file => `${loaderDir}/src/${file}`)]);
+
+        // assert for test files
+        assert.file([...testFiles.map(file => `${loaderDir}/test/${file}`)]);
+
+        // assert for example files
+        assert.file([...exampleFiles.map(file => `${loaderDir}/examples/simple/${file}`)]);
+
+        // Check the contents of the webpack config and loader file
+        assert.fileContent([
+            [`${loaderDir}/examples/simple/webpack.config.js`, 'resolveLoader: {'],
+            [`${loaderDir}/src/index.js`, 'export default function loader(source) {'],
+        ]);
+
+        // higher timeout so travis has enough time to execute
+    }, 10000);
+});
+
+describe('makeLoaderName', () => {
+    it("should kebab-case loader name and append '-loader'", () => {
+        const loaderName = makeLoaderName('This is a test');
+        expect(loaderName).toEqual('this-is-a-test-loader');
+    });
+
+    it('should not modify a properly formatted loader name', () => {
+        const loaderName = makeLoaderName('properly-named-loader');
+        expect(loaderName).toEqual('properly-named-loader');
+    });
 });
