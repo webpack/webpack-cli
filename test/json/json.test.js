8f3259ea525a54a10c3f5f4ca9381e22ced5e93d
Emanuele <my.burning@gmail.com>
2020-02-24 10:39:49 +0000

    chore: added webpack 4 to the matrix and changed tests (#1260)

--- test/json/json.test.js
+++ test/json/json.test.js
@@ -1,21 +1,23 @@
 'use strict';
-const { stat } = require('fs');
-const { resolve, join } = require('path');
+const { join } = require('path');
 const { run } = require('../utils/test-utils');
+const webpack = require('webpack');
 
 describe('json flag', () => {
-    it('should match the snapshot of --json command', done => {
+    it('should match the snapshot of --json command', async () => {
         const { stdout } = run(__dirname, [__dirname, '--json']);
         const jsonstdout = JSON.parse(stdout);
-        expect(jsonstdout.outputPath).toBe(join(__dirname, 'bin'));
-        expect(jsonstdout.assetsByChunkName.main[0]).toBe('main.js');
-        expect(jsonstdout.chunks[0].modules[0].reasons[1].userRequest).toBe(join(__dirname, 'index.js'));
-        expect(jsonstdout.chunks[0].names[0]).toBe('main');
-        stat(resolve(__dirname, 'bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(jsonstdout.assets[0].size).toBe(stats.size);
-            expect(stats.isFile()).toBe(true);
-            done();
+        const compiler = await webpack({
+            entry: './index.js',
+            output: {
+                filename: 'main.js',
+                path: join(__dirname, 'bin'),
+            },
+        });
+        compiler.run((err, stats) => {
+            expect(err).toBeFalsy();
+            const webpackStats = stats.toJson({ json: true });
+            expect(jsonstdout).toEqual(webpackStats);
         });
     });
 });
