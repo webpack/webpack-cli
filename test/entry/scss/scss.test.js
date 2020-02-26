f4ce2489dcfae370b82801398ab0fb09021ea6f1
emanuele <my.burning@gmail.com>
2020-02-26 12:50:26 +0000

    fix(webpack-cli): increased time out for installing deps

--- test/entry/scss/scss.test.js
+++ test/entry/scss/scss.test.js
@@ -3,11 +3,15 @@
 const { run, runInstall } = require('../../utils/test-utils');
 
 describe('entry point', () => {
-    it('should support SCSS files', async () => {
-        await runInstall();
-        const { stdout } = run(__dirname);
-        expect(stdout).toBeTruthy();
-        expect(stdout).toContain('home.css');
-        expect(stdout).toContain('home.js');
-    }, 30000);
+    it(
+        'should support SCSS files',
+        async () => {
+            await runInstall();
+            const { stdout } = run(__dirname);
+            expect(stdout).toBeTruthy();
+            expect(stdout).toContain('home.css');
+            expect(stdout).toContain('home.js');
+        },
+        1000 * 60 * 5,
+    );
 });
--- test/typescript/typescript.test.js
+++ test/typescript/typescript.test.js
@@ -5,14 +5,18 @@ const { stat } = require('fs');
 const { resolve } = require('path');
 
 describe('webpack cli', () => {
-    it('should support typescript file', async () => {
-        await runInstall(__dirname);
-        const { stderr, stdout } = run(__dirname, ['-c', './webpack.config.ts']);
-        expect(stderr).toBeFalsy();
-        expect(stdout).toBeTruthy();
-        stat(resolve(__dirname, 'bin/foo.bundle.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-        });
-    }, 30000);
+    it(
+        'should support typescript file',
+        async () => {
+            await runInstall(__dirname);
+            const { stderr, stdout } = run(__dirname, ['-c', './webpack.config.ts']);
+            expect(stderr).toBeFalsy();
+            expect(stdout).toBeTruthy();
+            stat(resolve(__dirname, 'bin/foo.bundle.js'), (err, stats) => {
+                expect(err).toBe(null);
+                expect(stats.isFile()).toBe(true);
+            });
+        },
+        1000 * 60 * 5,
+    );
 });
