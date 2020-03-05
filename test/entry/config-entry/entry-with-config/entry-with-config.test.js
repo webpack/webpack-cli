9f847008249fdc5ec4d919d4bd8ef2d68a501a01
dingchunlei <harley.mistyyyy@gmail.com>
2020-03-05 13:44:51 +0800

    chore(webpack-cli): remove the unused eslint comment and console statement

--- test/entry/config-entry/entry-with-config/entry-with-config.test.js
+++ test/entry/config-entry/entry-with-config/entry-with-config.test.js
@@ -5,14 +5,11 @@ const { run, extractSummary } = require('../../../utils/test-utils');
 
 describe('single entry with entry in config', () => {
     it('should use config entry if config entry existed', done => {
-        const { stdout, stderr } = run(__dirname, ['-c', '../1.js'], false);
+        const { stdout } = run(__dirname, ['-c', '../1.js'], false);
         const summary = extractSummary(stdout);
         const outputDir = 'entry-with-config/binary';
-        // eslint-disable-next-line quotes
         expect(summary['Output Directory']).toContain(outputDir);
 
-        // eslint-disable-next-line quotes
-        console.log(stderr);
         expect(stdout).toContain('./a.js');
         stat(resolve(__dirname, './binary/index.bundle.js'), (err, stats) => {
             expect(err).toBeFalsy();
