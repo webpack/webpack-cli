47fec11dbe85ac6bd3b0c1dbf70cdfaba0b35268
Emanuele <my.burning@gmail.com>
2020-03-11 12:05:04 +0000

    tests: fix tests (#1315)

--- test/entry/config-entry/entry-with-command/entry-with-command.test.js
+++ test/entry/config-entry/entry-with-command/entry-with-command.test.js
@@ -1,14 +1,11 @@
 'use strict';
 const { stat } = require('fs');
 const { resolve } = require('path');
-const { run, extractSummary } = require('../../../utils/test-utils');
+const { run } = require('../../../utils/test-utils');
 
 describe('config entry and command entry all exist', () => {
     it('should use command entry if config command existed', done => {
         const { stdout } = run(__dirname, ['-c', '../1.js', './index.js'], false);
-        const summary = extractSummary(stdout);
-        const outputDir = 'entry-with-command/binary';
-        expect(summary['Output Directory']).toContain(outputDir);
 
         expect(stdout).toContain('./index.js');
         stat(resolve(__dirname, './binary/main.bundle.js'), (err, stats) => {
--- test/entry/config-entry/entry-with-config/entry-with-config.test.js
+++ test/entry/config-entry/entry-with-config/entry-with-config.test.js
@@ -1,14 +1,11 @@
 'use strict';
 const { stat } = require('fs');
 const { resolve } = require('path');
-const { run, extractSummary } = require('../../../utils/test-utils');
+const { run } = require('../../../utils/test-utils');
 
 describe('default entry and config entry all exist', () => {
     it('should use config entry if config entry existed', done => {
         const { stdout } = run(__dirname, ['-c', '../1.js'], false);
-        const summary = extractSummary(stdout);
-        const outputDir = 'entry-with-config/binary';
-        expect(summary['Output Directory']).toContain(outputDir);
 
         expect(stdout).toContain('./a.js');
         stat(resolve(__dirname, './binary/index.bundle.js'), (err, stats) => {
