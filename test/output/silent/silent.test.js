8aaaccc4d14d2a433374931409007e68404f2093
Anshuman Verma <anshu.av97@gmail.com>
2020-03-15 20:58:18 +0530

    test: add tests for silent flag

---
+++ test/output/silent/silent.test.js
@@ -0,0 +1,14 @@
+const { run } = require('../../utils/test-utils');
+
+describe('Silent flag should not log anything', () => {
+    it('should not log anything when silent flag is supplied', () => {
+        const { stdout, stderr } = run(__dirname, ['--silent']);
+        expect(stdout).toBeFalsy();
+        expect(stderr).toBeFalsy();
+    });
+    it('should ignore stats flag when silent is supplied', () => {
+        const { stdout, stderr } = run(__dirname, ['--silent', '--stats', 'minimal']);
+        expect(stdout).toBeFalsy();
+        expect(stderr).toBeFalsy();
+    });
+});
