93df6c979dda617f98055b5abc2611f6d2b13a1b
Anshuman Verma <anshu.av97@gmail.com>
2020-03-15 11:44:20 +0530

    fix: folder structure of default tests

--- test/defaults/with-config/default-with-config.test.js
+++ test/defaults/with-config/default-with-config.test.js
@@ -16,4 +16,16 @@ describe('output flag defaults with config', () => {
             done();
         });
     });
+    it('should aggressively apply defaults when config flag is used', done => {
+        const { stdout, stderr } = run(__dirname, ['--defaults', '--config', './webpack.config.js'], false);
+        // Should use the output dir specified in the config
+        expect(stdout).toContain('./index.js');
+        // Should not throw because of unknown entry in config since it will pickup the default entry
+        expect(stderr).toBeFalsy();
+        stat(resolve(__dirname, './dist/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
 });
