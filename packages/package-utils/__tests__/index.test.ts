2fb08c8ce218bb925602d1c0600a790a23ad39f4
evilebottnawi <sheo13666q@gmail.com>
2020-03-25 20:01:46 +0300

    tests: skip broken tests

--- packages/package-utils/__tests__/index.test.ts
+++ packages/package-utils/__tests__/index.test.ts
@@ -5,6 +5,8 @@ jest.mock('@webpack-cli/package-utils');
 import { packageExists, promptInstallation } from '@webpack-cli/package-utils';
 import ExternalCommand from '../../webpack-cli/lib/commands/ExternalCommand';
 
+jest.setTimeout(30000);
+
 describe('@webpack-cli/package-utils', () => {
     it('should check existence of package', () => {
     	(packageExists as jest.Mock).mockImplementation(() => true);
@@ -14,6 +16,11 @@ describe('@webpack-cli/package-utils', () => {
 
     it('should not throw if the user interrupts', async () => {
     	(promptInstallation as jest.Mock).mockImplementation(() => { throw new Error() });
+
+    	const result = await ExternalCommand.run('info');
+
+    	console.log(result);
+
         await expect(ExternalCommand.run('info')).resolves.not.toThrow();
     });
 });
