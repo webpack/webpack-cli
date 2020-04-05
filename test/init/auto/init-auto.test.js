058949a1b82ef4a66f1b2fe583ee7fde543c3539
Rishabh Chawla <rishabh31121999@gmail.com>
2020-04-05 10:16:17 +0530

    chore: add init tests

---
+++ test/init/auto/init-auto.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable node/no-unpublished-require */
+'use strict';
+
+const { run } = require('../../utils/test-utils');
+const firstPrompt = 'Will your application have multiple bundles?';
+const fs = require('fs');
+const { join } = require('path');
+
+describe('init auto flag', () => {
+    it('should prompt with w/o auto flag', () => {
+        const { stdout, stderr } = run(__dirname, ['init'], false);
+        expect(stdout).toBeTruthy();
+        expect(stderr).toBeFalsy();
+        expect(stdout).toContain(firstPrompt);
+    });
+
+    it('should scaffold and not prompt with auto flag', () => {
+        const { stdout } = run(__dirname, ['init', '--auto'], false);
+        // Test no prompts are present
+        expect(stdout).toBeTruthy();
+        expect(stdout).not.toContain(firstPrompt);
+
+        // Test regressively files are scaffolded
+        const files = ['./sw.js', './package.json', './yarn.lock', './src/index.js'];
+        files.forEach(file => {
+            expect(fs.existsSync(join(__dirname, file))).toBeTruthy();
+        });
+    });
+});
