661f9a66be79d9fedf01f043f73482eb4dc74a9e
Anshuman Verma <anshu.av97@gmail.com>
2020-04-06 16:23:38 +0530

    tests: add tests for zero config

--- test/zero-config/entry-present/zero-config.test.js
+++ test/zero-config/entry-present/zero-config.test.js
@@ -4,7 +4,6 @@ describe('Zero Config tests', () => {
     it('runs when no config is supplied but entry is present', () => {
         const { stdout, stderr } = run(__dirname, [], false);
         // Should be able to find the entry file
-        expect(stdout).not.toContain("Module not found: Error: Can't resolve './src'");
         expect(stdout).toContain('./src/index.js');
         // Should output at the default output dir and filename
         expect(stdout).toContain('Entrypoint main = main.js');
