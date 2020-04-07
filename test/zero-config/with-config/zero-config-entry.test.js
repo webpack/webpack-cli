848d80ee6a67fffae8d41d78f3097ef3cd1f4614
Anshuman Verma <anshu.av97@gmail.com>
2020-04-07 14:22:13 +0530

    tests: add test for zero config (#1418)

---
+++ test/zero-config/with-config/zero-config-entry.test.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const { run } = require('../../utils/test-utils');
+
+describe('Zero Config', () => {
+    it('runs when config is present but not supplied via flag', () => {
+        const { stdout, stderr } = run(__dirname, [], false);
+        // default entry should be used
+        expect(stdout).toContain('./index.js');
+        // should pick up the output path from config
+        expect(stdout).toContain('Entrypoint main = test-output');
+        // check that the output file exists
+        expect(fs.existsSync(path.join(__dirname, '/dist/test-output.js'))).toBeTruthy();
+        expect(stderr).toBeFalsy();
+    });
+});
