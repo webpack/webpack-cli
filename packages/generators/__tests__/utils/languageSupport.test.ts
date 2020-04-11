15b01b5575d64a82100ec996911d7d30282a3f71
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-04-11 16:32:31 -0500

    tests(generators): add lang support tests for babel and ts

--- packages/generators/__tests__/utils/languageSupport.test.ts
+++ packages/generators/__tests__/utils/languageSupport.test.ts
@@ -1,6 +1,22 @@
 import language, { LangType, getBabelLoader, getTypescriptLoader } from '../../lib/utils/languageSupport';
 
 describe('languageSupport', () => {
+    const getMockGenerator: any = () => {
+        return {
+            entryOption: "'./path/to/index.js'",
+            dependencies: [],
+            configuration: {
+                config: {
+                    webpackOptions: {
+                        module: {
+                            rules: [],
+                        },
+                    },
+                },
+            },
+        };
+    };
+
     it('getBabelLoader', () => {
         const rule = getBabelLoader(['test1', 'test2']);
         expect(rule.loader).toEqual("'babel-loader'");
@@ -14,10 +30,42 @@ describe('languageSupport', () => {
     });
 
     it('works with ES6', () => {
-        expect(true).toBeTruthy();
+        const gen = getMockGenerator();
+        language(gen, LangType.ES6);
+        expect(gen.entryOption).toEqual("'./path/to/index.js'");
+        expect(gen.dependencies).toEqual(['babel-loader', '@babel/core', '@babel/preset-env']);
+        expect(gen.configuration.config.webpackOptions.module.rules.length).toEqual(1);
     });
 
-    it('works with TypeScript', () => {
-        expect(true).toBeTruthy();
+    it('works with TypeScript for single entry', () => {
+        const gen = getMockGenerator();
+        language(gen, LangType.Typescript);
+        // this helper preserves the original js entryOption but updates the
+        // webpack config to use ts
+        expect(gen.entryOption).toEqual("'./path/to/index.js'");
+        expect(gen.configuration.config.webpackOptions.entry).toEqual("'./path/to/index.ts'");
+        expect(gen.dependencies).toEqual(['typescript', 'ts-loader']);
+        expect(gen.configuration.config.webpackOptions.module.rules.length).toEqual(1);
+    });
+
+    it('works with TypeScript for multi entry', () => {
+        const gen = getMockGenerator();
+        gen.entryOption = {
+            test1: "'./path/to/test1.js'",
+            test2: "'./path/to/test2.js'",
+        };
+        language(gen, LangType.Typescript);
+        // this helper preserves the original js entryOption but updates the
+        // webpack config to use ts
+        expect(gen.entryOption).toEqual({
+            test1: "'./path/to/test1.js'",
+            test2: "'./path/to/test2.js'",
+        });
+        expect(gen.configuration.config.webpackOptions.entry).toEqual({
+            test1: "'./path/to/test1.ts'",
+            test2: "'./path/to/test2.ts'",
+        });
+        expect(gen.dependencies).toEqual(['typescript', 'ts-loader']);
+        expect(gen.configuration.config.webpackOptions.module.rules.length).toEqual(1);
     });
 });
