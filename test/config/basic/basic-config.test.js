4eccb84168c15439b2dfe22fecd4b03100a86cd5
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-04-18 15:14:29 -0500

    chore: fix basic config test

--- test/config/basic/basic-config.test.js
+++ test/config/basic/basic-config.test.js
@@ -5,7 +5,11 @@ const { run } = require('../../utils/test-utils');
 
 describe('basic config file', () => {
     it('is able to understand and parse a very basic configuration file', (done) => {
-        const { stdout, stderr } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js'), '--output', './binary/a.bundle.js']);
+        const { stdout, stderr } = run(
+            __dirname,
+            ['-c', resolve(__dirname, 'webpack.config.js'), '--output', './binary/a.bundle.js'],
+            false,
+        );
         expect(stderr).toBeFalsy();
         expect(stdout).not.toBe(undefined);
         stat(resolve(__dirname, './binary/a.bundle.js'), (err, stats) => {
