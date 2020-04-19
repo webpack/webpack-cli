54aa9b8ae83e720bf3ba46ed97b0c9012637c7d9
Aman Kumar <aman29271@gmail.com>
2020-04-19 18:51:38 +0530

    tests: test added for display verbose

--- test/display/single-config/single-config-display.test.js
+++ test/display/single-config/single-config-display.test.js
@@ -2,8 +2,7 @@
 
 const { run } = require('../../utils/test-utils');
 const PROGRESS_TEXT = 'building';
-const STATS_VERBOSE_SINGLE_ENTRY_TEXT = 'single entry';
-const STATS_VERBOSE_MULTIPLE_ENTRY_TEXT = 'multi entry';
+const STATS_VERBOSE_TEXT = 'LOG from webpack.buildChunkGraph';
 
 describe('single config display test', () => {
     it('should display profiling information', () => {
@@ -14,13 +13,13 @@ describe('single config display test', () => {
 
     it('should display single entry information in stats', () => {
         const { stdout, stderr } = run(__dirname, ['--config', 'webpack.config.single.js', '--stats', 'verbose']);
-        expect(stdout).toContain(STATS_VERBOSE_SINGLE_ENTRY_TEXT);
+        expect(stdout).toContain(STATS_VERBOSE_TEXT);
         expect(stderr).toBeFalsy();
     });
 
     it('should display multiple entry information in stats', () => {
         const { stdout, stderr } = run(__dirname, ['--config', 'webpack.config.multiple.js', '--stats', 'verbose']);
-        expect(stdout).toContain(STATS_VERBOSE_MULTIPLE_ENTRY_TEXT);
+        expect(stdout).toContain(STATS_VERBOSE_TEXT);
         expect(stderr).toBeFalsy();
     });
 });
