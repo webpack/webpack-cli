3bdc601a84c2b5e3efced6b451fdcea40f561a1c
James George <jamesgeorge998001@gmail.com>
2020-04-20 18:22:21 +0530

    test(generators): Add test case for production CSS config (#1473)

--- packages/generators/__tests__/utils/styleSupport.test.ts
+++ packages/generators/__tests__/utils/styleSupport.test.ts
@@ -22,18 +22,28 @@ describe('styleSupport', () => {
         expect(ExtractUseProps.length).toEqual(2);
     });
 
+    it('generates production CSS configuration', () => {
+        const gen = getMockGenerator();
+        gen.isProd = true;
+        const { ExtractUseProps, regExpForStyles } = style(gen, StylingType.CSS);
+        expect(gen.dependencies).toEqual(['css-loader']);
+        expect(regExpForStyles).toEqual('/.css$/');
+        expect(ExtractUseProps.length).toEqual(1);
+    });
+
     it('generates SASS configuration', () => {
         const gen = getMockGenerator();
         const { ExtractUseProps, regExpForStyles } = style(gen, StylingType.SASS);
-        expect(gen.dependencies).toEqual(['node-sass', 'sass-loader', 'css-loader', 'style-loader']);
         expect(regExpForStyles).toEqual('/.(scss|css)$/');
+        expect(gen.dependencies).toEqual(['node-sass', 'sass-loader', 'css-loader', 'style-loader']);
         expect(ExtractUseProps.length).toEqual(3);
     });
 
     it('generates production SASS configuration', () => {
         const gen = getMockGenerator();
         gen.isProd = true;
-        const { ExtractUseProps } = style(gen, StylingType.SASS);
+        const { ExtractUseProps, regExpForStyles } = style(gen, StylingType.SASS);
+        expect(regExpForStyles).toEqual('/.(scss|css)$/');
         expect(gen.dependencies).toEqual(['node-sass', 'sass-loader', 'css-loader']);
         expect(ExtractUseProps.length).toEqual(2);
     });
@@ -49,7 +59,8 @@ describe('styleSupport', () => {
     it('generates production LESS configuration', () => {
         const gen = getMockGenerator();
         gen.isProd = true;
-        const { ExtractUseProps } = style(gen, StylingType.LESS);
+        const { ExtractUseProps, regExpForStyles } = style(gen, StylingType.LESS);
+        expect(regExpForStyles).toEqual('/.(less|css)$/');
         expect(gen.dependencies).toEqual(['less', 'less-loader', 'css-loader']);
         expect(ExtractUseProps.length).toEqual(2);
     });
@@ -67,8 +78,9 @@ describe('styleSupport', () => {
     it('generates production PostCSS configuration', () => {
         const gen = getMockGenerator();
         gen.isProd = true;
-        const { ExtractUseProps } = style(gen, StylingType.PostCSS);
+        const { ExtractUseProps, regExpForStyles } = style(gen, StylingType.PostCSS);
         expect(gen.dependencies).toEqual(['precss', 'autoprefixer', 'css-loader', 'postcss-loader']);
+        expect(regExpForStyles).toEqual('/.css$/');
         expect(ExtractUseProps.length).toEqual(2);
     });
 });
