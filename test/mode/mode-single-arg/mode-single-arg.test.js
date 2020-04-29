276c95c1e2e73107d80234c353cb5ca05d8e7c98
Nitin Kumar <snitin315@gmail.com>
2020-04-29 20:59:12 +0530

    tests: add test for mode value none

--- test/mode/mode-single-arg/mode-single-arg.test.js
+++ test/mode/mode-single-arg/mode-single-arg.test.js
@@ -1,51 +1,56 @@
 'use strict';
 const { run } = require('../../utils/test-utils');
-const { stat } = require('fs');
+const { stat, readFile } = require('fs');
 const { resolve } = require('path');
 describe('mode flags', () => {
-    it('should load a production config when --prod is passed', (done) => {
-        const { stderr, stdout } = run(__dirname, ['--prod']);
-        expect(stderr).toBeFalsy();
-        expect(stdout).toBeTruthy();
-        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-            done();
-        });
-    });
-
-    it('should load a production config when --mode=production and --prod are passed', (done) => {
-        const { stderr, stdout } = run(__dirname, ['--mode', 'production', '--prod']);
-        expect(stderr).toContain('"mode" will be used');
-        expect(stdout).toBeTruthy();
-
-        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-            done();
-        });
-    });
-
-    it('should load a production config when --mode=production and --dev are passed', (done) => {
-        const { stderr, stdout } = run(__dirname, ['--mode', 'production', '--dev']);
-        expect(stderr).toContain('"mode" will be used');
-        expect(stdout).toBeTruthy();
-
-        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-            done();
-        });
-    });
-
-    it('should load a production config when passing --dev and --prod', (done) => {
-        const { stderr, stdout } = run(__dirname, ['--prod', '--dev']);
+    it('should load a development config when --mode=development is passed', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--mode', 'development']);
         expect(stderr).toBeFalsy();
         expect(stdout).toBeTruthy();
 
         stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+
+    it('should load a production config when --mode=productuon is passed', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--mode', 'production']);
+        expect(stderr).toBeFalsy();
+        expect(stdout).toBeTruthy();
+
+        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+
+    it('should load a none config when --mode=none is passed', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--mode', 'none']);
+        expect(stderr).toBeFalsy();
+        expect(stdout).toBeTruthy();
+
+        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+    });
+
+    it('should show warning and load a production config when --mode=abcd is passed', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--mode', 'abcd']);
+        expect(stderr).toContain('invalid value for "mode" option. Using "production" by default');
+        expect(stdout).toBeTruthy();
+        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+        readFile(resolve(__dirname, './bin/main.js'), 'utf-8', (err, data) => {
+            expect(err).toBe(null);
+            expect(data).toContain('production test');
             done();
         });
     });
