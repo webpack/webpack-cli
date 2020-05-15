f6a7a61b8674efc3b3b05cfac07df89f02be9ae2
Rishabh Chawla <rishabh31121999@gmail.com>
2020-05-15 14:40:26 +0530

    chore: debug tests

--- test/init/multipleEntries/init-multipleEntries.test.js
+++ test/init/multipleEntries/init-multipleEntries.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const fs = require('fs');
-const { join, resolve } = require('path');
+const path = require('path');
 const rimraf = require('rimraf');
 const { runPromptWithAnswers } = require('../../utils/test-utils');
 const firstPrompt = 'Will your application have multiple bundles?';
 
 const ENTER = '\x0D';
-const genPath = join(__dirname, 'test-assets');
+const genPath = path.join(__dirname, 'test-assets');
 
 jest.setTimeout(100000);
 
@@ -22,19 +22,20 @@ describe('init with multiple entries', () => {
     });
 
     it('should scaffold with multiple entries', async () => {
-        const { stdout } = await runPromptWithAnswers(genPath, ['init'], [`Y${ENTER}`, `a, b${ENTER}`, ENTER, ENTER, ENTER, ENTER]);
+        const { stdout } = await runPromptWithAnswers(
+            genPath,
+            ['init'],
+            [`Y${ENTER}`, `a, b${ENTER}`, ENTER, ENTER, ENTER, ENTER, ENTER, ENTER],
+        );
 
         expect(stdout).toBeTruthy();
         expect(stdout).toContain(firstPrompt);
 
         // Test regressively files are scaffolded
         const files = ['./package.json', './src/a.js', './src/b.js', './yarn.lock', './.yo-rc.json'];
-        console.log(fs.readdirSync(genPath));
-
         // eslint-disable-next-line prettier/prettier
         files.forEach((file) => {
-            console.log(file);
-            expect(fs.existsSync(resolve(genPath, file))).toBeTruthy();
+            expect(fs.existsSync(path.resolve(genPath, file))).toBeTruthy();
         });
     });
 });
