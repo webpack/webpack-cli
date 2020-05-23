f74c3e5ec350b78d8047268d6d2e9ee87646c841
Nitin Kumar <46647141+snitin315@users.noreply.github.com>
2020-05-23 17:32:19 +0530

    tests: add cases for json and verbose (#1573)
    Co-authored-by: Rishabh Chawla <rishabh31121999@gmail.com>

--- packages/webpack-cli/__tests__/StatsGroup.test.js
+++ packages/webpack-cli/__tests__/StatsGroup.test.js
@@ -15,4 +15,26 @@ describe('StatsGroup', function () {
             });
         });
     }
+
+    it('should handle verbose', () => {
+        const group = new StatsGroup([
+            {
+                verbose: true,
+            },
+        ]);
+
+        const result = group.run();
+        expect(result.options.stats).toEqual('verbose');
+    });
+
+    it('should handle json', () => {
+        const group = new StatsGroup([
+            {
+                json: true,
+            },
+        ]);
+
+        const result = group.run();
+        expect(result.outputOptions.json).toBeTruthy();
+    });
 });
