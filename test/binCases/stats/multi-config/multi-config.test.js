9fa7cf1dd5158bc6a5f11e0d9439c20558c5ea0e
Aman Kumar <aman29271@gmail.com>
2020-05-27 23:20:32 +0530

    fix: multiple entry verbose

--- test/binCases/stats/multi-config/multi-config.test.js
+++ test/binCases/stats/multi-config/multi-config.test.js
@@ -11,22 +11,3 @@ test("multi-config", () => {
 	expect(summary).toEqual(expect.anything());
 	expect(stderr).toHaveLength(0);
 });
-test("multiple-config with --info-verbosity=verbose", () => {
-	const { code, stdout, stderr } = run(__dirname, [
-		"--config",
-		"./webpack.config.verbose.js",
-		"--info-verbosity",
-		"verbose",
-		"--mode",
-		"production"
-	]);
-	const summary = extractSummary(stdout);
-	expect(code).toBe(0);
-	expect(code).not.toBeNull();
-	expect(code).toBeDefined();
-	expect(stderr).toContain("Compilation  starting...");
-	expect(summary).toContain("index.js");
-	expect(summary).toContain("index2.js");
-	expect(stderr).toBeDefined();
-	expect(stderr).not.toBeNull();
-});
