af555845938292792d896148c5a3077a6e46ca01
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-06-24 13:25:51 -0500

    fix(serve): add merge handling for client object

--- packages/serve/src/getDevServerOptions.ts
+++ packages/serve/src/getDevServerOptions.ts
@@ -9,6 +9,8 @@
 export default function getDevServerOptions(compiler): any {
     let devServerOptions;
     if (compiler.compilers) {
+        // devServer options could be found in any of the compilers,
+        // so simply find the first instance and use it, if there is one
         const comp = compiler.compilers.find((comp) => comp.options.devServer);
         if (comp) {
             devServerOptions = comp.options.devServer;
--- packages/serve/src/mergeOptions.ts
+++ packages/serve/src/mergeOptions.ts
@@ -9,5 +9,13 @@
 export default function mergeOptions(cliOptions, devServerOptions): any {
     // CLI options should take precedence over devServer options,
     // and CLI options should have no default values included
-    return { ...devServerOptions, ...cliOptions };
+    const options = { ...devServerOptions, ...cliOptions };
+
+    if (devServerOptions.client && cliOptions.client) {
+        // the user could set some client options in their devServer config,
+        // then also specify client options on the CLI
+        options.client = { ...devServerOptions.client, ...cliOptions.client };
+    }
+
+    return options;
 }
