0c80e9ff7774be92509366f8e71bbe7b1fb426c0
Rishabh Chawla <rishabh31121999@gmail.com>
2020-06-26 22:12:05 +0530

    chore: update tests

--- test/stats/config/stats.test.js
+++ test/stats/config/stats.test.js
@@ -1,16 +1,26 @@
+/* eslint-disable node/no-extraneous-require */
 'use strict';
 // eslint-disable-next-line node/no-unpublished-require
 const { run } = require('../../utils/test-utils');
+const { version } = require('webpack');
 
 describe('stats flag with config', () => {
     it('should compile without stats flag', () => {
         const { stderr, stdout } = run(__dirname, []);
         expect(stderr).toBeFalsy();
-        expect(stdout).toContain(`stats: 'normal'`);
+        if (version.startsWith('5')) {
+            expect(stdout).toContain(`stats: { preset: 'normal' }`);
+        } else {
+            expect(stdout).toContain(`stats: 'normal'`);
+        }
     });
     it('should compile with stats flag', () => {
         const { stderr, stdout } = run(__dirname, ['--stats', 'errors-warnings']);
         expect(stderr).toBeFalsy();
-        expect(stdout).toContain(`stats: 'errors-warnings'`);
+        if (version.startsWith('5')) {
+            expect(stdout).toContain(`stats: { preset: 'errors-warnings' }`);
+        } else {
+            expect(stdout).toContain(`stats: 'errors-warnings'`);
+        }
     });
 });
