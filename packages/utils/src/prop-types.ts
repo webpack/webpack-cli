223b3eb2a9e50bf2a42ad6f862137b437a55d6cf
Anshuman Verma <anshu.av97@gmail.com>
2020-06-27 20:41:45 +0530

    chore: remove hardcoded config options from utils, use from core

--- packages/utils/src/prop-types.ts
+++ packages/utils/src/prop-types.ts
@@ -1,38 +1,10 @@
+import { config } from 'webpack';
+const configKeys = Object.keys(config.getNormalizedWebpackOptions({}));
+
 /**
  *
  * A Set of all accepted properties
  *
  * @returns {Set} A new set with accepted webpack properties
  */
-
-export const PROP_TYPES: Set<string> = new Set([
-    'amd',
-    'bail',
-    'cache',
-    'context',
-    'devServer',
-    'devtool',
-    'entry',
-    'externals',
-    'merge',
-    'mode',
-    'module',
-    'node',
-    'optimization',
-    'output',
-    'parallelism',
-    'performance',
-    'plugins',
-    'profile',
-    'recordsInputPath',
-    'recordsOutputPath',
-    'recordsPath',
-    'resolve',
-    'resolveLoader',
-    'splitChunks',
-    'stats',
-    'target',
-    'topScope',
-    'watch',
-    'watchOptions',
-]);
+export const PROP_TYPES: Set<string> = new Set(configKeys);
