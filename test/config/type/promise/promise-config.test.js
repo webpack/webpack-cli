5b655084c4ff4709df35e10008d1383d813566d0
Anshuman Verma <anshu.av97@gmail.com>
2020-07-04 00:28:38 +0530

    feat: promise support in config

--- test/config/type/promise/promise-config.test.js
+++ test/config/type/promise/promise-config.test.js
@@ -1,18 +1,13 @@
 'use strict';
 const { stat } = require('fs');
-const { resolve, sep } = require('path');
-const { run, extractSummary } = require('../../../utils/test-utils');
+const { resolve } = require('path');
+const { run } = require('../../../utils/test-utils');
 
 describe('promise configuration', () => {
-    it.skip('is able to understand a configuration file as a promise', (done) => {
-        const { stdout } = run(__dirname, ['-c', resolve(__dirname, 'webpack.config.js')], false);
-        const summary = extractSummary(stdout);
-        const outputDir = 'type/promise/binary';
-        const outDirectoryFromCompiler = summary['Output Directory'].split(sep);
-        const outDirToMatch = outDirectoryFromCompiler
-            .slice(outDirectoryFromCompiler.length - 3, outDirectoryFromCompiler.length)
-            .join('/');
-        expect(outDirToMatch).toContain(outputDir);
+    it('is able to understand a configuration file as a promise', (done) => {
+        const { stdout, stderr } = run(__dirname, ['-c', './webpack.config.js'], false);
+        expect(stdout).toBeTruthy();
+        expect(stderr).toBeFalsy();
         stat(resolve(__dirname, './binary/promise.js'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isFile()).toBe(true);
