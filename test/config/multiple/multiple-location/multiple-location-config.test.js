98056bce1c19b076390e3193c2e7ce2e729f5e24
Anshuman Verma <anshu.av97@gmail.com>
2020-07-06 21:36:56 +0530

    chore: rm redundant test dir cleanup, rm test level gitignore and handle at root (#1669)
    * chore: remove test level gitignores, handle in root
    
    * chore: rm redundant test dir removals
    
    * chore: rm redundant test dir removals
    
    Co-authored-by: James George <jamesgeorge998001@gmail.com>

--- test/config/multiple/multiple-location/multiple-location-config.test.js
+++ test/config/multiple/multiple-location/multiple-location-config.test.js
@@ -1,15 +1,9 @@
 'use strict';
 const { stat } = require('fs');
-const { resolve, join } = require('path');
+const { resolve } = require('path');
 const { run } = require('../../../utils/test-utils');
-const rimraf = require('rimraf');
-
-const outputPath = join(__dirname, 'binary');
 
 describe('multiple dev config files with webpack.config.js', () => {
-    afterEach(() => rimraf.sync(outputPath));
-    beforeAll(() => rimraf.sync(outputPath));
-
     it('Uses webpack.config.development.js', (done) => {
         const { stdout, stderr } = run(__dirname, [], false);
         expect(stderr).toBeFalsy();
--- test/config/multiple/with-mode/multiple-config.test.js
+++ test/config/multiple/with-mode/multiple-config.test.js
@@ -1,15 +1,9 @@
 'use strict';
 const { stat } = require('fs');
-const { resolve, join } = require('path');
+const { resolve } = require('path');
 const { run } = require('../../../utils/test-utils');
-const rimraf = require('rimraf');
-
-const outputPath = join(__dirname, 'binary');
 
 describe('multiple config files', () => {
-    afterEach(() => rimraf.sync(outputPath));
-    beforeAll(() => rimraf.sync(outputPath));
-
     it('Uses dev config when development mode is supplied', (done) => {
         const { stdout, stderr } = run(__dirname, ['--mode', 'development'], false);
         expect(stderr).toBeFalsy();
