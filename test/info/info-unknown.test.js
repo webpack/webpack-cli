ba17e2d07b61e4e72e19dd641e2128849851e6cb
James George <jamesgeorge998001@gmail.com>
2020-07-13 19:15:02 +0530

    chore: handle unknown args and improve test suite for info (#1676)
    * feat: handle unknown args with info subcommand
    
    * chore: recognize help and version args
    
    * chore: improve test suite

---
+++ test/info/info-unknown.test.js
@@ -0,0 +1,8 @@
+const { runInfo } = require('../utils/test-utils');
+
+describe('should handle unknown args', () => {
+    it('shows an appropriate warning on supplying unknown args', () => {
+        const { stderr } = runInfo(['--unknown'], __dirname);
+        expect(stderr).toContain('Unknown argument: --unknown');
+    });
+});
