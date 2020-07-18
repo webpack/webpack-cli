d14b1e4141aa524ca4c38df881e4548c424cabba
Anshuman Verma <anshu.av97@gmail.com>
2020-07-18 12:15:18 +0530

    feat: add MergeError class

--- test/merge/config/merge-config.test.js
+++ test/merge/config/merge-config.test.js
@@ -17,7 +17,7 @@ describe('merge flag configuration', () => {
     });
     it('merges two configurations together with flag alias', (done) => {
         const { stdout } = run(__dirname, ['--config', './1.js', '-m', './2.js'], false);
-        expect(stdout).toContain('option has not been set, webpack will fallback to');
+        expect(stdout).toContain('merged.js');
         stat(resolve(__dirname, './dist/merged.js'), (err, stats) => {
             expect(err).toBe(null);
             expect(stats.isFile()).toBe(true);
