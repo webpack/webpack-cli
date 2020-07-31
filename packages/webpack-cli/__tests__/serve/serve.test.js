3a503037159df7ac9fc4610b6c200c5ad25f5149
Rishabh Chawla <rishabh31121999@gmail.com>
2020-07-31 11:37:43 +0530

    tests: add basic integartion tests

---
+++ packages/webpack-cli/__tests__/serve/serve.test.js
@@ -0,0 +1,24 @@
+const { sync: spawnSync } = require('execa');
+const path = require('path');
+
+describe('Serve', () => {
+    it('should run with cli', () => {
+        const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../bin/cli.js'), ['serve'], {
+            cwd: path.resolve(__dirname),
+            reject: false,
+        });
+        expect(stdout).toContain('main.js');
+        expect(stdout).not.toContain('hot/dev-server.js');
+        expect(stderr).toHaveLength(0);
+    });
+
+    it('should work with flags', () => {
+        const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../bin/cli.js'), ['serve', '--hot'], {
+            cwd: path.resolve(__dirname),
+            reject: false,
+        });
+        expect(stdout).toContain('main.js');
+        expect(stdout).toContain('hot/dev-server.js');
+        expect(stderr).toHaveLength(0);
+    });
+});
