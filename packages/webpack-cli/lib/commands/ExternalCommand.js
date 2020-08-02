f600956be6b949125b95dd0cff488141df81e285
Rishabh Chawla <rishabh31121999@gmail.com>
2020-08-02 22:15:52 +0530

    chore: check dev-server

--- packages/webpack-cli/lib/commands/ExternalCommand.js
+++ packages/webpack-cli/lib/commands/ExternalCommand.js
@@ -34,6 +34,14 @@ class ExternalCommand {
                 logger.error(`Action Interrupted, use ${chalk.cyan('webpack-cli help')} to see possible commands.`);
             }
         }
+        // Check dev server availability before running serve 
+        if (name === 'serve') {
+            try { 
+                require.resolve('webpack-dev-server') ;
+            } catch (error) {
+                throw new Error(`You need to install 'webpack-dev-server' for running 'webpack serve'.\n${error}`);
+            }              
+        }
         return pkgLoc ? require(scopeName).default(...args) : null;
     }
 }
