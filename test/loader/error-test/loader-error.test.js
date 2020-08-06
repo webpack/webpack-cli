cc523b7daeb8b0ba48215c3a5fe812e6b2d9acb7
Nitin Kumar <snitin315@gmail.com>
2020-08-06 16:26:54 +0530

    tests: loader's error regression test for #1581

---
+++ test/loader/error-test/loader-error.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// eslint-disable-next-line node/no-unpublished-require
+const { run } = require('../../utils/test-utils');
+
+describe('loader error regression test for #1581', () => {
+    it(`should not ignore loader's error produce a failing build`, () => {
+        // Ignoring assertion on stderr because ts-loader is producing depreciation warnings
+        // with webpack@v5.0.0-beta.24 -> https://github.com/TypeStrong/ts-loader/issues/1169
+        const { stdout } = run(__dirname, [], false);
+
+        expect(stdout).toContain('[1 error]');
+        expect(stdout).toContain(`Cannot assign to 'foobar' because it is a constant`);
+    });
+});
