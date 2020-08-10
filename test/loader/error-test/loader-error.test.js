8611c6a4151e426b3e50750870529ba60f71b764
Nitin Kumar <snitin315@gmail.com>
2020-08-10 18:22:55 +0530

    tests: add test on error code

--- test/loader/error-test/loader-error.test.js
+++ test/loader/error-test/loader-error.test.js
@@ -7,8 +7,8 @@ describe('loader error regression test for #1581', () => {
     it(`should not ignore loader's error produce a failing build`, () => {
         // Ignoring assertion on stderr because ts-loader is producing depreciation warnings
         // with webpack@v5.0.0-beta.24 -> https://github.com/TypeStrong/ts-loader/issues/1169
-        const { stdout } = run(__dirname, [], false);
-
+        const { stdout, exitCode } = run(__dirname, [], false);
+        expect(exitCode).not.toEqual(0);
         expect(stdout).toContain('[1 error]');
         expect(stdout).toContain(`Cannot assign to 'foobar' because it is a constant`);
     });
