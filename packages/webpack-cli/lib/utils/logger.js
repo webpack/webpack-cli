0ab7b500527339ef367ea81304d598fecd4cc67b
Nitin Kumar <snitin315@gmail.com>
2020-08-12 19:45:25 +0530

    refactor: use console for logging (#1740)

--- packages/webpack-cli/lib/utils/logger.js
+++ packages/webpack-cli/lib/utils/logger.js
@@ -1,3 +1,9 @@
-const { getLogger } = require('webpack/lib/logging/runtime');
+const { red, cyan, yellow } = require('colorette');
 
-module.exports = getLogger('webpack-cli');
+module.exports = {
+    error: (val) => console.error(`[webpack-cli] ${red(val)}`),
+    warn: (val) => console.warn(`[webpack-cli] ${yellow(val)}`),
+    info: (val) => console.info(`[webpack-cli] ${cyan(val)}`),
+    log: (val) => console.log(`[webpack-cli] ${val}`),
+    help: (val) => console.log(val),
+};
