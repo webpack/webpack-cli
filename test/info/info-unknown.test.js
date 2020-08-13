f380f0b48512d55e9f2b0bab1b735335e8ab1f73
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-08-13 17:24:11 +0530

    tests: fix failing test suite

--- test/info/info-unknown.test.js
+++ test/info/info-unknown.test.js
@@ -4,6 +4,6 @@ const { runInfo } = require('../utils/test-utils');
 describe('should handle unknown args', () => {
     it('shows an appropriate warning on supplying unknown args', () => {
         const { stderr } = runInfo(['--unknown'], __dirname);
-        expect(stderr).toContain(red('[webpack-cli] Unknown argument: --unknown'));
+        expect(stderr).toContain(`[webpack-cli] ${red('Unknown argument: --unknown')}`);
     });
 });
