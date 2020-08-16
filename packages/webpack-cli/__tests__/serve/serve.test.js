5534914d70ebb8d25d226b57fb98222ab5cd080a
Rishabh Chawla <rishabh31121999@gmail.com>
2020-08-16 17:30:18 +0530

    chore: update tests

--- packages/webpack-cli/__tests__/serve/serve.test.js
+++ packages/webpack-cli/__tests__/serve/serve.test.js
@@ -1,26 +1,15 @@
-const { sync: spawnSync } = require('execa');
-const path = require('path');
-jest.setTimeout(10);
-describe('Serve', () => {
-    it('should run with cli', () => {
-        const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../../bin/cli.js'), ['serve'], {
-            cwd: path.resolve(__dirname),
-            reject: false,
-            timeout: 4000,
-        });
+const { runServe } = require('../../../../test/utils/test-utils');
 
+describe('Serve', () => {
+    it('should run with cli', async () => {
+        const { stdout, stderr } = await runServe([], __dirname);
         expect(stdout).toContain('main.js');
         expect(stdout).not.toContain('hot/dev-server.js');
         expect(stderr).toHaveLength(0);
     });
 
-    it('should work with flags', () => {
-        const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../../bin/cli.js'), ['serve', '--hot'], {
-            cwd: path.resolve(__dirname),
-            reject: false,
-            timeout: 4000,
-        });
-
+    it('should work with flags', async () => {
+        const { stdout, stderr } = await runServe(['--hot'], __dirname);
         expect(stdout).toContain('main.js');
         expect(stdout).toContain('hot/dev-server.js');
         expect(stderr).toHaveLength(0);
