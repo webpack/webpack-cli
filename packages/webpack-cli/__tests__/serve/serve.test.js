91859042fa1fa326939b814bddb956c7df7c615a
Rishabh Chawla <rishabh31121999@gmail.com>
2020-08-16 16:14:25 +0530

    chore: add integration test

--- packages/webpack-cli/__tests__/serve/serve.test.js
+++ packages/webpack-cli/__tests__/serve/serve.test.js
@@ -1,12 +1,14 @@
 const { sync: spawnSync } = require('execa');
 const path = require('path');
-
+jest.setTimeout(10);
 describe('Serve', () => {
     it('should run with cli', () => {
         const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../../bin/cli.js'), ['serve'], {
             cwd: path.resolve(__dirname),
             reject: false,
+            timeout: 4000,
         });
+
         expect(stdout).toContain('main.js');
         expect(stdout).not.toContain('hot/dev-server.js');
         expect(stderr).toHaveLength(0);
@@ -16,7 +18,9 @@ describe('Serve', () => {
         const { stdout, stderr } = spawnSync(path.resolve(__dirname, '../../bin/cli.js'), ['serve', '--hot'], {
             cwd: path.resolve(__dirname),
             reject: false,
+            timeout: 4000,
         });
+
         expect(stdout).toContain('main.js');
         expect(stdout).toContain('hot/dev-server.js');
         expect(stderr).toHaveLength(0);
