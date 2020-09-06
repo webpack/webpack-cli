6a10efd8bf01719f4f03f39fad6f3d2ea755b24e
Nitin Kumar <snitin315@gmail.com>
2020-09-06 07:48:30 +0530

    tests: for snapshot config related flags

---
+++ test/core-flags/snapshot-flags.test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { run, hyphenToUpperCase } = require('../utils/test-utils');
+const { flagsFromCore } = require('../../packages/webpack-cli/lib/utils/cli-flags');
+
+const snapshotFlags = flagsFromCore.filter(({ name }) => name.startsWith('snapshot'));
+
+describe('snapshot config related flags', () => {
+    snapshotFlags.forEach((flag) => {
+        // extract property name from flag name
+        let property = flag.name.split('snapshot-')[1];
+
+        const propName = hyphenToUpperCase(property);
+
+        if (flag.type === Boolean) {
+            it(`should config --${flag.name} correctly`, () => {
+                const { stderr, stdout } = run(__dirname, [`--${flag.name}`]);
+
+                expect(stderr).toBeFalsy();
+                if (flag.name.includes('reset')) {
+                    const option = propName.split('Reset')[0];
+                    expect(stdout).toContain(`${option}: []`);
+                } else if (flag.name.includes('timestamp')) {
+                    expect(stdout).toContain(`timestamp: true`);
+                } else if (flag.name.includes('hash')) {
+                    expect(stdout).toContain(`hash: true`);
+                }
+            });
+        }
+
+        if (flag.type === String) {
+            it(`should config --${flag.name} correctly`, () => {
+                const { stderr, stdout } = run(__dirname, [`--${flag.name}`, 'test-snap-path']);
+                expect(stderr).toBeFalsy();
+                expect(stdout).toContain('test-snap-path');
+            });
+        }
+    });
+});
