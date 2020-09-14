bad06120ad6cb80b2cd8eadc17c63b7216a2dbfb
Nitin Kumar <snitin315@gmail.com>
2020-09-14 21:18:54 +0530

    tests: update

--- test/config/defaults/basic-config/default-js-config.test.js
+++ test/config/defaults/basic-config/default-js-config.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { run } = require('../../../utils/test-utils');
+const { run, isWebpack5 } = require('../../../utils/test-utils');
 
 describe('Zero Config', () => {
     it('runs when config is present but not supplied via flag', () => {
@@ -9,10 +9,12 @@ describe('Zero Config', () => {
         expect(stdout).toContain('./index.js');
         // should pick up the output path from config
         expect(stdout).toContain('test-output');
-        expect(stdout).toContain('Hash');
-        expect(stdout).toContain('Version');
-        expect(stdout).toContain('Built at');
-        expect(stdout).toContain('Time');
+        if (!isWebpack5) {
+            expect(stdout).toContain('Hash');
+            expect(stdout).toContain('Version');
+            expect(stdout).toContain('Built at');
+            expect(stdout).toContain('Time');
+        }
         // Should return the correct exit code
         expect(exitCode).toEqual(0);
         // check that the output file exists
