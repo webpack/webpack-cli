facf4407c9bc870a80e6493566395bc5e5de936e
Rishabh Chawla <rishabh31121999@gmail.com>
2020-09-16 16:47:51 +0530

    feat: add command space

--- packages/webpack-cli/lib/utils/InteractiveModePlugin.js
+++ packages/webpack-cli/lib/utils/InteractiveModePlugin.js
@@ -1,14 +1,17 @@
 const readline = require('readline');
+
 class InteractiveModePlugin {
     constructor() {
+        this.name = 'InteractiveModePlugin';
         this.keys = {
             quit: 'q',
             recompile: '\n',
+            pause: 'p',
         };
-
         this.handlers = {
             quit: this.quitHandler,
             recompile: this.recompileHandler,
+            pause: this.pauseHandler,
         };
     }
 
@@ -16,13 +19,11 @@ class InteractiveModePlugin {
         // Configure stdin for keypress event
         const stdin = process.stdin;
         stdin.setEncoding('utf-8');
-        stdin.setRawMode(true);
         readline.emitKeypressEvents(stdin);
 
         // Configure keypress event for actions
         const actions = Object.keys(this.keys);
         stdin.on('keypress', (_, actionKey) => {
-            process.stdout.write(actionKey.name + '\n');
             const possibleActions = actions.filter((key) => {
                 return this.keys[key] === actionKey.name;
             });
@@ -40,8 +41,16 @@ class InteractiveModePlugin {
         });
 
         // Clear output afterCompile
-        compiler.hooks.afterCompile.tap('InteractiveModePlugin', () => {
+        compiler.hooks.afterCompile.tap(this.name, () => {
             process.stdout.write('\x1B[2J\x1B[3J\x1B[H');
+        });
+
+        compiler.hooks.done.tap(this.name, () => {
+            setTimeout(() => {
+                const totalRows = process.stdout.rows;
+                readline.cursorTo(process.stdout, 0, totalRows - 1);
+                process.stdout.write('> ');
+            }, 100);
         });
     }
 
@@ -52,6 +61,11 @@ class InteractiveModePlugin {
 
     // eslint-disable-next-line no-unused-vars
     recompileHandler(compiler) {
+        // TODO: implement it
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    pauseHandler(compiler) {
         // TODO: implement it
     }
 }
