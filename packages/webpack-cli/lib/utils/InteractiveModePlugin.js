b6cb983646922b9988e0ac97822f7e9cb4e1177b
Rishabh Chawla <rishabh31121999@gmail.com>
2020-09-23 11:09:38 +0530

    feat: add support for info line

--- packages/webpack-cli/lib/utils/InteractiveModePlugin.js
+++ packages/webpack-cli/lib/utils/InteractiveModePlugin.js
@@ -1,5 +1,19 @@
 const readline = require('readline');
 const logger = require('./logger');
+const { version } = require('webpack');
+
+const spawnCommand = (msg) => {
+    const lines = 3;
+    const totalRows = process.stdout.rows;
+    readline.cursorTo(process.stdout, 0, totalRows - lines);
+    readline.clearScreenDown(process.stdout);
+
+    logger.log(msg);
+    process.stdout.write('\nq: quit, s: stop, r: recompile, p: pause');
+
+    readline.cursorTo(process.stdout, 0, totalRows - 2);
+    process.stdout.write('➜ ');
+};
 
 class InteractiveModePlugin {
     constructor() {
@@ -23,15 +37,12 @@ class InteractiveModePlugin {
         const stdin = process.stdin;
         stdin.setEncoding('utf-8');
         readline.emitKeypressEvents(stdin);
-        if (process.stdout.isTTY) {
-            stdin.setRawMode(true);
-        }
 
         // Configure keypress event for actions
         const actions = Object.keys(this.keys);
         stdin.on('keypress', (_, actionKey) => {
-            const possibleActions = actions.filter((key) => {
-                return this.keys[key] === actionKey.name;
+            const possibleActions = actions.filter((action) => {
+                return this.keys[action] === actionKey.name;
             });
 
             if (possibleActions.length === 0) {
@@ -46,16 +57,25 @@ class InteractiveModePlugin {
             this.handlers[action](compiler);
         });
 
+        // Clear for first run as well
+        process.stdout.write('\x1B[2J\x1B[3J\x1B[H');
+
         // Clear output on watch invalidate
         compiler.hooks.invalid.tap(this.name, () => {
             process.stdout.write('\x1B[2J\x1B[3J\x1B[H');
         });
 
-        compiler.hooks.afterDone.tap(this.name, () => {
-            const totalRows = process.stdout.rows;
-            readline.cursorTo(process.stdout, 0, totalRows - 1);
-            process.stdout.write('> ');
-        });
+        if (version.startsWith('5')) {
+            compiler.hooks.afterDone.tap(this.name, () => {
+                spawnCommand('compilation completed');
+            });
+        } else {
+            compiler.hooks.done.tap(this.name, () => {
+                setTimeout(() => {
+                    spawnCommand('compilation completed');
+                }, 1);
+            });
+        }
     }
 
     // eslint-disable-next-line no-unused-vars
@@ -66,28 +86,19 @@ class InteractiveModePlugin {
     // eslint-disable-next-line no-unused-vars
     recompileHandler(compiler) {
         // TODO: implement it
-        const totalRows = process.stdout.rows;
-        readline.cursorTo(process.stdout, 0, totalRows - 2);
-        logger.info('recompling is not supported');
-        process.stdout.write('> ');
+        spawnCommand('recompilation not supported');
     }
 
     // eslint-disable-next-line no-unused-vars
     pauseHandler(compiler) {
         // TODO: implement it
-        const totalRows = process.stdout.rows;
-        readline.cursorTo(process.stdout, 0, totalRows - 2);
-        logger.info('pausing is not supported       ');
-        process.stdout.write('> ');
+        spawnCommand('pausing not supported');
     }
 
     // eslint-disable-next-line no-unused-vars
     stopHandler(compiler) {
         //TODO: implement it
-        const totalRows = process.stdout.rows;
-        readline.cursorTo(process.stdout, 0, totalRows - 2);
-        logger.info('stoping is not supported');
-        process.stdout.write('> ');
+        spawnCommand('stop not supported');
     }
 }
 module.exports = { InteractiveModePlugin };
