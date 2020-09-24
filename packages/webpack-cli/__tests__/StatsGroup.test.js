ec569e24ffaf40fe2633d39b448f3301079376ad
Anshuman Verma <anshu.av97@gmail.com>
2020-09-24 19:29:51 +0530

    refactor: migrate stats group

--- packages/webpack-cli/__tests__/StatsGroup.test.js
+++ packages/webpack-cli/__tests__/StatsGroup.test.js
@@ -1,15 +1,19 @@
-const StatsGroup = require('../lib/groups/StatsGroup');
+const resolveStats = require('../lib/groups/resolveStats');
 
 describe('StatsGroup', function () {
-    it('should handle json', () => {
-        const group = new StatsGroup([
-            {
-                json: true,
-            },
-        ]);
-
-        const result = group.run();
+    it('should assign json correctly', () => {
+        const result = resolveStats({
+            json: true,
+        });
         expect(result.options.stats).toBeFalsy();
         expect(result.outputOptions.json).toBeTruthy();
+    });
+
+    it('should assign stats correctly', () => {
+        const result = resolveStats({
+            stats: 'warning',
+        });
+        expect(result.options.stats).toEqual('warning');
+        expect(result.outputOptions.json).toBeFalsy();
     });
 });
