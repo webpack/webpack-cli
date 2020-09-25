a0850424426244d366a19c649dff66f18b4916f1
Nitin Kumar <snitin315@gmail.com>
2020-09-25 12:35:28 +0530

    chore: use logger

--- packages/webpack-cli/bin/cli.js
+++ packages/webpack-cli/bin/cli.js
@@ -5,6 +5,7 @@ require('v8-compile-cache');
 const importLocal = require('import-local');
 const runCLI = require('../lib/bootstrap');
 const { yellow } = require('colorette');
+const { error } = require('../lib/utils/logger');
 const { packageExists, promptInstallation } = require('@webpack-cli/package-utils');
 
 // Prefer the local installation of webpack-cli
@@ -18,7 +19,7 @@ if (packageExists('webpack')) {
     runCLI(rawArgs);
 } else {
     promptInstallation('webpack', () => {
-        console.error(`It looks like ${yellow('webpack')} is not installed.`);
+        error(`It looks like ${yellow('webpack')} is not installed.`);
     });
     return;
 }
