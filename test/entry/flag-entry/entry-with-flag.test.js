50399f187179d41abdbd5ab11690947b1fbbf847
Anshuman Verma <anshu.av97@gmail.com>
2020-09-30 21:48:40 +0530

    tests: fix tests

--- test/entry/flag-entry/entry-with-flag.test.js
+++ test/entry/flag-entry/entry-with-flag.test.js
@@ -5,6 +5,23 @@ const { stat, readFile } = require('fs');
 const { resolve } = require('path');
 
 describe('entry flag', () => {
+    it('should resolve the path to src/index.cjs', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--entry', './src/index.cjs', '-o', './dist/'], false);
+        expect(stderr).toBeFalsy();
+        expect(stdout).toBeTruthy();
+
+        stat(resolve(__dirname, './dist/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+        readFile(resolve(__dirname, './dist/main.js'), 'utf-8', (err, data) => {
+            expect(err).toBe(null);
+            expect(data).toContain('Kazuya Miyuki');
+            done();
+        });
+    });
+
     it('should load ./src/a.js as entry', (done) => {
         const { stderr, stdout } = run(__dirname, ['--entry', './src/a.js']);
         expect(stderr).toBeFalsy();
@@ -35,23 +52,6 @@ describe('entry flag', () => {
         readFile(resolve(__dirname, './bin/main.js'), 'utf-8', (err, data) => {
             expect(err).toBe(null);
             expect(data).toContain('Hello from a.js');
-            done();
-        });
-    });
-
-    it('should resolve the path to src/index.cjs', (done) => {
-        const { stderr, stdout } = run(__dirname, ['--entry', './src/index.cjs']);
-        expect(stderr).toBeFalsy();
-        expect(stdout).toBeTruthy();
-
-        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-            done();
-        });
-        readFile(resolve(__dirname, './bin/main.js'), 'utf-8', (err, data) => {
-            expect(err).toBe(null);
-            expect(data).toContain('Kazuya Miyuki');
             done();
         });
     });
