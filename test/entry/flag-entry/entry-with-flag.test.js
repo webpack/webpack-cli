6f49e96e35338e61cfda2e33964daefecac9649d
Anshuman Verma <anshu.av97@gmail.com>
2020-10-03 11:55:16 +0530

    refactor: migrate basic group (#1837)
    * refactor: migrate basic group
    
    * chore: rm stale utils, pass entry directly
    
    * tests: add test for cjs entry
    
    * tests: fix tests

--- test/entry/flag-entry/entry-with-flag.test.js
+++ test/entry/flag-entry/entry-with-flag.test.js
@@ -5,6 +5,23 @@ const { stat, readFile } = require('fs');
 const { resolve } = require('path');
 
 describe('entry flag', () => {
+    it('should resolve the path to src/index.cjs', (done) => {
+        const { stderr, stdout } = run(__dirname, ['--entry', './src/index.cjs', '-o', './dist/'], false);
+        expect(stderr).toBeFalsy();
+        expect(stdout).toBeTruthy();
+
+        stat(resolve(__dirname, './dist/main.js'), (err, stats) => {
+            expect(err).toBe(null);
+            expect(stats.isFile()).toBe(true);
+            done();
+        });
+        readFile(resolve(__dirname, './dist/main.js'), 'utf-8', (err, data) => {
+            expect(err).toBe(null);
+            expect(data).toContain('Kazuya Miyuki');
+            done();
+        });
+    });
+
     it('should load ./src/a.js as entry', (done) => {
         const { stderr, stdout } = run(__dirname, ['--entry', './src/a.js']);
         expect(stderr).toBeFalsy();
@@ -40,7 +57,9 @@ describe('entry flag', () => {
     });
 
     it('should throw error for invalid entry file', () => {
-        const { stderr } = run(__dirname, ['--entry', './src/test.js']);
-        expect(stderr).toContain('Error: you provided an invalid entry point.');
+        const { stdout, stderr, exitCode } = run(__dirname, ['--entry', './src/test.js']);
+        expect(stdout).toContain("Module not found: Error: Can't resolve");
+        expect(exitCode).toEqual(1);
+        expect(stderr).toBeFalsy();
     });
 });
