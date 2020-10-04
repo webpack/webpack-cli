955a1b14d85fdce9c292ff7c7039f38610dab43b
Nitin Kumar <snitin315@gmail.com>
2020-10-04 09:39:09 +0530

    fix: cache

--- test/cache/cache.test.js
+++ test/cache/cache.test.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const { run } = require('../utils/test-utils');
+const { run, isWebpack5 } = require('../utils/test-utils');
 
 describe('bail and watch warning', () => {
     it('should log warning in case of single compiler', () => {
         let { stderr, stdout } = run(__dirname, ['-c', 'webpack.config.js'], false);
+        // run 2nd compilation
+        ({ stderr, stdout } = run(__dirname, ['-c', 'webpack.config.js'], false));
 
-        expect(stderr).toContain('starting to restore cache content');
-        expect(stdout).toContain('[cached] 1 module');
+        if (isWebpack5) {
+            expect(stderr).toContain('starting to restore cache content');
+            expect(stdout).toContain('[cached] 1 module');
+        }
     });
 });
