83a2dd226e49112296c881a7786b375c9cdb6b50
Nitin Kumar <snitin315@gmail.com>
2020-10-04 14:17:47 +0530

    refactor: migrate advanced group (#1863)
    Co-authored-by: Anshuman Verma <anshu.av97@gmail.com>

---
+++ packages/webpack-cli/__tests__/resolveAdvanced.test.js
@@ -0,0 +1,30 @@
+const resolveAdvanced = require('../lib/groups/resolveAdvanced');
+
+const targetValues = ['web', 'webworker', 'node', 'async-node', 'node-webkit', 'electron-main', 'electron-renderer', 'electron-preload'];
+
+describe('advanced options', function () {
+    it('should load the HMR plugin', () => {
+        const result = resolveAdvanced({
+            hot: true,
+        });
+        expect(result.options.plugins[0].constructor.name).toEqual('HotModuleReplacementPlugin');
+    });
+
+    it('should load the prefetch plugin', () => {
+        const result = resolveAdvanced({
+            prefetch: 'url',
+        });
+        expect(result.options.plugins[0].constructor.name).toEqual('PrefetchPlugin');
+    });
+
+    {
+        targetValues.map((option) => {
+            it(`should handle ${option} option`, () => {
+                const result = resolveAdvanced({
+                    target: option,
+                });
+                expect(result.options.target).toEqual(option);
+            });
+        });
+    }
+});
