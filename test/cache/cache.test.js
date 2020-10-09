8e52d4e848b9f6bfdf5212f70c5c2bbe6574ede0
Anshuman Verma <anshu.av97@gmail.com>
2020-10-09 16:08:15 +0530

    fix: cache

--- test/cache/cache.test.js
+++ test/cache/cache.test.js
@@ -4,9 +4,9 @@ const { run, isWebpack5 } = require('../utils/test-utils');
 
 describe('cache related tests', () => {
     it('should log warning in case of single compiler', () => {
-        let { stderr, stdout } = run(__dirname, ['-c', 'webpack.config.js'], false);
+        let { stderr, stdout } = run(__dirname, ['-c', './webpack.config.js'], false);
         // run 2nd compilation
-        ({ stderr, stdout } = run(__dirname, ['-c', 'webpack.config.js'], false));
+        ({ stderr, stdout } = run(__dirname, ['-c', './webpack.config.js'], false));
 
         if (isWebpack5) {
             expect(stderr).toContain('starting to restore cache content');
