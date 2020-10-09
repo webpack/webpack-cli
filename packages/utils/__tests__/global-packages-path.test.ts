e7702680331ae8708d2a28071f77e594d0181b16
Anshuman Verma <anshu.av97@gmail.com>
2020-10-09 20:27:18 +0530

    fix: tests

--- packages/utils/__tests__/global-packages-path.test.ts
+++ packages/utils/__tests__/global-packages-path.test.ts
@@ -1,22 +1,25 @@
 'use strict';
+jest.setMock('../lib/get-package-manager', {
+    getPackageManager: jest.fn(),
+});
 import { getPathToGlobalPackages } from '../lib/global-packages-path';
+import { getPackageManager } from '../lib/get-package-manager';
 jest.mock('execa');
 jest.mock('cross-spawn');
 const globalModulesNpmValue = 'test-npm';
 jest.setMock('global-modules', globalModulesNpmValue);
-const getPackageManagerMock = jest.fn();
 
 import path from 'path';
 import spawn from 'cross-spawn';
 
 describe('getPathToGlobalPackages', () => {
     it('uses global-modules if package manager is npm', () => {
-        getPackageManagerMock.mockReturnValue('npm');
+        (getPackageManager as jest.Mock).mockReturnValue('npm');
         expect(getPathToGlobalPackages()).toEqual(globalModulesNpmValue);
     });
 
     it('executes a command to find yarn global dir if package manager is yarn', () => {
-        getPackageManagerMock.mockReturnValue('yarn');
+        (getPackageManager as jest.Mock).mockReturnValue('yarn');
         (spawn.sync as jest.Mock).mockReturnValue({
             stdout: {
                 toString: (): string => {
