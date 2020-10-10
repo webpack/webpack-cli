59a34d72f34788d0c2885774d84380cead82a07b
Anshuman Verma <anshu.av97@gmail.com>
2020-10-10 14:14:22 +0530

    fix: add compilation lifecycle in watch flag

--- test/watch/watch-flag.test.js
+++ test/watch/watch-flag.test.js
@@ -14,12 +14,12 @@ describe('--watch flag', () => {
         let semaphore = 1;
         proc.stdout.on('data', (chunk) => {
             const data = chunk.toString();
-            if (semaphore === 1 && data.includes('watching files for updates')) {
+            if (data.includes('watching files for updates')) {
                 writeFileSync(resolve(__dirname, './src/index.js'), `console.log('watch flag test');`);
-                semaphore--;
+                semaphore = 0;
                 return;
             }
-            if (semaphore === 0) {
+            if (semaphore === 0 && data.includes('index.js')) {
                 if (version.startsWith('5')) {
                     for (const word of wordsInStatsv5) {
                         expect(data).toContain(word);
@@ -34,6 +34,38 @@ describe('--watch flag', () => {
                 done();
                 return;
             }
+        });
+    });
+
+    it('should print compilation lifecycle', (done) => {
+        const proc = runAndGetWatchProc(__dirname, ['--watch'], false, '', true);
+        let semaphore = 0;
+        proc.stdout.on('data', (chunk) => {
+            const data = chunk.toString();
+            if (semaphore === 0) {
+                expect(data).toContain('Compilation  starting');
+            }
+            if (semaphore === 1) {
+                expect(data).toContain('Compilation  finished');
+            }
+            if (semaphore === 2) {
+                if (version.startsWith('5')) {
+                    for (const word of wordsInStatsv5) {
+                        expect(data).toContain(word);
+                    }
+                } else {
+                    for (const word of wordsInStatsv4) {
+                        expect(data).toContain(word);
+                    }
+                }
+            }
+            if (semaphore === 3) {
+                expect(data).toContain('watching files for updates');
+                proc.kill();
+                done();
+                return;
+            }
+            semaphore++;
         });
     });
 });
