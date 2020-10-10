761c5281605837175a8c7cd37154e80540746ce6
Anshuman Verma <anshu.av97@gmail.com>
2020-10-10 14:34:22 +0530

    fix: tests

--- test/watch/watch-flag.test.js
+++ test/watch/watch-flag.test.js
@@ -42,6 +42,7 @@ describe('--watch flag', () => {
         let semaphore = 0;
         proc.stdout.on('data', (chunk) => {
             const data = chunk.toString();
+            console.log({ data, semaphore });
             if (semaphore === 0) {
                 expect(data).toContain('Compilation  starting');
             }
