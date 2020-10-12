229c5378aa50654190e740d97f5629fc769c5f1e
evilebottnawi <sheo13666q@gmail.com>
2020-10-12 14:55:47 +0300

    tests: debug problem

--- test/config-lookup/custom-name/custom-name.test.js
+++ test/config-lookup/custom-name/custom-name.test.js
@@ -6,6 +6,8 @@ const { run } = require('../../utils/test-utils');
 describe('custom config file', () => {
     it('should work', () => {
         const { stdout, stderr, exitCode } = run(__dirname, ['--config', resolve(__dirname, 'config.webpack.js')], false);
+        console.log(stdout);
+        console.log(stderr);
         expect(stderr).toBeFalsy();
         expect(stdout).toBeTruthy();
         expect(exitCode).toBe(0);
