f4196bd9387245ebee516f49c03dfcacf2b4889d
Jordan Pittman <jordan@cryptica.me>
2020-10-13 19:06:24 -0400

    fix: log error traces

--- packages/webpack-cli/lib/utils/logger.js
+++ packages/webpack-cli/lib/utils/logger.js
@@ -1,7 +1,13 @@
 const { red, cyan, yellow, green } = require('colorette');
 
 module.exports = {
-    error: (val) => console.error(`[webpack-cli] ${red(val)}`),
+    error: (val) => {
+        if (val && val.stack) {
+            val = val.stack;
+        }
+
+        console.error(`[webpack-cli] ${red(val)}`);
+    },
     warn: (val) => console.warn(`[webpack-cli] ${yellow(val)}`),
     info: (val) => console.info(`[webpack-cli] ${cyan(val)}`),
     success: (val) => console.log(`[webpack-cli] ${green(val)}`),
