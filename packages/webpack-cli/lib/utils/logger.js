555742fe454581702714ab8eeb270aabb726535f
evilebottnawi <sheo13666q@gmail.com>
2020-10-15 19:11:46 +0300

    fix: output stacktrace on errors

--- packages/webpack-cli/lib/utils/logger.js
+++ packages/webpack-cli/lib/utils/logger.js
@@ -1,7 +1,8 @@
+const util = require('util');
 const { red, cyan, yellow, green } = require('colorette');
 
 module.exports = {
-    error: (val) => console.error(`[webpack-cli] ${red(val)}`),
+    error: (val) => console.error(`[webpack-cli] ${red(util.format(val))}`),
     warn: (val) => console.warn(`[webpack-cli] ${yellow(val)}`),
     info: (val) => console.info(`[webpack-cli] ${cyan(val)}`),
     success: (val) => console.log(`[webpack-cli] ${green(val)}`),
