564279e5b634a399647bcdb21449e5e6a7f0637e
Alexander Akait <evilebottnawi@users.noreply.github.com>
2020-10-15 20:42:43 +0300

    fix: run CLI after webpack installation (#1951)

--- packages/webpack-cli/bin/cli.js
+++ packages/webpack-cli/bin/cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 'use strict';
+
 require('v8-compile-cache');
+
 const importLocal = require('import-local');
 const runCLI = require('../lib/bootstrap');
 const { yellow } = require('colorette');
@@ -9,23 +11,29 @@ const { error, success } = require('../lib/utils/logger');
 const { packageExists } = require('../lib/utils/package-exists');
 const { promptInstallation } = require('../lib/utils/prompt-installation');
 
-// Prefer the local installation of webpack-cli
+// Prefer the local installation of `webpack-cli`
 if (importLocal(__filename)) {
     return;
 }
+
 process.title = 'webpack';
 
+const [, , ...rawArgs] = process.argv;
+
 if (packageExists('webpack')) {
-    const [, , ...rawArgs] = process.argv;
     runCLI(rawArgs);
 } else {
-    promptInstallation('webpack', () => {
+    promptInstallation('webpack -W', () => {
         error(`It looks like ${yellow('webpack')} is not installed.`);
     })
-        .then(() => success(`${yellow('webpack')} was installed sucessfully.`))
+        .then(() => {
+            success(`${yellow('webpack')} was installed sucessfully.`);
+
+            runCLI(rawArgs);
+        })
         .catch(() => {
-            process.exitCode = 2;
             error(`Action Interrupted, Please try once again or install ${yellow('webpack')} manually.`);
+
+            process.exit(2);
         });
-    return;
 }
