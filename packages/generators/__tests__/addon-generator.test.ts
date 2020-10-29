1257606e31fefac1a4a4b2de1a37d0578a59222b
evilebottnawi <sheo13666q@gmail.com>
2020-10-29 15:25:45 +0300

    chore(deps): update

--- packages/generators/__tests__/addon-generator.test.ts
+++ packages/generators/__tests__/addon-generator.test.ts
@@ -4,7 +4,6 @@ jest.setMock('@webpack-cli/utils', {
 
 import fs from 'fs';
 import path from 'path';
-import mkdirp from 'mkdirp';
 import rimraf from 'rimraf';
 import { getPackageManager } from 'webpack-cli/lib/utils/get-package-manager';
 import addonGenerator from '../src/addon-generator';
@@ -22,7 +21,7 @@ describe.skip('addon generator', () => {
 
     beforeAll(() => {
         rimraf.sync(testAssetsPath);
-        mkdirp.sync(genPath);
+        fs.mkdirSync(genPath, { recursive: true });
         // set the working directory to here so that the addon directory is
         // generated in ./test-assets/test-addon
         process.chdir(genPath);
