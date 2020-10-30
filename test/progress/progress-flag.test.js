88ef010b570e8f908fd48a2a0ba7fcff89126153
evilebottnawi <sheo13666q@gmail.com>
2020-10-30 13:50:53 +0300

    fix: test

--- test/progress/progress-flag.test.js
+++ test/progress/progress-flag.test.js
@@ -25,7 +25,7 @@ describe('progress flag', () => {
         const { stderr, stdout, exitCode } = run(__dirname, ['--progress=unknown']);
 
         expect(exitCode).toBe(2);
-        expect(stderr).toContain('[webpack-cli] Invalid unknown value for the progress option. Allowed value is profile.');
+        expect(stderr).toContain('Invalid unknown value for the progress option. Allowed value is profile.');
         expect(stdout).toBeFalsy();
     });
 
