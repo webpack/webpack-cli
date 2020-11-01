a232625bc72897e4b01d3eb85a1326c8084d3c43
Anshuman Verma <anshu.av97@gmail.com>
2020-11-01 13:02:22 +0530

    fix: assign cache value for default configs

--- packages/webpack-cli/lib/utils/flag-defaults.js
+++ packages/webpack-cli/lib/utils/flag-defaults.js
@@ -1,23 +1,24 @@
-const cacheDefaults = (finalConfig, parsedArgs) => {
+const cacheDefaults = (finalConfig, parsedArgs, outputOptions) => {
     // eslint-disable-next-line no-prototype-builtins
     const hasCache = finalConfig.hasOwnProperty('cache');
     let cacheConfig = {};
-    if (hasCache && parsedArgs.config) {
+    if (hasCache && (parsedArgs.config || outputOptions.defaultConfig)) {
         if (finalConfig.cache && finalConfig.cache.type === 'filesystem') {
             cacheConfig.buildDependencies = {
-                config: parsedArgs.config,
+                config: parsedArgs.config || outputOptions.defaultConfig,
             };
         }
         return { cache: cacheConfig };
     }
+    console.log({ cacheConfig, outputOptions });
     return cacheConfig;
 };
 
-const assignFlagDefaults = (compilerConfig, parsedArgs) => {
+const assignFlagDefaults = (compilerConfig, parsedArgs, outputOptions) => {
     if (Array.isArray(compilerConfig)) {
-        return compilerConfig.map((config) => cacheDefaults(config, parsedArgs));
+        return compilerConfig.map((config) => cacheDefaults(config, parsedArgs, outputOptions));
     }
-    return cacheDefaults(compilerConfig, parsedArgs);
+    return cacheDefaults(compilerConfig, parsedArgs, outputOptions);
 };
 
 module.exports = assignFlagDefaults;
