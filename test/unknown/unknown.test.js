a8b91c387dcbb824a2d128ada524364b30c9fbd1
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-11-02 17:38:37 +0530

    tests: assert for exit code

--- test/unknown/unknown.test.js
+++ test/unknown/unknown.test.js
@@ -2,13 +2,15 @@ const { run } = require('../utils/test-utils');
 
 describe('unknown behaviour', () => {
     it('warns the user if an unknown flag is passed in', () => {
-        const { stderr } = run(__dirname, ['--unknown']);
+        const { stderr, exitCode } = run(__dirname, ['--unknown']);
         expect(stderr).toBeTruthy();
         expect(stderr).toContain('Unknown argument: --unknown');
+        expect(exitCode).toBe(2);
     });
     it('suggests the closest match to an unknown flag', () => {
-        const { stderr, stdout } = run(__dirname, ['--entyr', './a.js']);
+        const { stderr, stdout, exitCode } = run(__dirname, ['--entyr', './a.js']);
         expect(stderr).toContain('Unknown argument: --entyr');
         expect(stdout).toContain('Did you mean --entry?');
+        expect(exitCode).toBe(2);
     });
 });
