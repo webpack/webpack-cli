a3ed530a34764be4969738e16b5561b9003e55b4
Nitin Kumar <snitin315@gmail.com>
2020-11-02 13:48:20 +0530

    tests: check for exit codes info

--- test/info/info-unknown.test.js
+++ test/info/info-unknown.test.js
@@ -3,7 +3,10 @@ const { runInfo } = require('../utils/test-utils');
 
 describe('should handle unknown args', () => {
     it('shows an appropriate warning on supplying unknown args', () => {
-        const { stderr } = runInfo(['--unknown'], __dirname);
+        const { stderr, stdout, exitCode } = runInfo(['--unknown'], __dirname);
+
+        expect(exitCode).toBe(2);
         expect(stderr).toContain(`[webpack-cli] ${red('Unknown argument: --unknown')}`);
+        expect(stdout).toBeFalsy();
     });
 });
