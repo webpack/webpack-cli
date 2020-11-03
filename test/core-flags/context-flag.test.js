10abb4cf6b9577103e4e3f3b195246973c5f4f60
Nitin Kumar <snitin315@gmail.com>
2020-11-03 15:47:00 +0530

    tests: add case for invalid context value

--- test/core-flags/context-flag.test.js
+++ test/core-flags/context-flag.test.js
@@ -1,12 +1,22 @@
 'use strict';
 
+const { resolve } = require('path');
 const { run } = require('../utils/test-utils');
 
 describe('--context flag', () => {
     it('should allow to set context', () => {
-        const { stderr, stdout } = run(__dirname, ['--context', '/test-context-path']);
+        const { stderr, stdout, exitCode } = run(__dirname, ['--context', './']);
 
         expect(stderr).toBeFalsy();
-        expect(stdout).toContain('test-context-path');
+        expect(exitCode).toBe(0);
+        expect(stdout).toContain(`context: '${resolve(__dirname, './')}'`);
+    });
+
+    it('should throw module not found error for invalid context', () => {
+        const { stderr, stdout, exitCode } = run(__dirname, ['--context', '/test-context-path']);
+
+        expect(stderr).toBeFalsy();
+        expect(exitCode).toBe(1);
+        expect(stdout).toContain(`Module not found: Error: Can't resolve './src/main.js' in '/test-context-path'`);
     });
 });
