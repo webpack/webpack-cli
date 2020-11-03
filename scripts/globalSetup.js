4d2f337e8545462caeb014fb9365e1f6b8353690
Anshuman Verma <anshu.av97@gmail.com>
2020-11-03 13:40:18 +0530

    chore: add endl
    Co-authored-by: Nitin Kumar <snitin315@gmail.com>

--- scripts/globalSetup.js
+++ scripts/globalSetup.js
@@ -1,3 +1,3 @@
 const { version } = require('webpack');
 
-module.exports = () => console.log(`Running tests for webpack @${version}`);
+module.exports = () => console.log(`\n Running tests for webpack @${version} \n`);
