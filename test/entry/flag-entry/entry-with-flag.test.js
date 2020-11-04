30d0b68bc421cdf9657d2c213236a733d7fcabe7
Nitin Kumar <snitin315@gmail.com>
2020-11-04 09:58:50 +0530

    tests: fix

--- test/entry/flag-entry/entry-with-flag.test.js
+++ test/entry/flag-entry/entry-with-flag.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { run } = require('../../utils/test-utils');
+const { run, isWebpack5 } = require('../../utils/test-utils');
 const { stat, readFile } = require('fs');
 const { resolve } = require('path');
 
@@ -43,23 +43,28 @@ describe('entry flag', () => {
         });
     });
 
-    it('should resolve the path to /src/a.js as ./src/a.js', (done) => {
+    it('should resolve the path to /src/a.js as ./src/a.js for webpack-5 only', (done) => {
         const { stderr, stdout, exitCode } = run(__dirname, ['--entry', '/src/a.js']);
 
-        expect(exitCode).toBe(0);
-        expect(stderr).toBeFalsy();
-        expect(stdout).toBeTruthy();
+        if (isWebpack5) {
+            expect(exitCode).toBe(0);
+            expect(stderr).toBeFalsy();
+            expect(stdout).toBeTruthy();
 
-        stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
-            expect(err).toBe(null);
-            expect(stats.isFile()).toBe(true);
-            done();
-        });
-        readFile(resolve(__dirname, './bin/main.js'), 'utf-8', (err, data) => {
-            expect(err).toBe(null);
-            expect(data).toContain('Hello from a.js');
-            done();
-        });
+            stat(resolve(__dirname, './bin/main.js'), (err, stats) => {
+                expect(err).toBe(null);
+                expect(stats.isFile()).toBe(true);
+                done();
+            });
+            readFile(resolve(__dirname, './bin/main.js'), 'utf-8', (err, data) => {
+                expect(err).toBe(null);
+                expect(data).toContain('Hello from a.js');
+                done();
+            });
+        } else {
+            expect(exitCode).toBe(1);
+            expect(stdout).toContain(`Module not found: Error: Can't resolve`);
+        }
     });
 
     it('should throw error for invalid entry file', () => {
