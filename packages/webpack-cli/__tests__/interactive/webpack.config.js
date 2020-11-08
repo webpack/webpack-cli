3f9fcedfba87c9e399f81ff54877b3f515fc61e9
Rishabh Chawla <rishabh31121999@gmail.com>
2020-11-08 12:43:10 +0530

    chore: lint file

--- packages/webpack-cli/__tests__/interactive/webpack.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line node/no-unpublished-require
-const WebpackCLITestPlugin = require('../../../../test/utils/webpack-cli-test-plugin');
-
-module.exports = {
-    mode: 'development',
-    devtool: false,
-    plugins: [new WebpackCLITestPlugin(['plugins'], false)],
-};
