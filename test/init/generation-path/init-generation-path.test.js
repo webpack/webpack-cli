43d6c616d77b3be3638109ea738e965a9d4ed2c6
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-11-09 17:10:04 +0530

    tests: add test case for supplying relative path

--- test/init/generation-path/init-generation-path.test.js
+++ test/init/generation-path/init-generation-path.test.js
@@ -10,12 +10,44 @@ const firstPrompt = 'Will your application have multiple bundles?';
 const genPath = join(__dirname, 'test-assets');
 
 describe('init generate-path flag', () => {
-    beforeAll(() => {
+    beforeEach(() => {
         rimraf.sync(genPath);
     });
 
     it('should scaffold in the specified path with --generation-path', () => {
         const { stdout } = run(__dirname, ['init', '--generation-path', 'test-assets', '--auto'], false);
+        // Test no prompts are present
+        expect(stdout).toBeTruthy();
+        expect(stdout).not.toContain(firstPrompt);
+
+        // Skip test in case installation fails
+        if (!fs.existsSync(resolve(genPath, './yarn.lock'))) {
+            return;
+        }
+
+        // Test regressively files are scaffolded
+        const files = ['./sw.js', './package.json', './src/index.js', './webpack.config.js'];
+
+        files.forEach((file) => {
+            expect(fs.existsSync(resolve(genPath, file))).toBeTruthy();
+        });
+
+        // Check package json is correctly configured
+        const pkgJsonTests = () => {
+            const pkgJson = require(join(genPath, './package.json'));
+            expect(pkgJson).toBeTruthy();
+            expect(pkgJson['devDependencies']).toBeTruthy();
+            expect(pkgJson['devDependencies']['webpack']).toBeTruthy();
+            expect(pkgJson['scripts']['build'] == 'webpack').toBeTruthy();
+        };
+        expect(pkgJsonTests).not.toThrow();
+    });
+
+    it('should scaffold in the current directory', () => {
+        // Create test-assets directory
+        fs.mkdirSync(genPath);
+
+        const { stdout } = run(genPath, ['init', '--generation-path', './', '--auto'], false);
         // Test no prompts are present
         expect(stdout).toBeTruthy();
         expect(stdout).not.toContain(firstPrompt);
