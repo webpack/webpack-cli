44f27e69235887b50f7c0986e19ad8f1c6253a77
Nitin Kumar <snitin315@gmail.com>
2020-11-09 07:44:41 +0530

    refactor: remove interactive instances

--- packages/webpack-cli/lib/webpack-cli.js
+++ packages/webpack-cli/lib/webpack-cli.js
@@ -399,12 +399,6 @@ class WebpackCLI {
 
         compiler = this.createCompiler(options, callback);
 
-        if (compiler && outputOptions.interactive) {
-            const interactive = require('./utils/interactive');
-
-            interactive(compiler, options, outputOptions);
-        }
-
         return Promise.resolve();
     }
 }
