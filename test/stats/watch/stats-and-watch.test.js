86a6b11b7d25fc283898e4bb0a09704c3d3c5e28
Nitin Kumar <snitin315@gmail.com>
2020-11-11 08:15:10 +0530

    fix: ci

--- test/stats/watch/stats-and-watch.test.js
+++ test/stats/watch/stats-and-watch.test.js
@@ -6,20 +6,18 @@ describe('stats and watch', () => {
     it('should not log stats with the "none" value from the configuration', async () => {
         const { stderr, stdout } = await runWatch(__dirname, ['-c', './webpack.config.js', '--color']);
 
-        expect(stdout).toMatchInlineSnapshot(`
-            "[webpack-cli] 
-            [webpack-cli] [32mCompilation finished[39m
-            [webpack-cli] [32mwatching files for updates...[39m"
-        `);
+        expect(stdout).toContain('[webpack-cli] [32mCompilation starting...[39m');
+        expect(stdout).toContain('[webpack-cli] [32mCompilation finished[39m');
+        expect(stdout).toContain('[webpack-cli] [32mwatching files for updates...[39m');
         expect(stderr).toBeFalsy();
     });
 
     it('should not log stats with the "none" value from the configuration and multi compiler mode', async () => {
         const { stderr, stdout } = await runWatch(__dirname, ['-c', './multi-webpack.config.js']);
 
-        expect(stdout).toMatchInlineSnapshot('[webpack-cli] [32mCompilation starting...[39m');
         expect(stdout).toContain('[webpack-cli] [32mCompilation starting...[39m');
-        expect(stdout).toContain('[webpack-cli] watching files for updates...');
+        expect(stdout).toContain('[webpack-cli] [32mCompilation finished[39m');
+        expect(stdout).toContain('[webpack-cli] [32mwatching files for updates...[39m');
         expect(stderr).toBeFalsy();
     });
 
@@ -28,10 +26,10 @@ describe('stats and watch', () => {
 
         const output = isWebpack5 ? 'successfully' : 'main.js';
 
-        expect(stdout).toContain('[webpack-cli] Compilation starting...');
-        expect(stdout).toContain('[webpack-cli] Compilation finished');
+        expect(stdout).toContain('[webpack-cli] [32mCompilation starting...[39m');
+        expect(stdout).toContain('[webpack-cli] [32mCompilation finished[39m');
+        expect(stdout).toContain('[webpack-cli] [32mwatching files for updates...[39m');
         expect(stdout).toContain(output);
-        expect(stdout).toContain('[webpack-cli] watching files for updates...');
         expect(stderr).toBeFalsy();
     });
 });
