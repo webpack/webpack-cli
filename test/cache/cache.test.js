2fc7d07d3024364df01f127fa1ac0874fb9c4b0c
evilebottnawi <sheo13666q@gmail.com>
2020-11-27 16:05:39 +0300

    tests: default cache setup

--- test/cache/cache.test.js
+++ test/cache/cache.test.js
@@ -1,17 +1,63 @@
 'use strict';
 
+const path = require('path');
+const rimraf = require('rimraf');
 const { run, isWebpack5 } = require('../utils/test-utils');
 
-describe('cache related tests', () => {
-    it('should log warning in case of single compiler', () => {
-        let { stderr, stdout, exitCode } = run(__dirname, ['-c', './webpack.config.js'], false);
-        // run 2nd compilation
-        ({ stderr, stdout, exitCode } = run(__dirname, ['-c', './webpack.config.js'], false));
+describe('cache', () => {
+    beforeEach((done) => {
+        rimraf(path.join(__dirname, '../../node_modules/.cache/webpack/test'), () => {
+            rimraf(path.join(__dirname, '../../node_modules/.cache/webpack/test-1'), () => {
+                done();
+            });
+        });
+    });
+
+    it('should work', () => {
+        let { exitCode, stderr, stdout } = run(__dirname, ['-c', './webpack.config.js', '--cache-name', 'test'], false);
 
         if (isWebpack5) {
-            expect(stderr).toContain('starting to restore cache content');
-            expect(stdout).toContain('[cached]');
+            expect(stderr).toContain('No pack exists at');
+            expect(stderr).toContain('Stored pack');
+            expect(stdout).toBeTruthy();
+        } else {
+            expect(exitCode).toEqual(2);
+        }
+
+        ({ exitCode, stderr, stdout } = run(__dirname, ['-c', './webpack.config.js', '--cache-name', 'test'], false));
+
+        if (isWebpack5) {
             expect(exitCode).toEqual(0);
+            expect(stderr).toContain('restore cache container');
+            expect(stderr).toContain('restore cache content metadata');
+            expect(stderr).toContain('restore cache content');
+            expect(stdout).toBeTruthy();
+        } else {
+            expect(exitCode).toEqual(2);
+        }
+    });
+
+    it('should work with autoloading configuration', () => {
+        let { exitCode, stderr, stdout } = run(__dirname, ['--cache-name', 'test-1'], false);
+
+        if (isWebpack5) {
+            expect(stderr).toContain('No pack exists at');
+            expect(stderr).toContain('Stored pack');
+            expect(stdout).toBeTruthy();
+        } else {
+            expect(exitCode).toEqual(2);
+        }
+
+        ({ exitCode, stderr, stdout } = run(__dirname, ['--cache-name', 'test-1'], false));
+
+        if (isWebpack5) {
+            expect(exitCode).toEqual(0);
+            expect(stderr).toContain('restore cache container');
+            expect(stderr).toContain('restore cache content metadata');
+            expect(stderr).toContain('restore cache content');
+            expect(stdout).toBeTruthy();
+        } else {
+            expect(exitCode).toEqual(2);
         }
     });
 });
