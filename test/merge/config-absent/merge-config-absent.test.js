db05cc026be998f4eb65f7952399144c85bd1f2d
Nitin Kumar <snitin315@gmail.com>
2020-12-01 10:46:27 +0530

    tests: correct merge-config-absent test

--- test/merge/config-absent/merge-config-absent.test.js
+++ test/merge/config-absent/merge-config-absent.test.js
@@ -5,12 +5,12 @@ const { run } = require('../../utils/test-utils');
 describe('merge flag configuration', () => {
     it('Show warning message when the merge config is absent', () => {
         // 2.js doesn't exist, let's try merging with it
-        const { exitCode, stdout, stderr } = run(__dirname, ['--config', './1.js', '--merge', './2.js'], false);
+        const { exitCode, stderr, stdout } = run(__dirname, ['--config', './1.js', '--config', './2.js', '--merge'], false);
 
-        expect(exitCode).toBe(2);
+        expect(exitCode).toEqual(2);
         // Since the process will exit, nothing on stdout
         expect(stdout).toBeFalsy();
         // Confirm that the user is notified
-        expect(stderr).toContain('At least two configurations are required for merge.');
+        expect(stderr).toContain(`The specified config file doesn't exist`);
     });
 });
