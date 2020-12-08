66ed2656103f5e8ba4eb84a2d556a21f703bb474
Rishabh Chawla <rishabh31121999@gmail.com>
2020-12-08 19:20:09 +0530

    tests: add tests for other serve and watch

--- test/stdin/stdin.test.js
+++ test/stdin/stdin.test.js
@@ -1,11 +1,41 @@
 const { runAndGetWatchProc } = require('../utils/test-utils');
 
 describe('stdin', () => {
-    it('should stop the process when stdin ends', () => {
+    it('should stop the process when stdin ends', (done) => {
         const proc = runAndGetWatchProc(__dirname, [], false, '', true);
         let semaphore = false;
         proc.on('exit', () => {
             expect(semaphore).toBe(true);
+            proc.kill();
+            done();
+        });
+        proc.stdin.end(() => {
+            semaphore = true;
+        });
+    });
+
+    it('should stop serve then stdin ends', (done) => {
+        const proc = runAndGetWatchProc(__dirname, ['serve'], false, '', true);
+        let semaphore = false;
+
+        proc.on('exit', () => {
+            expect(semaphore).toBe(true);
+            proc.kill();
+            done();
+        });
+
+        proc.stdin.end(() => {
+            semaphore = true;
+        });
+    });
+
+    it('should stop watch then stdin ends', (done) => {
+        const proc = runAndGetWatchProc(__dirname, ['--watch'], false, '', true);
+        let semaphore = false;
+        proc.on('exit', () => {
+            expect(semaphore).toBe(true);
+            proc.kill();
+            done();
         });
         proc.stdin.end(() => {
             semaphore = true;
