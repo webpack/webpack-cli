4abf5fc05097f71b4e46330e158aaa7b8701ce49
evilebottnawi <sheo13666q@gmail.com>
2020-12-10 17:31:05 +0300

    tests: refactor

--- test/serve/basic/serve-basic.test.js
+++ test/serve/basic/serve-basic.test.js
@@ -26,6 +26,22 @@ describe('basic serve usage', () => {
         return;
     }
 
+    it('should work', async () => {
+        const { stderr, stdout } = await runServe([], __dirname);
+
+        expect(stderr).toBeFalsy();
+        expect(stdout).toContain('main.js');
+        expect(stdout).not.toContain('HotModuleReplacementPlugin');
+    });
+
+    it('should work with flags', async () => {
+        const { stderr, stdout } = await runServe(['--hot'], __dirname);
+
+        expect(stderr).toBeFalsy();
+        expect(stdout).toContain('main.js');
+        expect(stdout).toContain('HotModuleReplacementPlugin');
+    });
+
     it('should respect the --no-color flag', async () => {
         const { stdout, stderr } = await runServe(['--help', '--no-color'], __dirname);
 
