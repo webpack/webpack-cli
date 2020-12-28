47358044f4dd5f458bc98b675bbaa4f8c1fcf5e6
evilebottnawi <sheo13666q@gmail.com>
2020-12-28 20:44:10 +0300

    fix: do not apply HotModuleReplacement plugin twice

--- test/utils/webpack-cli-test-plugin.js
+++ test/utils/webpack-cli-test-plugin.js
@@ -1,17 +1,32 @@
 class WebpackCLITestPlugin {
-    constructor(options, showAll = true) {
+    constructor(options, showAll = true, showHooks) {
         this.opts = options;
         this.showAll = showAll;
+        this.showHooks = showHooks;
     }
 
     apply(compiler) {
         compiler.hooks.done.tap('webpack-cli Test Plugin', () => {
+            if (this.showHooks) {
+                const identifiers = this.showHooks.split('.');
+
+                let shown = compiler;
+
+                identifiers.forEach((identifier) => {
+                    shown = shown[identifier];
+                });
+
+                console.log(shown);
+            }
+
             if (this.showAll) {
                 console.log(compiler.options);
             }
+
             if (this.opts) {
                 this.opts.map((e) => {
                     const config = Object.getOwnPropertyDescriptor(compiler.options, e);
+
                     console.log(config.value);
                 });
             }
