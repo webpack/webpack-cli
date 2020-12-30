4345b38092a6e9a91db173f06787b641cfade29b
evilebottnawi <sheo13666q@gmail.com>
2020-12-30 15:49:17 +0300

    refactor: code

--- packages/webpack-cli/lib/utils/logger.js
+++ packages/webpack-cli/lib/utils/logger.js
@@ -3,7 +3,7 @@ const { red, cyan, yellow, green } = require('colorette');
 
 module.exports = {
     error: (val) => console.error(`[webpack-cli] ${red(util.format(val))}`),
-    warn: (val, needColors) => console.warn(`[webpack-cli] ${needColors ? yellow(val) : val}`),
+    warn: (val) => console.warn(`[webpack-cli] ${yellow(val)}`),
     info: (val) => console.info(`[webpack-cli] ${cyan(val)}`),
     success: (val) => console.log(`[webpack-cli] ${green(val)}`),
     log: (val) => console.log(`[webpack-cli] ${val}`),
