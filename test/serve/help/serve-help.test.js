c63f8114837014c3f9989e2d3c5b9b8458ca7759
Anshuman Verma <anshu.av97@gmail.com>
2021-01-09 13:15:10 +0530

    fix: tests

--- test/serve/help/serve-help.test.js
+++ test/serve/help/serve-help.test.js
@@ -5,7 +5,7 @@ const descriptionText = 'Run the webpack dev server';
 
 describe('serve help', () => {
     it('should output serve help', async () => {
-        const { stderr, stdout, exitCode } = await runServe(['--help'], __dirname);
+        const { stderr, stdout, exitCode } = await runServe(__dirname, ['--help']);
         expect(stderr).toBeFalsy();
         expect(exitCode).toBe(0);
         expect(stdout).toContain(descriptionText);
@@ -19,7 +19,7 @@ describe('serve help', () => {
     });
 
     it('should output all flags when verbose help is set', async () => {
-        const { stderr, stdout, exitCode } = await runServe(['--help', 'verbose'], __dirname);
+        const { stderr, stdout, exitCode } = await runServe(__dirname, ['--help', 'verbose']);
         expect(stderr).toBeFalsy();
         expect(exitCode).toBe(0);
         expect(stdout).toContain(descriptionText);
