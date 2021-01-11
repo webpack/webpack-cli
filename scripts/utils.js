2599979328180e3c72e1ba071b01398425fdc75d
evilebottnawi <sheo13666q@gmail.com>
2021-01-11 18:18:49 +0300

    tests: fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -10,13 +10,22 @@ function collectTestFolders(strategy) {
 }
 
 function extractFolder(folderToRead, folders = [], folderStrategy) {
-    const files = fs.readdirSync(folderToRead);
+    let files;
+
+    try {
+        files = fs.readdirSync(folderToRead);
+    } catch (error) {
+        // Ignore
+    }
+
     files.forEach((file) => {
         const filePath = path.resolve(path.join(folderToRead, file));
         const stats = fs.statSync(filePath);
+
         if (folderStrategy(stats, file)) {
             folders.push(folderToRead);
         }
+
         if (stats.isDirectory() && file !== 'node_modules') {
             extractFolder(filePath, folders, folderStrategy);
         }
