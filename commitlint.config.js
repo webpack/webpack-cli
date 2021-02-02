ca976790cd7ebbb5a73cdbff51a3c9f8823357ea
Anshuman Verma <anshu.av97@gmail.com>
2021-02-02 11:44:29 +0530

    chore: use default rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,3 @@
-'use strict';
-
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'body-leading-blank': [1, 'always'],
-        'footer-leading-blank': [1, 'always'],
-        'header-max-length': [2, 'always', 80],
-        'scope-case': [2, 'always', 'lowerCase'],
-        'scope-empty': [0, 'never'],
-        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-        'subject-empty': [0, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-        'type-case': [2, 'always', 'lowerCase'],
-        'scope-enum': [0, 'never'],
-        'type-empty': [2, 'never'],
-        'type-enum': [2, 'always', ['ast', 'break', 'chore', 'cli', 'docs', 'feat', 'fix', 'misc', 'tests', 'refactor']],
-    },
 };
