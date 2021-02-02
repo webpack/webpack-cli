805f9ffb3493f7bea2d1be2c6e3c8884abebe621
Anshuman Verma <anshu.av97@gmail.com>
2021-02-02 16:27:14 +0530

    chore: use conventional commitlint (#2394)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
-'use strict';
-
 module.exports = {
-    extends: ['cz', '@commitlint/config-lerna-scopes'],
-    rules: {
-        'body-leading-blank': [1, 'always'],
-        'footer-leading-blank': [1, 'always'],
-        'header-max-length': [2, 'always', 80],
-        'scope-case': [2, 'always', 'lowerCase'],
-        'scope-empty': [0, 'never'],
-        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-        'subject-empty': [0, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-        'type-case': [2, 'always', 'lowerCase'],
-        // turn it on once CLI will be inside its own package, so the scope will be the name of the packaged
-        // part of the @commitlint/config-lerna-scopes
-        'scope-enum': [0, 'never'],
-        'type-empty': [2, 'never'],
-        'type-enum': [2, 'always', ['ast', 'break', 'chore', 'cli', 'docs', 'feat', 'fix', 'misc', 'tests', 'refactor']],
-    },
+    extends: ['@commitlint/config-conventional'],
 };
