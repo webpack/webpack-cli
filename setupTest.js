340180ca4fdd27ca8d0e62f589a44239b5d726b4
Nitin Kumar <snitin315@gmail.com>
2021-02-09 14:06:54 +0530

    test: disable colors

--- setupTest.js
+++ setupTest.js
@@ -1,4 +1,14 @@
+//eslint-disable-next-line node/no-extraneous-require
+const { jasmine } = require('@jest/globals');
+
 jest.setTimeout(240000);
 
-// disable colors
-process.env.NO_COLOR = true;
+jasmine.getEnv().addReporter({
+    specStarted: (result) => (jasmine.currentTest = result),
+    specDone: (result) => (jasmine.currentTest = result),
+});
+
+// disable colors by default except for tests on "colors" itself
+if (!jasmine.testPath.includes('test/colors/colors.test.js')) {
+    process.env.NO_COLOR = true;
+}
