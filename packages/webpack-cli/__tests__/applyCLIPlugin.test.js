905be144b0381d2f9f81c340bc6d4858da620f42
Anshuman Verma <anshu.av97@gmail.com>
2021-02-09 23:48:37 +0530

    feat: display used config path when logging level=log

--- packages/webpack-cli/__tests__/applyCLIPlugin.test.js
+++ packages/webpack-cli/__tests__/applyCLIPlugin.test.js
@@ -5,7 +5,7 @@ const applyCLIPlugin = new webpackCLI().applyCLIPlugin;
 
 describe('CLIPluginResolver', () => {
     it('should add CLI plugin to single compiler object', async () => {
-        const result = await applyCLIPlugin({ options: {} }, { hot: true, prefetch: true });
+        const result = await applyCLIPlugin({ options: {}, path: new WeakMap() }, { hot: true, prefetch: true });
         expect(result.options.plugins[0] instanceof CLIPlugin).toBeTruthy();
         expect(result.options.plugins[0].options).toEqual({
             configPath: undefined,
@@ -18,7 +18,7 @@ describe('CLIPluginResolver', () => {
     });
 
     it('should add CLI plugin to multi compiler object', async () => {
-        const result = await applyCLIPlugin({ options: [{}, {}] }, { hot: true, prefetch: true });
+        const result = await applyCLIPlugin({ options: [{}, {}], path: new WeakMap() }, { hot: true, prefetch: true });
         expect(result.options[0].plugins[0] instanceof CLIPlugin).toBeTruthy();
         expect(result.options[1].plugins[0] instanceof CLIPlugin).toBeTruthy();
         expect(result.options).toEqual([
--- test/core-flags/infrastructure-logging.test.js
+++ test/core-flags/infrastructure-logging.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { run } = require('../utils/test-utils');
+const { run, runWatch } = require('../utils/test-utils');
+const { resolve } = require('path');
 
 describe('infrastructure logging related flag', () => {
     it('should set infrastructureLogging.debug properly', () => {
@@ -25,6 +26,58 @@ describe('infrastructure logging related flag', () => {
         expect(exitCode).toBe(0);
         expect(stderr).toContain("Compilation 'compiler' starting...");
         expect(stderr).toContain("Compilation 'compiler' finished");
+        expect(stdout).toContain(`level: 'log'`);
+    });
+
+    it('should log used default config when level is log', () => {
+        const { exitCode, stderr, stdout } = run(__dirname, ['--infrastructure-logging-level', 'log']);
+
+        expect(exitCode).toBe(0);
+        const configPath = resolve(__dirname, './webpack.config.js');
+        expect(stderr).toContain("Compilation 'compiler' starting...");
+        expect(stderr).toContain(`Using config ${configPath}`);
+        expect(stderr).toContain("Compilation 'compiler' finished");
+        expect(stdout).toContain(`level: 'log'`);
+    });
+
+    it('should log used supplied config when level is log', () => {
+        const { exitCode, stderr, stdout } = run(__dirname, [
+            '--config',
+            'webpack.cache.config.js',
+            '--infrastructure-logging-level',
+            'log',
+        ]);
+
+        expect(exitCode).toBe(0);
+        const configPath = resolve(__dirname, './webpack.cache.config.js');
+        expect(stderr).toContain(`Using config ${configPath}`);
+        expect(stdout).toContain(`level: 'log'`);
+    });
+
+    it('should log used supplied config with watch', async () => {
+        const { stderr, stdout } = await runWatch(__dirname, [
+            '--config',
+            'webpack.cache.config.js',
+            '--infrastructure-logging-level',
+            'log',
+        ]);
+
+        const configPath = resolve(__dirname, './webpack.cache.config.js');
+        expect(stderr).toContain(`Using config ${configPath}`);
+        expect(stdout).toContain(`level: 'log'`);
+    });
+
+    it('should log used supplied config with serve', async () => {
+        const { stderr, stdout } = await runWatch(__dirname, [
+            'serve',
+            '--config',
+            'webpack.cache.config.js',
+            '--infrastructure-logging-level',
+            'log',
+        ]);
+
+        const configPath = resolve(__dirname, './webpack.cache.config.js');
+        expect(stderr).toContain(`Using config ${configPath}`);
         expect(stdout).toContain(`level: 'log'`);
     });
 });
