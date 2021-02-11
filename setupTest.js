9ee315db3ec202fe509932487960e4be07da2724
Nitin Kumar <snitin315@gmail.com>
2021-02-11 10:46:34 +0530

    fix: windows tests

--- setupTest.js
+++ setupTest.js
@@ -8,6 +8,6 @@ jasmine.getEnv().addReporter({
 });
 
 // disable colors by default except for tests on "colors" itself
-if (!jasmine.testPath.includes('test/colors/colors.test.js')) {
+if (!jasmine.testPath.includes('colors.test.js')) {
     process.env.NO_COLOR = true;
 }
