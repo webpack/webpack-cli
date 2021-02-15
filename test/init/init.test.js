2ae2fc7da058110314bcf352fa7d0a3017d1e722
Rishabh Chawla <rishabh31121999@gmail.com>
2021-02-15 18:39:25 +0530

    test: wrong template path

--- test/init/init.test.js
+++ test/init/init.test.js
@@ -1,9 +1,10 @@
 const { mkdirSync, existsSync } = require('fs');
 const { resolve } = require('path');
 const rimraf = require('rimraf');
-const { run } = require('../utils/test-utils');
+const { run, runPromptWithAnswers } = require('../utils/test-utils');
 
 const assetsPath = resolve(__dirname, './test-assets');
+const ENTER = '\x0D';
 
 describe('init', () => {
     beforeEach(() => {
@@ -17,25 +18,36 @@ describe('init', () => {
     it('should generate default project when nothing is passed', () => {
         const { stdout, stderr } = run(assetsPath, ['init']);
         expect(stdout).toContain('Project has been initialised with webpack!');
-        expect(stderr).toContain('create webpack.config.js');
+        expect(stderr).toContain('webpack.config.js');
 
         // Test files
-        const files = ['package.json', 'examples', 'src', 'test', 'src/index.js', 'webpack.config.js'];
+        const files = ['package.json', 'src', 'src/index.js', 'webpack.config.js'];
         files.forEach((file) => {
-            expect(existsSync(assetsPath, file)).toBeTruthy();
+            expect(existsSync(resolve(assetsPath, file))).toBeTruthy();
         });
     });
 
     it('should generate project when generationPath is supplied', () => {
         const { stdout, stderr } = run(__dirname, ['init', `--generation-path=${assetsPath}`]);
-        console.log({ stdout, stderr });
         expect(stdout).toContain('Project has been initialised with webpack!');
-        expect(stderr).toContain('create webpack.config.js');
+        expect(stderr).toContain('webpack.config.js');
 
         // Test files
-        const files = ['package.json', 'examples', 'src', 'test', 'src/index.js', 'webpack.config.js'];
+        const files = ['package.json', 'src', 'src/index.js', 'webpack.config.js'];
         files.forEach((file) => {
-            expect(existsSync(assetsPath, file)).toBeTruthy();
+            expect(existsSync(resolve(assetsPath, file))).toBeTruthy();
+        });
+    });
+
+    it('should ask question when wrong template is supplied', async () => {
+        const { stdout, stderr } = await runPromptWithAnswers(assetsPath, ['init', '--template=apple'], [`${ENTER}`]);
+        expect(stdout).toContain('Project has been initialised with webpack!');
+        expect(stderr).toContain('webpack.config.js');
+
+        // Test files
+        const files = ['package.json', 'src', 'src/index.js', 'webpack.config.js'];
+        files.forEach((file) => {
+            expect(existsSync(resolve(assetsPath, file))).toBeTruthy();
         });
     });
 });
