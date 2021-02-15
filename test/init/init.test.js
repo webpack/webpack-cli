988299fb90b47d1415a34b497f8ffb37b2479a6f
Rishabh Chawla <rishabh31121999@gmail.com>
2021-02-15 18:20:18 +0530

    test: add test for generation-path

--- test/init/init.test.js
+++ test/init/init.test.js
@@ -1,4 +1,4 @@
-const { mkdirSync } = require('fs');
+const { mkdirSync, existsSync } = require('fs');
 const { resolve } = require('path');
 const rimraf = require('rimraf');
 const { run } = require('../utils/test-utils');
@@ -17,6 +17,25 @@ describe('init', () => {
     it('should generate default project when nothing is passed', () => {
         const { stdout, stderr } = run(assetsPath, ['init']);
         expect(stdout).toContain('Project has been initialised with webpack!');
-        expect(stderr).toContain('create src/index.js');
+        expect(stderr).toContain('create webpack.config.js');
+
+        // Test files
+        const files = ['package.json', 'examples', 'src', 'test', 'src/index.js', 'webpack.config.js'];
+        files.forEach((file) => {
+            expect(existsSync(assetsPath, file)).toBeTruthy();
+        });
+    });
+
+    it('should generate project when generationPath is supplied', () => {
+        const { stdout, stderr } = run(__dirname, ['init', `--generation-path=${assetsPath}`]);
+        console.log({ stdout, stderr });
+        expect(stdout).toContain('Project has been initialised with webpack!');
+        expect(stderr).toContain('create webpack.config.js');
+
+        // Test files
+        const files = ['package.json', 'examples', 'src', 'test', 'src/index.js', 'webpack.config.js'];
+        files.forEach((file) => {
+            expect(existsSync(assetsPath, file)).toBeTruthy();
+        });
     });
 });
