2bbddcb6fb3e63f5073f769e42026903e895be6e
Anshuman Verma <anshu.av97@gmail.com>
2021-03-08 11:05:44 +0530

    test: remove irrelevant snapshots

--- scripts/globalSetup.js
+++ scripts/globalSetup.js
@@ -1,3 +1,25 @@
 const { version } = require('webpack');
+// eslint-disable-next-line node/no-unpublished-require
+const rimraf = require('rimraf');
+const { join } = require('path');
+const collectTestFolders = require('./utils');
 
-module.exports = () => console.log(`\n Running tests for webpack @${version} \n`);
+// Cleanup obsolete snapshots for non-relevant webpack version
+const isWebpack5 = version[0] === '5';
+
+const snapshotDirs = ['help'].map(() => `__snapshots-v${isWebpack5 ? '4' : version[0]}__`);
+
+const folderStrategy = (stats, file) => {
+    return stats.isDirectory() && snapshotDirs.includes(file);
+};
+
+const cleanupOutputDirs = () => {
+    for (const outputFolder of collectTestFolders(folderStrategy)) {
+        snapshotDirs.forEach((dir) => rimraf.sync(join(outputFolder, dir)));
+    }
+};
+
+module.exports = () => {
+    cleanupOutputDirs();
+    console.log(`\n Running tests for webpack @${version} \n`);
+};
