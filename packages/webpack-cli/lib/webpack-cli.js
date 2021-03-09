5275ad5b66a45aefece6e60c1f184b7022e2f474
Rishabh Chawla <rishabh31121999@gmail.com>
2021-03-09 14:18:31 +0530

    fix: error message

--- packages/webpack-cli/lib/webpack-cli.js
+++ packages/webpack-cli/lib/webpack-cli.js
@@ -944,7 +944,7 @@ class WebpackCLI {
                             (command) => command.name.includes(name) || name === command.alias,
                         );
                         if (typeof builtInCommandUsed !== 'undefined') {
-                            this.logger.error(`For using '${name}' command you need to install '${builtInCommandUsed[0].pkg}' package`);
+                            this.logger.error(`For using '${name}' command you need to install '${builtInCommandUsed.pkg}' package`);
                         } else {
                             this.logger.error(`Can't find and load command '${name}'`);
                             this.logger.error("Run 'webpack --help' to see available commands and options");
