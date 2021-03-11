f96ed8a380a52021b9b07b164ae3415c6d3cd3dd
Rishabh Chawla <rishabh31121999@gmail.com>
2021-03-11 12:08:49 +0530

    chore: better names for folders

--- smoketests/index.js
+++ smoketests/index.js
@@ -2,7 +2,7 @@
 const tests = [
     require('./missing-packages/webpack-dev-server.test.js'),
     require('./missing-packages/webpack.test.js'),
-    require('./missing-command-help/generator.test.js'),
+    require('./missing-command-packages/generator.test.js'),
 ];
 
 (async () => {
