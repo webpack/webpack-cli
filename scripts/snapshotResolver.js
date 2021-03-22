d92e0962f7cbbdbe2b188d974702d8686d3ba5d3
Anshuman Verma <anshu.av97@gmail.com>
2021-03-22 20:09:17 +0530

    fix: improve snapshot resolver

--- scripts/snapshotResolver.js
+++ scripts/snapshotResolver.js
@@ -1,24 +1,14 @@
-const { dirname, basename } = require('path');
-const { version } = require('webpack');
+const path = require('path');
 
-// For these tests we create different snapshot dir per version
-const customSnapshotDir = ['help'];
+const webpack = require('webpack');
+
+const [webpackVersion] = webpack.version;
+const snapshotExtension = `.snap.webpack${webpackVersion}`;
+
+console.log('Current webpack version:', webpackVersion);
 
 module.exports = {
-    // resolves from test to snapshot path
-    resolveSnapshotPath: (testPath, snapshotExtension) => {
-        const perVersionDir = customSnapshotDir.some((dir) => testPath.includes(`test/${dir}`));
-        const snapshotDirName = !perVersionDir ? '__snapshots__' : `__snapshots-v${version[0]}__`;
-        return `${dirname(testPath)}/${snapshotDirName}/${basename(testPath) + snapshotExtension}`;
-    },
-
-    // resolves from snapshot to test path
-    resolveTestPath: (snapshotFilePath, snapshotExtension) => {
-        const perVersionDir = customSnapshotDir.some((dir) => snapshotFilePath.includes(`test/${dir}`));
-        const snapshotDirName = !perVersionDir ? '__snapshots__' : `__snapshots-v${version[0]}__`;
-        return snapshotFilePath.replace(`/${snapshotDirName}`, '').slice(0, -snapshotExtension.length);
-    },
-
-    // Example test path, used for preflight consistency check of the implementation above
-    testPathForConsistencyCheck: 'some/__tests__/example.test.js',
+    resolveSnapshotPath: (testPath) => path.join(path.dirname(testPath), '__snapshots__', `${path.basename(testPath)}${snapshotExtension}`),
+    resolveTestPath: (snapshotPath) => snapshotPath.replace(`${path.sep}__snapshots__`, '').slice(0, -snapshotExtension.length),
+    testPathForConsistencyCheck: path.join('consistency_check', '__tests__', 'example.test.js'),
 };
