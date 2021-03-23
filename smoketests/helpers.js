6c269a902adb4fd51b686aadaaf90c7718e7171c
Nitin Kumar <snitin315@gmail.com>
2021-03-23 07:51:00 +0530

    test: refactor smoketests

--- smoketests/helpers.js
+++ smoketests/helpers.js
@@ -1,5 +1,9 @@
+/* eslint-disable node/no-unpublished-require */
+
 const fs = require('fs');
 const path = require('path');
+const execa = require('execa');
+const stripAnsi = require('strip-ansi');
 
 const ROOT_PATH = process.env.GITHUB_WORKSPACE ? process.env.GITHUB_WORKSPACE : path.resolve(__dirname, '..');
 
@@ -17,7 +21,121 @@ const swapPkgName = (current, isSubPackage = false) => {
     fs.renameSync(getPkgPath(current, isSubPackage), getPkgPath(next, isSubPackage));
 };
 
+const CLI_ENTRY_PATH = getBinPath();
+
+const runTest = (package, cliArgs = [], logMessage, isSubPackage = false) => {
+    // Simulate package missing
+    swapPkgName(package, isSubPackage);
+
+    const proc = execa(CLI_ENTRY_PATH, cliArgs, {
+        cwd: __dirname,
+    });
+
+    proc.stdin.setDefaultEncoding('utf-8');
+
+    proc.stdout.on('data', (chunk) => {
+        console.log(`  stdout: ${chunk.toString()}`);
+    });
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('  timeout: killing process');
+            proc.kill();
+        }, 30000);
+
+        const prompt = 'Would you like to install';
+        let hasLogMessage = false,
+            hasPrompt = false,
+            hasPassed = false;
+
+        proc.stderr.on('data', (chunk) => {
+            let data = stripAnsi(chunk.toString());
+            console.log(`  stderr: ${data}`);
+
+            if (data.includes(logMessage)) {
+                hasLogMessage = true;
+            }
+
+            if (data.includes(prompt)) {
+                hasPrompt = true;
+            }
+
+            if (hasLogMessage && hasPrompt) {
+                hasPassed = true;
+                proc.kill();
+            }
+        });
+
+        proc.on('exit', () => {
+            swapPkgName(`.${package}`, isSubPackage);
+            resolve(hasPassed);
+        });
+
+        proc.on('error', () => {
+            swapPkgName(`.${package}`, isSubPackage);
+            resolve(false);
+        });
+    });
+};
+
+const runTestWithHelp = (package, cliArgs = [], logMessage, isSubPackage = false) => {
+    // Simulate package missing
+    swapPkgName(package, isSubPackage);
+
+    const proc = execa(CLI_ENTRY_PATH, cliArgs, {
+        cwd: __dirname,
+    });
+
+    proc.stdin.setDefaultEncoding('utf-8');
+
+    proc.stdout.on('data', (chunk) => {
+        console.log(`  stdout: ${chunk.toString()}`);
+    });
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            proc.kill();
+        }, 30000);
+
+        const undefinedLogMessage = "Can't find and load command";
+
+        let hasLogMessage = false,
+            hasUndefinedLogMessage = false,
+            hasPassed = false;
+
+        proc.stderr.on('data', (chunk) => {
+            let data = stripAnsi(chunk.toString());
+            console.log(`  stderr: ${data}`);
+
+            if (data.includes(logMessage)) {
+                hasLogMessage = true;
+            }
+
+            if (data.includes(undefinedLogMessage)) {
+                hasUndefinedLogMessage = true;
+            }
+
+            if (hasLogMessage || hasUndefinedLogMessage) {
+                hasPassed = true;
+                proc.kill();
+            }
+        });
+
+        proc.on('exit', () => {
+            swapPkgName(`.${package}`, isSubPackage);
+            resolve(hasPassed);
+        });
+
+        proc.on('error', () => {
+            swapPkgName(`.${package}`, isSubPackage);
+            resolve(false);
+        });
+    });
+};
+
 module.exports = {
     getBinPath,
     swapPkgName,
+    runTest,
+    runTestWithHelp,
 };
--- smoketests/missing-packages/webpack.test.js
+++ smoketests/missing-packages/webpack.test.js
@@ -1,66 +1,14 @@
 'use strict';
 
-const execa = require('execa');
-const stripAnsi = require('strip-ansi');
+const { runTest } = require('../helpers');
 
-const { getBinPath, swapPkgName } = require('../helpers');
+const webpackTest = () => {
+    const packageName = 'webpack';
+    const args = [];
+    const logMessage = 'It looks like webpack is not installed.';
 
-const CLI_ENTRY_PATH = getBinPath();
-
-const runTest = () => {
-    // Simulate package missing
-    swapPkgName('webpack');
-
-    const proc = execa(CLI_ENTRY_PATH, [], {
-        cwd: __dirname,
-    });
-
-    proc.stdin.setDefaultEncoding('utf-8');
-
-    proc.stdout.on('data', (chunk) => {
-        console.log(`  stdout: ${chunk.toString()}`);
-    });
-
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            proc.kill();
-        }, 30000);
-
-        const logMessage = 'It looks like webpack is not installed.';
-        const prompt = 'Would you like to install';
-        let hasLogMessage = false,
-            hasPrompt = false,
-            hasPassed = false;
-
-        proc.stderr.on('data', (chunk) => {
-            let data = stripAnsi(chunk.toString());
-            console.log(`  stderr: ${data}`);
-
-            if (data.includes(logMessage)) {
-                hasLogMessage = true;
-            }
-
-            if (data.includes(prompt)) {
-                hasPrompt = true;
-            }
-
-            if (hasLogMessage && hasPrompt) {
-                hasPassed = true;
-                proc.kill();
-            }
-        });
-
-        proc.on('exit', () => {
-            swapPkgName('.webpack');
-            resolve(hasPassed);
-        });
-
-        proc.on('error', () => {
-            swapPkgName('.webpack');
-            resolve(false);
-        });
-    });
+    return runTest(packageName, args, logMessage);
 };
 
-module.exports.run = [runTest];
+module.exports.run = [webpackTest];
 module.exports.name = 'Missing webpack';
