d3c6042c3ddc5758395f7db0ab4ad2c0c0e638ca
Anshuman Verma <anshu.av97@gmail.com>
2021-03-24 16:43:10 +0530

    fix: rm globalsetup

--- scripts/globalSetup.js
+++ scripts/globalSetup.js
@@ -1,28 +1,3 @@
 const { version } = require('webpack');
-// eslint-disable-next-line node/no-unpublished-require
-const rimraf = require('rimraf');
-const { join } = require('path');
-const collectTestFolders = require('./utils');
 
-// Cleanup obsolete snapshots for non-relevant webpack version
-const isWebpack5 = version[0] === '5';
-
-const snapshotDirs = ['help'].map(() => `__snapshots-v${isWebpack5 ? '4' : '5'}__`);
-
-const folderStrategy = (stats, file) => {
-    return stats.isDirectory() && snapshotDirs.includes(file);
-};
-
-const cleanupOutputDirs = () => {
-    for (const outputFolder of collectTestFolders(folderStrategy)) {
-        snapshotDirs.forEach((dir) => {
-            console.log(`removed ${join(outputFolder, dir)}`);
-            rimraf.sync(join(outputFolder, dir));
-        });
-    }
-};
-
-module.exports = () => {
-    cleanupOutputDirs();
-    console.log(`\n Running tests for webpack @${version} \n`);
-};
+module.exports = () => console.log(`\n Running tests for webpack @${version} \n`);
