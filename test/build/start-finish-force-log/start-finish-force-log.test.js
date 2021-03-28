281aad3ee4961f1643453eb1a926e88e0b7f019c
Anshuman Verma <anshu.av97@gmail.com>
2021-03-28 12:01:39 +0530

    feat: add flag to force start finish log (#2566)

---
+++ test/build/start-finish-force-log/start-finish-force-log.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { run, runWatch, isWebpack5 } = require('../../utils/test-utils');
+
+describe('start finish force log', () => {
+    it('start finish force log when env is set', () => {
+        const { exitCode, stderr, stdout } = run(__dirname, [], {
+            env: { WEBPACK_CLI_START_FINISH_FORCE_LOG: true },
+        });
+        expect(exitCode).toBe(0);
+        expect(stderr).toContain('Compiler starting...');
+        expect(stderr).toContain('Compiler finished');
+        const output = isWebpack5 ? 'compiled successfully' : 'main.js';
+        expect(stdout).toContain(output);
+    });
+
+    it('should show name of the config', () => {
+        const { exitCode, stderr, stdout } = run(__dirname, ['--name', 'log config'], {
+            env: { WEBPACK_CLI_START_FINISH_FORCE_LOG: true },
+        });
+        expect(exitCode).toBe(0);
+        expect(stderr).toContain("Compiler 'log config' starting...");
+        expect(stderr).toContain("Compiler 'log config' finished");
+        const output = isWebpack5 ? 'compiled successfully' : 'main.js';
+        expect(stdout).toContain(output);
+    });
+
+    it('should work with watch', async () => {
+        const { stderr, stdout } = await runWatch(__dirname, ['watch'], {
+            env: { WEBPACK_CLI_START_FINISH_FORCE_LOG: true },
+        });
+        expect(stderr).toContain('Compiler starting...');
+        expect(stderr).toContain('Compiler finished');
+        const output = isWebpack5 ? 'compiled successfully' : 'main.js';
+        expect(stdout).toContain(output);
+    });
+
+    it('should work with multi compiler', () => {
+        const { exitCode, stderr, stdout } = run(__dirname, ['--config', './webpack.config.array.js'], {
+            env: { WEBPACK_CLI_START_FINISH_FORCE_LOG: true },
+        });
+        expect(exitCode).toBe(0);
+        expect(stderr).toContain("Compiler 'Gojou' starting...");
+        expect(stderr).toContain("Compiler 'Satoru' starting...");
+        expect(stderr).toContain("Compiler 'Gojou' finished");
+        expect(stderr).toContain("Compiler 'Satoru' finished");
+        const output = isWebpack5 ? 'compiled successfully' : 'main.js';
+        expect(stdout).toContain(output);
+    });
+});
