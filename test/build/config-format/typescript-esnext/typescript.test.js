2235e84688fddaf200d123d7d65bbef61bfed9a6
evilebottnawi <sheo13666q@gmail.com>
2021-04-02 17:40:56 +0300

    style: format

--- test/build/config-format/typescript-esnext/typescript.test.js
+++ test/build/config-format/typescript-esnext/typescript.test.js
@@ -2,15 +2,19 @@
 const { run } = require('../../../utils/test-utils');
 const { existsSync } = require('fs');
 const { resolve } = require('path');
+
 describe('webpack cli', () => {
     it('should support typescript esnext file', async () => {
         if (process.version.slice(1, 3) < 14) {
             expect(true).toBe(true);
+
             return;
         }
+
         const { exitCode, stderr, stdout } = await run(__dirname, ['-c', './webpack.config.ts'], {
             nodeOptions: ['--loader=ts-node/esm', '--experimental-modules'],
         });
+
         expect(stderr).not.toBeFalsy();
         expect(stdout).toBeTruthy();
         expect(exitCode).toBe(0);
