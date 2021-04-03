7a47527d29b9db8c57753f4b1135b14001940ece
Anshuman Verma <anshu.av97@gmail.com>
2021-04-03 11:35:13 +0530

    chore: skip mac w4

--- test/build/config-format/typescript-esnext/typescript.test.js
+++ test/build/config-format/typescript-esnext/typescript.test.js
@@ -1,11 +1,11 @@
-jest.setTimeout(360000);
 // eslint-disable-next-line node/no-unpublished-require
-const { run } = require('../../../utils/test-utils');
+const { run, isWebpack5 } = require('../../../utils/test-utils');
 const { existsSync } = require('fs');
 const { resolve } = require('path');
 
 describe('webpack cli', () => {
     it('should support typescript esnext file', async () => {
+        const isMacOS = process.platform === 'darwin';
         const majorNodeVersion = process.version.slice(1, 3);
         if (majorNodeVersion < 14) {
             expect(true).toBe(true);
@@ -13,11 +13,16 @@ describe('webpack cli', () => {
             return;
         }
 
+        if (isMacOS && !isWebpack5) {
+            expect(true).toBe(true);
+
+            return;
+        }
+
         const { exitCode, stderr, stdout } = await run(__dirname, ['-c', './webpack.config.ts'], {
             nodeOptions: ['--loader=ts-node/esm'],
+            env: { WEBPACK_CLI_FORCE_LOAD_ESM_CONFIG: true },
         });
-        console.log({ exitCode, stderr, stdout });
-
         expect(stderr).not.toBeFalsy();
         expect(stdout).toBeTruthy();
         expect(exitCode).toBe(0);
