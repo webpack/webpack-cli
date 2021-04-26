de7b431c408d7a0b3eb4f387ba7e3e3611d1d0f0
Nitin Kumar <snitin315@gmail.com>
2021-04-26 18:37:23 +0530

    test: more snapshot testing (#2666)

--- test/build/hot/hot-flag.test.js
+++ test/build/hot/hot-flag.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const { run } = require('../../utils/test-utils');
+const { run, normalizeStderr, normalizeStdout } = require('../../utils/test-utils');
 const { readFileSync } = require('fs');
 const { resolve } = require('path');
 
@@ -8,7 +8,7 @@ describe('--hot flag', () => {
         const { exitCode, stderr, stdout } = await run(__dirname, ['--hot']);
 
         expect(exitCode).toBe(0);
-        expect(stderr).toBeFalsy();
+        expect(normalizeStderr(stderr)).toMatchSnapshot('stderr');
         expect(stdout).toBeTruthy();
         expect(readFileSync(resolve(__dirname, './dist/main.js')).toString()).toContain('webpackHotUpdate');
     });
@@ -17,7 +17,7 @@ describe('--hot flag', () => {
         const { exitCode, stderr, stdout } = await run(__dirname, ['--hot', 'only']);
 
         expect(exitCode).toBe(0);
-        expect(stderr).toBeFalsy();
+        expect(normalizeStderr(stderr)).toMatchSnapshot('stderr');
         expect(stdout).toBeTruthy();
         expect(readFileSync(resolve(__dirname, './dist/main.js')).toString()).toContain('webpackHotUpdate');
     });
@@ -26,15 +26,15 @@ describe('--hot flag', () => {
         const { exitCode, stderr, stdout } = await run(__dirname, ['--hot', 'unknown']);
 
         expect(exitCode).toBe(2);
-        expect(stderr).toContain(`[webpack-cli] 'unknown' is an invalid value for the --hot option. Use 'only' instead.`);
-        expect(stdout).toBeFalsy();
+        expect(normalizeStderr(stderr)).toMatchSnapshot('stderr');
+        expect(normalizeStdout(stdout)).toMatchSnapshot('stdout');
     });
 
     it('should be successful when --no-hot is passed', async () => {
         const { exitCode, stderr, stdout } = await run(__dirname, ['--no-hot']);
 
         expect(exitCode).toBe(0);
-        expect(stderr).toBeFalsy();
+        expect(normalizeStderr(stderr)).toMatchSnapshot('stderr');
         expect(stdout).toBeTruthy();
         expect(readFileSync(resolve(__dirname, './dist/main.js')).toString()).not.toContain('webpackHotUpdate');
     });
