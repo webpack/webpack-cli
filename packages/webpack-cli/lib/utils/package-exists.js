f484fdaf4af6e99d1d634951d30d92eb9036c144
Rishabh Chawla <rishabh31121999@gmail.com>
2021-04-26 12:45:05 +0530

    fix: double require problem

--- packages/webpack-cli/lib/utils/package-exists.js
+++ packages/webpack-cli/lib/utils/package-exists.js
@@ -1,9 +1,18 @@
 function packageExists(packageName) {
+    const { execSync } = require('child_process');
+
+    const command = `
     try {
-        return require.resolve(packageName);
-    } catch (error) {
-        return false;
-    }
+      console.log(require.resolve('${packageName}'));
+    } catch (err) {
+      console.log('Not Found');
+    }`;
+
+    const rootPath = execSync(`node -e "${command}"`, {
+        encoding: 'utf8',
+    }).trimEnd();
+
+    return rootPath !== 'Not Found';
 }
 
 module.exports = packageExists;
