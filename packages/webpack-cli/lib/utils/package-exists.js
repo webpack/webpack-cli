346c947b01af493237769f1fe8cae3abe8610d71
Rishabh Chawla <rishabh31121999@gmail.com>
2021-04-26 14:25:00 +0530

    chore: debug

--- packages/webpack-cli/lib/utils/package-exists.js
+++ packages/webpack-cli/lib/utils/package-exists.js
@@ -1,20 +1,25 @@
 function packageExists(packageName) {
-    const { execSync } = require('child_process');
-
-    const command = `
     try {
-      console.log(require.resolve('${packageName}'));
+        const { execSync } = require('child_process');
+
+        const command = `
+        try {
+          console.log(require.resolve('${packageName}'));
+        } catch (err) {
+          console.log('Not Found');
+        }`;
+
+        const rootPath = execSync(`node -e "${command}"`, {
+            encoding: 'utf8',
+        }).trimEnd();
+
+        console.log(rootPath);
+
+        return rootPath !== 'Not Found';
     } catch (err) {
-      console.log('Not Found');
-    }`;
-
-    const rootPath = execSync(`node -e "${command}"`, {
-        encoding: 'utf8',
-    }).trimEnd();
-
-    console.log(rootPath);
-
-    return rootPath !== 'Not Found';
+        console.log(err);
+        return false;
+    }
 }
 
 module.exports = packageExists;
