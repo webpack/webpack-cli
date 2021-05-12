5c12ce87f477830a4fd91f041ac30a5687fed5af
Rishabh Chawla <rishabh31121999@gmail.com>
2021-05-12 13:42:39 +0530

    chore: run on v4 only

--- test/serve/invalid-schema/invalid-schema.test.js
+++ test/serve/invalid-schema/invalid-schema.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const { run, normalizeStderr, normalizeStdout } = require('../../utils/test-utils');
+const { run, normalizeStderr, normalizeStdout, isDevServer4 } = require('../../utils/test-utils');
 
 describe('invalid schema', () => {
     it('should log webpack error and exit process on invalid config', async () => {
@@ -19,6 +19,12 @@ describe('invalid schema', () => {
     });
 
     it('should log webpack-dev-server error and exit process on invalid flag', async () => {
+        // Don't check on versions other than v4
+        if (!isDevServer4) {
+            expect(true).toBe(true);
+            return;
+        }
+
         const { exitCode, stderr, stdout } = await run(__dirname, ['serve', '--port', '-1']);
 
         expect(exitCode).toEqual(2);
