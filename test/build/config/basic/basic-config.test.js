a3189d3db9f969d207281ef08a582613fd4bac42
Rishabh Chawla <rishabh31121999@gmail.com>
2021-09-08 12:58:06 +0530

    chore: basic config description

--- test/build/config/basic/basic-config.test.js
+++ test/build/config/basic/basic-config.test.js
@@ -4,7 +4,7 @@ const { resolve } = require("path");
 const { run } = require("../../../utils/test-utils");
 
 describe("basic config file", () => {
-  it("is able to understand and parse a very basic configuration file", async () => {
+  it("should build and not throw error with a basic configuration file", async () => {
     const { exitCode, stderr, stdout } = await run(__dirname, [
       "-c",
       resolve(__dirname, "webpack.config.js"),
