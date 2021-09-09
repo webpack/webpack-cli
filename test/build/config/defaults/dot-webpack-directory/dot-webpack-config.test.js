17e74a93f425d6ce811c10afa5c3c556780fa942
Rishabh Chawla <rishabh31121999@gmail.com>
2021-09-09 13:20:36 +0530

    chore: update dot webpack config test

--- test/build/config/defaults/dot-webpack-directory/dot-webpack-config.test.js
+++ test/build/config/defaults/dot-webpack-directory/dot-webpack-config.test.js
@@ -3,8 +3,8 @@ const { existsSync } = require("fs");
 const { resolve } = require("path");
 const { run } = require("../../../../utils/test-utils");
 
-describe("multiple config files", () => {
-  it("Uses dev config when both dev and none are present", async () => {
+describe(".webpack configuration file", () => {
+  it("should build and not throw error when config is present in .webpack", async () => {
     const { stdout, stderr, exitCode } = await run(__dirname, []);
     expect(exitCode).toEqual(0);
     expect(stderr).toBeFalsy();
