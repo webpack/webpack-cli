c1d4b2daa7e50cfd6b3f8d25592933b386af9e50
Rishabh Chawla <rishabh31121999@gmail.com>
2021-09-14 10:21:02 +0530

    chore: default config with mode

--- test/build/config/defaults/with-mode/default-config-with-mode.test.js
+++ test/build/config/defaults/with-mode/default-config-with-mode.test.js
@@ -3,8 +3,8 @@ const { existsSync } = require("fs");
 const { resolve } = require("path");
 const { run } = require("../../../../utils/test-utils");
 
-describe("multiple config files", () => {
-  it("Uses dev config when development mode is supplied", async () => {
+describe("default config with mode from cli", () => {
+  it("should build and not throw error with development mode supplied", async () => {
     const { stdout, stderr, exitCode } = await run(__dirname, ["--mode", "development"]);
     expect(exitCode).toEqual(0);
     expect(stderr).toBeFalsy();
