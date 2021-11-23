c7a174eaffd864dffa1ddac544be827ad51c55af
Rishabh Chawla <rishabh31121999@gmail.com>
2021-11-23 12:24:27 +0530

    chore: fix single compiler tests

--- test/interactive/single-compiler/single-compiler-interactive.test.js
+++ test/interactive/single-compiler/single-compiler-interactive.test.js
@@ -6,196 +6,193 @@ const wordsInStatsv5 = ["asset", "index.js", "compiled", "webpack"];
 const clear = "\x1B[2J\x1B[3J\x1B[H";
 
 const runTest = (proc, checker, done) => {
-    let semaphore = 0;
-    proc.stdout.on("readable", () => {
-        if (semaphore >= checker.length) {
-            proc.kill();
-            done();
-            return;
-        }
+  let semaphore = 0;
+  proc.stdout.on("readable", () => {
+    if (semaphore >= checker.length) {
+      proc.kill();
+      done();
+      return;
+    }
 
-        // Construct chunk
-        let data = "";
-        let chunk;
-        while ((chunk = proc.stdout.read())) {
-            data += chunk.toString();
-        }
+    // Construct chunk
+    let data = "";
+    let chunk;
+    while ((chunk = proc.stdout.read())) {
+      data += chunk.toString();
+    }
 
-        if (data && checker[semaphore].check(data)) {
-            try {
-                checker[semaphore].perform(data);
-                semaphore++;
-            } catch (err) {
-                proc.kill();
-                done(err);
-            }
-
-            if (semaphore >= checker.length) {
-                proc.kill();
-                done();
-            }
-        }
-    });
-
-    proc.on("error", (error) => {
+    if (data && checker[semaphore].check(data)) {
+      try {
+        checker[semaphore].perform(data);
+        semaphore++;
+      } catch (err) {
         proc.kill();
-        done(error);
-    });
+        done(err);
+      }
 
-    proc.on("exit", () => {
+      if (semaphore >= checker.length) {
         proc.kill();
         done();
-    });
+      }
+    }
+  });
+
+  proc.on("error", (error) => {
+    proc.kill();
+    done(error);
+  });
+
+  proc.on("exit", () => {
+    proc.kill();
+    done();
+  });
 };
 
 describe("--interactive flag with single compiler", () => {
-    // webpack v4 should not be supported https://github.com/webpack/webpack-cli/pull/1796#pullrequestreview-605767369
-    if (!isWebpack5) {
-        it("should throw error upon --interactive", async () => {
-            const { stdout, stderr, exitCode } = await run(__dirname, ["--interactive"]);
-            expect(stdout).toMatchSnapshot();
-            expect(stderr).toMatchSnapshot();
-            expect(exitCode).toBe(1);
-        });
-        return;
-    }
-
-    it("should output in interactive with --interactive", (done) => {
-        const proc = runAndGetProcess(__dirname, ["--interactive"]);
-        const checker = [
-            {
-                check: (data) => {
-                    return data.includes("\u25B2");
-                },
-                perform: () => {
-                    writeFileSync(
-                        resolve(__dirname, "./src/index.js"),
-                        `console.log('I am Batman');`,
-                    );
-                },
-            },
-            {
-                check: (data) => {
-                    return data.includes(clear);
-                },
-                perform: (data) => {
-                    expect(data).toContain(clear);
-                },
-            },
-            {
-                check: (data) => {
-                    return data.includes(wordsInStatsv5[0]);
-                },
-                perform: (data) => {
-                    for (const word of wordsInStatsv5) {
-                        expect(data).toContain(word);
-                    }
-                },
-            },
-        ];
-
-        runTest(proc, checker, done);
+  // webpack v4 should not be supported https://github.com/webpack/webpack-cli/pull/1796#pullrequestreview-605767369
+  if (!isWebpack5) {
+    it("should throw error upon --interactive", async () => {
+      const { stdout, stderr, exitCode } = await run(__dirname, ["--interactive"]);
+      expect(stdout).toMatchSnapshot();
+      expect(stderr).toMatchSnapshot();
+      expect(exitCode).toBe(1);
     });
+    return;
+  }
 
-    it("should stop watching on s", (done) => {
-        const proc = runAndGetProcess(__dirname, ["--interactive=verbose"]);
-        const checker = [
-            {
-                check: (data) => {
-                    return data.includes("\u25B2");
-                },
-                perform: () => {
-                    proc.stdin.write("s", (err) => {
-                        if (err) {
-                            proc.kill();
-                            done(err);
-                            return;
-                        }
-                    });
-                },
-            },
-            {
-                check: (data) => {
-                    return data.includes("stoped");
-                },
-                perform: (data) => {
-                    expect(data).toContain("stoped");
-                },
-            },
-        ];
+  it("should output in interactive with --interactive", (done) => {
+    const proc = runAndGetProcess(__dirname, ["--watch --interactive"]);
+    const checker = [
+      {
+        check: (data) => {
+          return data.includes("\u25B2");
+        },
+        perform: () => {
+          writeFileSync(resolve(__dirname, "./src/index.js"), `console.log('I am Batman');`);
+        },
+      },
+      {
+        check: (data) => {
+          return data.includes(clear);
+        },
+        perform: (data) => {
+          expect(data).toContain(clear);
+        },
+      },
+      {
+        check: (data) => {
+          return data.includes(wordsInStatsv5[0]);
+        },
+        perform: (data) => {
+          for (const word of wordsInStatsv5) {
+            expect(data).toContain(word);
+          }
+        },
+      },
+    ];
 
-        runTest(proc, checker, done);
-    });
+    runTest(proc, checker, done);
+  });
 
-    it("should should start watching on w after stoping with s", (done) => {
-        const proc = runAndGetProcess(__dirname, ["--interactive=verbose"]);
-        const checker = [
-            {
-                check: (data) => {
-                    return data.includes("\u25B2");
-                },
-                perform: () => {
-                    proc.stdin.write("q\n", (err) => {
-                        if (err) {
-                            proc.kill();
-                            done(err);
-                            return;
-                        }
-                    });
-                },
-            },
-            {
-                check: (data) => {
-                    return data.includes("stoped");
-                },
-                perform: (data) => {
-                    expect(data).toContain("stoped");
-                    proc.stdin.write("w\n", (err) => {
-                        if (err) {
-                            proc.kill();
-                            done(err);
-                            return;
-                        }
-                    });
-                },
-            },
-            {
-                check: (data) => {
-                    return data.includes("compilation completed");
-                },
-                perform: (data) => {
-                    expect(data).toContain("compilation completed");
-                },
-            },
-        ];
+  it("should stop watching on s", (done) => {
+    const proc = runAndGetProcess(__dirname, ["--watch --interactive=verbose"]);
+    const checker = [
+      {
+        check: (data) => {
+          return data.includes("\u25B2");
+        },
+        perform: () => {
+          proc.stdin.write("s", (err) => {
+            if (err) {
+              proc.kill();
+              done(err);
+              return;
+            }
+          });
+        },
+      },
+      {
+        check: (data) => {
+          return data.includes("stoped");
+        },
+        perform: (data) => {
+          expect(data).toContain("stoped");
+        },
+      },
+    ];
 
-        runTest(proc, checker, done);
-    });
+    runTest(proc, checker, done);
+  });
 
-    it("should quit in pressing q", (done) => {
-        const proc = runAndGetProcess(__dirname, ["--interactive=verbose"]);
-        const checker = [
-            {
-                check: (data) => {
-                    return data.includes("\u25B2");
-                },
-                perform: () => {
-                    proc.stdin.write("q\n", (err) => {
-                        if (err) {
-                            proc.kill();
-                            done(err);
-                            return;
-                        }
-                    });
-                },
-            },
-        ];
+  it("should should start watching on w after stoping with s", (done) => {
+    const proc = runAndGetProcess(__dirname, ["--watch --interactive=verbose"]);
+    const checker = [
+      {
+        check: (data) => {
+          return data.includes("\u25B2");
+        },
+        perform: () => {
+          proc.stdin.write("q\n", (err) => {
+            if (err) {
+              proc.kill();
+              done(err);
+              return;
+            }
+          });
+        },
+      },
+      {
+        check: (data) => {
+          return data.includes("stoped");
+        },
+        perform: (data) => {
+          expect(data).toContain("stoped");
+          proc.stdin.write("w\n", (err) => {
+            if (err) {
+              proc.kill();
+              done(err);
+              return;
+            }
+          });
+        },
+      },
+      {
+        check: (data) => {
+          return data.includes("compilation completed");
+        },
+        perform: (data) => {
+          expect(data).toContain("compilation completed");
+        },
+      },
+    ];
 
-        const doneCallback = (error) => {
-            expect(error).toBeFalsy();
-            done();
-        };
+    runTest(proc, checker, done);
+  });
 
-        runTest(proc, checker, doneCallback);
-    });
+  it("should quit in pressing q", (done) => {
+    const proc = runAndGetProcess(__dirname, ["--watch --interactive=verbose"]);
+    const checker = [
+      {
+        check: (data) => {
+          return data.includes("\u25B2");
+        },
+        perform: () => {
+          proc.stdin.write("q\n", (err) => {
+            if (err) {
+              proc.kill();
+              done(err);
+              return;
+            }
+          });
+        },
+      },
+    ];
+
+    const doneCallback = (error) => {
+      expect(error).toBeFalsy();
+      done();
+    };
+
+    runTest(proc, checker, doneCallback);
+  });
 });
