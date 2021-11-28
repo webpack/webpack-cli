268f1870adf8acd1122217d76a538e0152fab956
Rishabh Chawla <rishabh31121999@gmail.com>
2021-11-28 13:42:45 +0530

    chore: lint multi compiler test

--- test/interactive/multi-compiler/webpack.config.js
+++ test/interactive/multi-compiler/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 const compilers = ["shin-chan", "misae", "hiroshi", "himawari", "shiro"].map((name) => {
-    return {
-        name: name,
-        mode: "development",
-        output: {
-            path: path.join(__dirname, "dist"),
-            filename: `main-${name}.js`,
-        },
-    };
+  return {
+    name: name,
+    mode: "development",
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: `main-${name}.js`,
+    },
+  };
 });
 
 module.exports = compilers;
