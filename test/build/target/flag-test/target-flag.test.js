094e0e6538b4afbf64fcc966fab5d62fd2937eff
Nitin Kumar <snitin315@gmail.com>
2021-12-23 14:32:34 +0530

    ci: debug

--- test/build/target/flag-test/target-flag.test.js
+++ test/build/target/flag-test/target-flag.test.js
@@ -17,6 +17,8 @@ describe("--target flag", () => {
     it(`should accept ${val} with --target flag`, async () => {
       const { exitCode, stderr, stdout } = await run(__dirname, ["--target", `${val}`]);
 
+      console.log(stderr);
+      console.log(stdout);
       expect(exitCode).toBe(0);
       expect(stderr).toBeFalsy();
 
