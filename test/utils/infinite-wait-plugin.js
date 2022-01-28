429a46d4436513571a134664d39b631bb0b6fe97
Rishabh Chawla <rishabh31121999@gmail.com>
2022-01-28 12:35:32 +0530

    chore: update timeout

--- test/utils/infinite-wait-plugin.js
+++ test/utils/infinite-wait-plugin.js
@@ -1,6 +1,6 @@
 class InfiniteWaitPlugin {
   constructor(options) {
-    this.time = options.time || 1000;
+    this.time = options.time || 10000;
   }
   apply(compiler) {
     compiler.hooks.done.tapPromise("Infinite Wait Plugin", async () => {
