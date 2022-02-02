0991d9743115cb2abc50283c48632b7f0a2a9824
Rishabh Chawla <rishabh31121999@gmail.com>
2022-02-02 10:24:37 +0530

    chore: update plugin

--- test/utils/infinite-wait-plugin.js
+++ test/utils/infinite-wait-plugin.js
@@ -1,9 +1,10 @@
 class InfiniteWaitPlugin {
-  constructor(options) {
+  constructor(options = {}) {
     this.time = options.time || 10000;
   }
   apply(compiler) {
     compiler.hooks.done.tapPromise("Infinite Wait Plugin", async () => {
+      await new Promise((resolve) => process.nextTick(resolve));
       await new Promise((resolve) => {
         setTimeout(() => {
           resolve();
