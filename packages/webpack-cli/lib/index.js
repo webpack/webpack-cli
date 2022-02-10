ca19b62e3e20db0f3ef92316ae7d17ac699d51e3
taranek <t.taranek27@gmail.com>
2022-02-10 20:38:39 +0100

    fix(cli): removed old js file from package/webpack-cli

--- packages/webpack-cli/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CLI = require("./webpack-cli");
-module.exports = CLI;
-// TODO remove after drop `@webpack-cli/migrate`
-module.exports.utils = { logger: console };
