1ffc5229f02be0c2a98b1bcdb2ffc38683211c4f
taranek <t.taranek27@gmail.com>
2022-02-15 21:31:22 +0100

    feat(types): use cli types in other packages

--- packages/webpack-cli/src/index.ts
+++ packages/webpack-cli/src/index.ts
@@ -1,8 +1,8 @@
-import {IWebpackCLI} from "./types";
+import { IWebpackCLI } from "./types";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const CLI: IWebpackCLI = require("./webpack-cli");
 
 module.exports = CLI;
 // TODO remove after drop `@webpack-cli/migrate`
 module.exports.utils = { logger: console };
-
