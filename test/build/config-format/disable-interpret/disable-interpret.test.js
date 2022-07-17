46eb622c8e3e62a8852a5f52a1537ded147bb608
shuta13 <hollystarsun@gmail.com>
2022-07-18 07:37:55 +0900

    refactor: standalone test

---
+++ test/build/config-format/disable-interpret/disable-interpret.test.js
@@ -0,0 +1,24 @@
+const { run } = require("../../../utils/test-utils");
+const { existsSync, unlinkSync } = require("fs");
+const { resolve } = require("path");
+
+// eslint-disable-next-line node/no-unpublished-require
+const execa = require("execa");
+const { sync: spawnSync } = execa;
+
+describe("webpack cli", () => {
+  it('should work with the "disable-interpret" option from flags', async () => {
+    const configFileName = "webpack.config";
+    const configFilePath = resolve(__dirname, `${configFileName}.ts`);
+    const buildScripts = spawnSync("yarn", ["tsc", configFilePath]);
+    expect(buildScripts.stdout).toBeTruthy();
+
+    const { exitCode, stderr, stdout } = await run(__dirname, ["--disable-interpret"]);
+    unlinkSync(resolve(__dirname, `${configFileName}.js`));
+
+    expect(stderr).toBeFalsy();
+    expect(stdout).toBeTruthy();
+    expect(exitCode).toBe(0);
+    expect(existsSync(resolve(__dirname, "dist/foo.bundle.js"))).toBeTruthy();
+  });
+});
