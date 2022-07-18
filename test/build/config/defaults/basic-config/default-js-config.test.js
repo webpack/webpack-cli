ca0b69e296bd60923c1d53e7ef8352692f15c4fa
Nitin Kumar <snitin315@gmail.com>
2022-07-18 22:53:29 +0530

    refactor: drop webpack v4 from tests, packages, and CI (#3338)

--- test/build/config/defaults/basic-config/default-js-config.test.js
+++ test/build/config/defaults/basic-config/default-js-config.test.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { run, isWebpack5 } = require("../../../../utils/test-utils");
+const { run } = require("../../../../utils/test-utils");
 
 describe("default config", () => {
   it("should build and not throw error when config is present but not supplied via flag", async () => {
@@ -8,19 +8,11 @@ describe("default config", () => {
 
     expect(exitCode).toEqual(0);
     expect(stderr).toBeFalsy();
-
     // default entry should be used
     expect(stdout).toContain("./src/index.js");
     // should pick up the output path from config
     expect(stdout).toContain("test-output");
-
-    if (!isWebpack5) {
-      expect(stdout).toContain("Hash");
-      expect(stdout).toContain("Version");
-      expect(stdout).toContain("Built at");
-      expect(stdout).toContain("Time");
-    }
-
+    expect(stdout).toContain("compiled successfully");
     // check that the output file exists
     expect(fs.existsSync(path.join(__dirname, "/dist/test-output.js"))).toBeTruthy();
   });
