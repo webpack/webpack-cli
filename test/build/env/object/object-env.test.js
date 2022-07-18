edc787de1f3868ed621ca884619f0d37676b1207
Nitin Kumar <snitin315@gmail.com>
2022-07-18 22:53:29 +0530

    refactor: drop webpack v4 from tests, packages, and CI (#3338)

--- test/build/env/object/object-env.test.js
+++ test/build/env/object/object-env.test.js
@@ -5,7 +5,7 @@ const path = require("path");
 const execa = require("execa");
 const { sync: spawnSync } = execa;
 
-const { run, isWebpack5 } = require("../../../utils/test-utils");
+const { run } = require("../../../utils/test-utils");
 
 describe("env object", () => {
   it("is able to set env for an object", async () => {
@@ -15,10 +15,9 @@ describe("env object", () => {
     expect(stderr).toBeFalsy();
     expect(stdout).toBeTruthy();
 
-    if (isWebpack5) {
-      const executable = path.join(__dirname, "./dist/main.js");
-      const bundledScript = spawnSync("node", [executable]);
-      expect(bundledScript.stdout).toBe("environment is development");
-    }
+    const executable = path.join(__dirname, "./dist/main.js");
+    const bundledScript = spawnSync("node", [executable]);
+
+    expect(bundledScript.stdout).toBe("environment is development");
   });
 });
