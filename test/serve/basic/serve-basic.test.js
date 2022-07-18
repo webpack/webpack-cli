04744ace26ee5d01a1aa64a60da6ef9d89d075a8
Nitin Kumar <snitin315@gmail.com>
2022-07-18 17:10:02 +0530

    refactor: drop webpack v4 from tests, packages, and CI

--- test/serve/basic/serve-basic.test.js
+++ test/serve/basic/serve-basic.test.js
@@ -235,7 +235,7 @@ describe("basic serve usage", () => {
       expect(stdout).not.toContain("HotModuleReplacementPlugin");
     }
 
-    expect(stdout).toContain("from webpack.Compiler");
+    expect(stderr).toContain("webpack.Progress");
     expect(stdout).toContain("main.js");
   });
 
@@ -363,71 +363,6 @@ describe("basic serve usage", () => {
       expect(stdout).not.toContain("HotModuleReplacementPlugin");
       expect(stdout).toContain("main.js");
     }
-  });
-
-  devServer4Test('should work with the "--hot" option using the "only" value', async () => {
-    const { stdout, stderr } = await runWatch(testPath, ["serve", "--port", port, "--hot=only"]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(stdout).toContain("HotModuleReplacementPlugin");
-    expect(stdout).toContain("main.js");
-  });
-
-  devServer4Test('should work with "--hot" and "--port" options', async () => {
-    const { stdout, stderr } = await runWatch(testPath, ["serve", "--port", port, "--hot"]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(stdout).toContain("HotModuleReplacementPlugin");
-    expect(stdout).toContain("main.js");
-  });
-
-  devServer4Test('should work with the "--hot" and "--progress" options', async () => {
-    const { stdout, stderr } = await runWatch(testPath, [
-      "serve",
-      "--port",
-      port,
-      "--hot",
-      "--progress",
-    ]);
-
-    expect(stderr).toContain("webpack.Progress");
-    expect(stdout).toContain("HotModuleReplacementPlugin");
-    expect(stdout).toContain("main.js");
-  });
-
-  it('should work with the default "publicPath" option', async () => {
-    const { stderr, stdout } = await runWatch(__dirname, ["serve"]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-
-    if (isDevServer4) {
-      expect(stdout).toContain("HotModuleReplacementPlugin");
-    } else {
-      expect(stdout).not.toContain("HotModuleReplacementPlugin");
-    }
-
-    expect(stdout).toContain("main.js");
-  });
-
-  it('should work with the "--output-public-path" option', async () => {
-    const { stderr, stdout } = await runWatch(__dirname, [
-      "serve",
-      "--output-public-path",
-      "/my-public-path/",
-      "--stats",
-      "verbose",
-    ]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(stdout).toContain("/my-public-path/");
-
-    if (isDevServer4) {
-      expect(stdout).toContain("HotModuleReplacementPlugin");
-    } else {
-      expect(stdout).not.toContain("HotModuleReplacementPlugin");
-    }
-
-    expect(stdout).toContain("main.js");
   });
 
   it('should respect the "publicPath" option from configuration', async () => {
@@ -583,19 +518,6 @@ describe("basic serve usage", () => {
     expect(stdout).toContain("development");
   });
 
-  it("should log used supplied config with serve", async () => {
-    const { stderr, stdout } = await runWatch(
-      __dirname,
-      ["serve", "--config", "log.config.js", "--port", port],
-      {
-        killString: /Compiler is watching files for updates\.\.\./,
-      },
-    );
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(stdout).toBeTruthy();
-  });
-
   it("should log error on using '--watch' flag with serve", async () => {
     const { exitCode, stdout, stderr } = await runWatch(testPath, ["serve", "--watch"]);
 
@@ -633,68 +555,6 @@ describe("basic serve usage", () => {
     expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
     expect(stdout).toContain("compiled successfully");
     expect(stdout.match(/HotModuleReplacementPlugin/g)).toBeNull();
-  });
-
-  it('should respect the "publicPath" option from configuration using multi compiler mode (from the "devServer" options)', async () => {
-    const { stderr, stdout } = await runWatch(__dirname, [
-      "serve",
-      "--config",
-      "multi-dev-server-output-public-path.config.js",
-      "--port",
-      port,
-    ]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-
-    if (isDevServer4) {
-      expect(stdout).toContain("HotModuleReplacementPlugin");
-    } else {
-      expect(stdout).not.toContain("HotModuleReplacementPlugin");
-    }
-
-    expect(stdout).toContain("one");
-    expect(stdout).toContain("first-output/main.js");
-    expect(stdout).toContain("two");
-    expect(stdout).toContain("second-output/main.js");
-  });
-
-  it("should work with entries syntax", async () => {
-    const { stderr, stdout } = await runWatch(__dirname, [
-      "serve",
-      "./src/entry.js",
-      "--port",
-      port,
-    ]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-
-    if (isDevServer4) {
-      expect(stdout).toContain("HotModuleReplacementPlugin");
-    } else {
-      expect(stdout).not.toContain("HotModuleReplacementPlugin");
-    }
-
-    expect(stdout).toContain("development");
-  });
-
-  it("should work and log warning on the `watch option in a configuration", async () => {
-    const { stderr, stdout } = await runWatch(__dirname, [
-      "serve",
-      "--config",
-      "./watch.config.js",
-      "--port",
-      port,
-    ]);
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-
-    if (isDevServer4) {
-      expect(stdout).toContain("HotModuleReplacementPlugin");
-    } else {
-      expect(stdout).not.toContain("HotModuleReplacementPlugin");
-    }
-
-    expect(stdout).toContain("development");
   });
 
   it("should log used supplied config with serve", async () => {
@@ -724,45 +584,6 @@ describe("basic serve usage", () => {
 
     expect(normalizedStderr).toMatchSnapshot("stderr");
     expect(stdout).toBeTruthy();
-  });
-
-  it("should log error on using '--watch' flag with serve", async () => {
-    const { exitCode, stdout, stderr } = await runWatch(testPath, ["serve", "--watch"]);
-
-    expect(exitCode).toBe(2);
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(normalizeStdout(stdout)).toMatchSnapshot("stdout");
-  });
-
-  it("should log error on using '-w' alias with serve", async () => {
-    const { exitCode, stdout, stderr } = await runWatch(testPath, ["serve", "-w"]);
-
-    expect(exitCode).toBe(2);
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(normalizeStdout(stdout)).toMatchSnapshot("stdout");
-  });
-
-  it("should log an error on unknown flag", async () => {
-    const { exitCode, stdout, stderr } = await runWatch(testPath, [
-      "serve",
-      "--port",
-      port,
-      "--unknown-flag",
-    ]);
-
-    expect(exitCode).toBe(2);
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(normalizeStdout(stdout)).toMatchSnapshot("stdout");
-  });
-
-  it('should work with the "stats" option in config', async () => {
-    const { stderr, stdout } = await runWatch(__dirname, ["serve", "--config", "stats.config.js"], {
-      killString: /Compiled successfully|modules/i,
-    });
-
-    expect(normalizeStderr(stderr)).toMatchSnapshot("stderr");
-    expect(stdout).toContain("compiled successfully");
-    expect(stdout.match(/HotModuleReplacementPlugin/g)).toBeNull();
   });
 
   it("should throw error when same ports in multicompiler", async () => {
