dbdb5431347c3c305fe7678802b1ad3db8d7f5fc
Nitin Kumar <snitin315@gmail.com>
2022-07-27 14:55:55 +0530

    refactor!: the minimum supported webpack version is v5.0.0 (#3342)
    BREAKING CHANGE: webpack-cli no longer supports webpack v4, the minimum supported version is webpack v5.0.0

--- test/build/target/flag-test/webpack.config.js
+++ test/build/target/flag-test/webpack.config.js
@@ -3,6 +3,5 @@ const WebpackCLITestPlugin = require("../../../utils/webpack-cli-test-plugin");
 module.exports = {
   entry: "./index.js",
   mode: "development",
-  target: "node",
   plugins: [new WebpackCLITestPlugin()],
 };
