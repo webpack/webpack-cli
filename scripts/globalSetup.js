6bb8a79a7684bfa697fbef8aae0b9453b44718fd
Nitin Kumar <snitin315@gmail.com>
2022-09-24 06:56:17 +0530

    ci: debug

--- scripts/globalSetup.js
+++ scripts/globalSetup.js
@@ -1,3 +1,4 @@
 const { version } = require("webpack");
 
-module.exports = () => console.log(`\n Running tests for webpack @${version} \n`);
+module.exports = () =>
+  console.log(`\n Running tests for webpack @${version} \n NODE_PATH-> ${process.env.NODE_PATH}`);
