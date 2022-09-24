ffae46da58336a520ba269e61e64dea2c4a7cf4d
Nitin Kumar <snitin315@gmail.com>
2022-09-24 06:45:34 +0530

    ci: debug

--- scripts/setupTest.js
+++ scripts/setupTest.js
@@ -1,5 +1,7 @@
 jest.setTimeout(240000);
 
+// Debug
+console.log("NODE_PATH->", process.env.NODE_PATH);
 if (!expect.getState().testPath.includes("colors.test.js")) {
   process.env.NO_COLOR = true;
 }
