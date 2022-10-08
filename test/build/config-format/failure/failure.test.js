0f7da89f8924738bb972d1d16f438346ea88d17c
Nitin Kumar <snitin315@gmail.com>
2022-10-08 18:11:31 +0530

    feature!: update `interpret` to v3 and `rechoir` to v0.8 (#3437)

--- test/build/config-format/failure/failure.test.js
+++ test/build/config-format/failure/failure.test.js
@@ -7,11 +7,9 @@ describe("failure", () => {
     const { exitCode, stderr, stdout } = await run(__dirname, ["-c", "webpack.config.iced"]);
 
     expect(exitCode).toBe(2);
-    expect(stderr).toContain(`Unable load '${path.resolve(__dirname, "./webpack.config.iced")}'`);
-    expect(stderr).toContain('Unable to use specified module loaders for ".iced".');
-    expect(stderr).toContain("Cannot find module 'iced-coffee-script/register'");
-    expect(stderr).toContain("Cannot find module 'iced-coffee-script'");
-    expect(stderr).toContain("Please install one of them");
+    expect(stderr).toContain(
+      `Failed to load '${path.resolve(__dirname, "./webpack.config.iced")}'`,
+    );
     expect(stdout).toBeFalsy();
   });
 });
