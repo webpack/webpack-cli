0550fc633ae4854102400b9fb99e3f344e800250
Nitin Kumar <snitin315@gmail.com>
2022-10-19 07:23:02 +0530

    fix: ci

--- packages/webpack-cli/src/webpack-cli.ts
+++ packages/webpack-cli/src/webpack-cli.ts
@@ -2171,8 +2171,8 @@ class WebpackCLI implements IWebpackCLI {
     options: Partial<WebpackDevServerOptions>,
     callback?: Callback<[Error | undefined, WebpackCLIStats | undefined]>,
   ): Promise<WebpackCompiler> {
-    if (typeof options.nodeEnv === "string") {
-      process.env.NODE_ENV = options.nodeEnv;
+    if (typeof options.defineProcessEnvNodeEnv === "string") {
+      process.env.NODE_ENV = options.defineProcessEnvNodeEnv;
     }
 
     let config = await this.loadConfig(options);
