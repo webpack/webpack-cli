9adfd5f8bdec79c3c6f7f525db72a398be6c2af6
alexander.akait <sheo13666q@gmail.com>
2023-03-19 23:53:56 +0200

    test: added

---
+++ test/build/config/named-export/mjs.test.js
@@ -0,0 +1,18 @@
+const { run } = require("../../../utils/test-utils");
+
+describe("webpack cli", () => {
+  it("should support mjs config format", async () => {
+    const { exitCode, stderr, stdout } = await run(__dirname, ["-c", "webpack.config.mjs"], {
+      env: { WEBPACK_CLI_FORCE_LOAD_ESM_CONFIG: true },
+    });
+
+    if (/Error: Not supported/.test(stderr)) {
+      expect(exitCode).toBe(2);
+      expect(stdout).toBeFalsy();
+    } else {
+      expect(exitCode).toBe(2);
+      expect(stderr).toMatch(/Unable to find default export./);
+      expect(stdout).toBeTruthy();
+    }
+  });
+});
