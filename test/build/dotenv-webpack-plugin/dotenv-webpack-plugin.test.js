4e039ba330cf961a2bc624fbb487396dcf2d53ec
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-05-04 09:39:47 +0530

    test: add test for value override

--- test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
+++ test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
@@ -26,4 +26,27 @@ describe("dotenv-webpack-plugin", () => {
     expect(data).toContain("value1");
     expect(data).toContain("value2");
   });
+
+  // write a test to check if cli overrides variables in .env file when .env.production file is present
+  it("reads .env.production file and overrides values from .env variables correctly", async () => {
+    const testDir = __dirname + "/overrides-config";
+    const { exitCode, stderr, stdout } = await run(testDir, ["--entry", "./src/index.js"]);
+
+    expect(exitCode).toBe(0);
+    expect(stderr).toBeFalsy();
+    expect(stdout).toBeTruthy();
+    expect(existsSync(resolve(testDir, "./dist/main.js"))).toBeTruthy();
+
+    let data;
+
+    try {
+      data = await readFile(resolve(testDir, "./dist/main.js"), "utf-8");
+    } catch (error) {
+      expect(error).toBe(null);
+    }
+
+    expect(data).toContain("Hello from index.js");
+    expect(data).toContain('"process.env.WEBPACK_VARIABLE:",production_value');
+    expect(data).toContain('"import.meta.env.WEBPACK_VARIABLE:",production_value');
+  });
 });
