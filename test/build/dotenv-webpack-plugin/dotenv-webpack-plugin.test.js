379094528af453417deedf0cb9deaa3e45a8b45b
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-05-04 09:45:17 +0530

    test: add test for the case when variable does not start with webpack_

--- test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
+++ test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
@@ -27,7 +27,6 @@ describe("dotenv-webpack-plugin", () => {
     expect(data).toContain("value2");
   });
 
-  // write a test to check if cli overrides variables in .env file when .env.production file is present
   it("reads .env.production file and overrides values from .env variables correctly", async () => {
     const testDir = __dirname + "/overrides-config";
     const { exitCode, stderr, stdout } = await run(testDir, ["--entry", "./src/index.js"]);
@@ -48,5 +47,27 @@ describe("dotenv-webpack-plugin", () => {
     expect(data).toContain("Hello from index.js");
     expect(data).toContain('"process.env.WEBPACK_VARIABLE:",production_value');
     expect(data).toContain('"import.meta.env.WEBPACK_VARIABLE:",production_value');
+  });
+
+  it("reads .env file and does not define a variable when it does not start with WEBPACK_", async () => {
+    const testDir = __dirname + "/non-webpack-variable";
+    const { exitCode, stderr, stdout } = await run(testDir, ["--entry", "./src/index.js"]);
+
+    expect(exitCode).toBe(0);
+    expect(stderr).toBeFalsy();
+    expect(stdout).toBeTruthy();
+    expect(existsSync(resolve(testDir, "./dist/main.js"))).toBeTruthy();
+
+    let data;
+
+    try {
+      data = await readFile(resolve(testDir, "./dist/main.js"), "utf-8");
+    } catch (error) {
+      expect(error).toBe(null);
+    }
+
+    expect(data).toContain("Hello from index.js");
+    expect(data).toContain('"process.env.NON_WEBPACK_VARIABLE:",process.env.NON_WEBPACK_VARIABLE');
+    expect(data).toContain('"import.meta.env.NON_WEBPACK_VARIABLE:",(void 0).NON_WEBPACK_VARIABLE');
   });
 });
