f8a6142cb4f22053c4e9cde903894fd3c2b04559
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-05-09 21:18:50 +0530

    test: add more test cases

--- test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
+++ test/build/dotenv-webpack-plugin/dotenv-webpack-plugin.test.js
@@ -21,7 +21,7 @@ const getBuildOutput = async (testDir, buildPath = "dist") => {
 
 describe("dotenv-webpack-plugin", () => {
   it("reads .env file and defines variables correctly", async () => {
-    const testDir = join(__dirname, "/builtin-config");
+    const testDir = join(__dirname, "builtin-config");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -38,7 +38,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env.production file and overrides values from .env variables correctly", async () => {
-    const testDir = join(__dirname, "/overrides-config");
+    const testDir = join(__dirname, "overrides-config");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -55,7 +55,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env file and does not define a variable when it does not start with WEBPACK_", async () => {
-    const testDir = join(__dirname, "/non-webpack-variable");
+    const testDir = join(__dirname, "non-webpack-variable");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -73,7 +73,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env.production when mode is set to production", async () => {
-    const testDir = join(__dirname, "/mode-production");
+    const testDir = join(__dirname, "mode-production");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -94,7 +94,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env.development when mode is set to development", async () => {
-    const testDir = join(__dirname, "/mode-development");
+    const testDir = join(__dirname, "mode-development");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -116,7 +116,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env.none when mode is set to none", async () => {
-    const testDir = join(__dirname, "/mode-none");
+    const testDir = join(__dirname, "mode-none");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -138,7 +138,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env.example when file is present", async () => {
-    const testDir = join(__dirname, "/env-example");
+    const testDir = join(__dirname, "env-example");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -155,7 +155,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("overrides value from .env when same key in .env.local is present", async () => {
-    const testDir = join(__dirname, "/overrides-local");
+    const testDir = join(__dirname, "overrides-local");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -173,7 +173,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("overrides value from .env.[mode] when same key in .env.[mode].local is present", async () => {
-    const testDir = join(__dirname, "/overrides-local");
+    const testDir = join(__dirname, "overrides-local");
     const { exitCode, stderr, stdout } = await run(testDir, [
       "--entry",
       "./src/index.js",
@@ -191,7 +191,7 @@ describe("dotenv-webpack-plugin", () => {
   });
 
   it("reads .env file and applies for all configs", async () => {
-    const testDir = join(__dirname, "/multiple-configs");
+    const testDir = join(__dirname, "multiple-configs");
     const { exitCode, stderr, stdout } = await run(testDir, ["--read-dot-env"]);
 
     assertNoErrors(exitCode, stderr, stdout, testDir, "dist1");
@@ -207,5 +207,44 @@ describe("dotenv-webpack-plugin", () => {
     expect(data).toContain("Hello from index.js");
     expect(data).toContain('"process.env.WEBPACK_VARIABLE:",default_value');
     expect(data).toContain('"import.meta.env.WEBPACK_VARIABLE:",default_value');
+  });
+
+  it("reads env vars from a custom file path correctly", async () => {
+    const testDir = join(__dirname, "custom-config-path");
+    const { exitCode, stderr, stdout } = await run(testDir);
+
+    assertNoErrors(exitCode, stderr, stdout, testDir);
+
+    const data = await getBuildOutput(testDir);
+
+    expect(data).toContain("Hello from index.js");
+    expect(data).toContain('"process.env.WEBPACK_VARIABLE:",default_value');
+    expect(data).toContain('"import.meta.env.WEBPACK_VARIABLE:",default_value');
+  });
+
+  it("creates variables with custom prefixes when passed", async () => {
+    const testDir = join(__dirname, "custom-prefix");
+    const { exitCode, stderr, stdout } = await run(testDir);
+
+    assertNoErrors(exitCode, stderr, stdout, testDir);
+
+    const data = await getBuildOutput(testDir);
+
+    expect(data).toContain("Hello from index.js");
+    expect(data).toContain('"custom_prefix_WEBPACK_VARIABLE:",default_value');
+  });
+
+  it("validates env files paths", async () => {
+    const testDir = join(__dirname, "validate-config-paths");
+    const { stderr } = await run(testDir);
+
+    expect(stderr).toContain("envFiles option must be an array, received string");
+  });
+
+  it("validates custom prefixes", async () => {
+    const testDir = join(__dirname, "validates-prefixes");
+    const { stderr } = await run(testDir);
+
+    expect(stderr).toContain("prefixes option must be an array, received string");
   });
 });
---
+++ test/build/dotenv-webpack-plugin/validate-config-paths/src/index.js
@@ -0,0 +1,2 @@
+console.log("Hello from index.js");
+console.log("custom_prefix_WEBPACK_VARIABLE:", custom_prefix_WEBPACK_VARIABLE);
