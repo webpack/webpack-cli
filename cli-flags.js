f4f8d0ea19e2604c21a5c78e14483dc8eeae0fd7
Arnav <info@arnavgupta.net>
2024-04-07 15:44:27 +0530

    fix: improved flags

--- cli-flags.js
+++ cli-flags.js
@@ -1,54 +1,159 @@
-const cliFlags = [
-  {
-    name: "mode",
-    usage: "--mode <mode>",
-    description: "The mode to run the analyzer in: server, static, or disabled.",
-    type: String,
-    defaultValue: "server",
+"use strict";
+
+module.exports = {
+  version: {
+    configs: [
+      {
+        type: "boolean",
+        multiple: false,
+        description: "Output the version number",
+        path: "version",
+      },
+    ],
+    description: "Output the version number",
+    simpleType: "boolean",
+    multiple: false,
   },
-  {
-    name: "report",
-    usage: "--report <path>",
+  mode: {
+    configs: [
+      {
+        type: "enum",
+        values: ["server", "static", "json"],
+        multiple: false,
+        description: "The mode to run the analyzer in: server, static, or json.",
+        path: "mode",
+        defaultValue: "server",
+      },
+    ],
+    description: "The mode to run the analyzer in: server, static, or json.",
+    simpleType: "string",
+    multiple: false,
+  },
+  report: {
+    configs: [
+      {
+        type: "string",
+        multiple: false,
+        description: 'Path to bundle report file that will be generated in "static" mode.',
+        path: "report",
+        defaultValue: "report.html",
+      },
+    ],
     description: 'Path to bundle report file that will be generated in "static" mode.',
-    type: String,
+    simpleType: "string",
+    multiple: false,
   },
-  {
-    name: "open",
-    usage: "--open",
-    description: "Automatically open report in default browser.",
-    type: Boolean,
-    defaultValue: false,
+  title: {
+    configs: [
+      {
+        type: "string",
+        multiple: false,
+        description: "String to use in title element of html report.",
+        path: "title",
+        defaultValue: "Webpack Bundle Report",
+      },
+    ],
+    description: "String to use in title element of html report.",
+    simpleType: "string",
+    multiple: false,
   },
-  {
-    name: "port",
-    usage: "--port <number>",
+  size: {
+    configs: [
+      {
+        type: "enum",
+        values: ["stat", "parsed", "gzip"],
+        multiple: false,
+        description: "Module sizes to show in treemap by default.",
+        path: "defaultSizes",
+        defaultValue: "parsed",
+      },
+    ],
+    description: "Module sizes to show in treemap by default.",
+    simpleType: "string",
+    multiple: false,
+  },
+  "Dont Open": {
+    configs: [
+      {
+        type: "boolean",
+        multiple: false,
+        description: "Don't open report in default browser automatically",
+        path: "noOpen",
+        defaultValue: false,
+      },
+    ],
+    description: "Don't open report in default browser automatically",
+    simpleType: "boolean",
+    multiple: false,
+  },
+  port: {
+    configs: [
+      {
+        type: "number",
+        multiple: false,
+        description: "Port that will be used in `server` mode, default is 8888.",
+        path: "port",
+        defaultValue: 8888,
+      },
+    ],
     description: "Port that will be used in `server` mode, default is 8888.",
-    type: Number,
-    defaultValue: 8888,
+    simpleType: "number",
+    multiple: false,
   },
-  {
-    name: "host",
-    usage: "--host <address>",
+  host: {
+    configs: [
+      {
+        type: "string",
+        multiple: false,
+        description: "Host that will be used in `server` mode, default is 127.0.0.1.",
+        path: "host",
+        defaultValue: "127.0.0.1",
+      },
+    ],
     description: "Host that will be used in `server` mode, default is 127.0.0.1.",
-    type: String,
-    defaultValue: "127.0.0.1",
+    simpleType: "string",
+    multiple: false,
   },
-  {
-    name: "log-level",
-    usage: "--log-level <level>",
+  "log-level": {
+    configs: [
+      {
+        type: "enum",
+        values: ["debug", "info", "warn", "error", "silent"],
+        multiple: true,
+        description: "Level of logger (info, warn, error, silent).",
+        path: "logLevel",
+        defaultValue: "info",
+      },
+    ],
     description: "Level of logger (info, warn, error, silent).",
-    type: String,
-    values: ["debug", "info", "warn", "error", "silent"],
-    defaultValue: "info",
+    simpleType: "string",
     multiple: true,
   },
-  {
-    name: "exclude",
-    usage: "--exclude <regexp> ",
+  exclude: {
+    configs: [
+      {
+        type: "string",
+        multiple: false,
+        description: "Assets that should be excluded from the report.",
+        path: "exclude",
+        defaultValue: "",
+      },
+    ],
     description: "Assets that should be excluded from the report.",
-    type: String,
-    defaultValue: "info",
+    simpleType: "string",
+    multiple: false,
   },
-];
-
-module.exports = { cliFlags };
+  help: {
+    configs: [
+      {
+        type: "boolean",
+        multiple: false,
+        description: "output usage information",
+        path: "help",
+      },
+    ],
+    description: "Output usage information",
+    simpleType: "boolean",
+    multiple: false,
+  },
+};
