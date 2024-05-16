667d50dec1647ce4969e47321e3bf7c2d0c849d9
Uzair <uzairkhanuk3215@gmail.com>
2024-05-16 21:11:43 +0530

    feat: add entrypoint executable which wraps the generator cli

---
+++ packages/create-webpack-app/bin/cli.js
@@ -0,0 +1,19 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import minimist from "minimist";
+import { Plop, run } from "plop";
+/* cSpell:disable */
+const args = process.argv.slice(2);
+const argv = minimist(args);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+Plop.prepare(
+  {
+    cwd: argv.cwd,
+    configPath: path.resolve(__dirname, "../lib/plopfile.js"),
+    preload: argv.preload || [],
+    completion: argv.completion,
+  },
+  (env) => Plop.execute(env, run),
+);
