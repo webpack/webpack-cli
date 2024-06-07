9bf21bf9958d36ec0f5d7b45995d056bc2cd2790
Uzair <uzairkhanuk3215@gmail.com>
2024-06-08 03:30:40 +0530

    feat: create commander cli and implement skip feat
    created commander cli
    implement init command with
    -s -f flags to
    skip and -f to override

--- packages/create-webpack-app/bin/cli.js
+++ packages/create-webpack-app/bin/cli.js
@@ -1,15 +1,46 @@
 import { Command } from "commander";
+import { resolve, join } from "path";
+import nodePlop from "node-plop";
+// Cspell:ignore plopfile, plopfile.js
+
 const program = new Command();
+const plop = await nodePlop("./lib/plopfile.js");
+const defaultValues = {
+  init: {
+    projectName: "webpack-project",
+    projectPath: resolve(join(process.cwd(), ".")),
+    langType: "none",
+    devServer: true,
+    htmlWebpackPlugin: true,
+    workboxWebpackPlugin: true,
+    cssType: "CSS only",
+    isCSS: true,
+    isPostCSS: true,
+    extractPlugin: "No",
+    packageManager: "npm",
+  },
+};
 
-import pkg from "../package.json" with { type: "json" };
-
-program.version(pkg.version, "-v, --version");
+program.version("1.0.0", "-v, --version");
 program.helpOption("-h, --help", "Display help for command");
 
 program
-  .option("-s, --skip", "Skip the prompt and use the default values")
-  .option("-f, --force", "Force the generator actions to override existing files");
+  .command("init")
+  .description("Initialize a new Webpack project")
+  .option("-s, --skip", "Skip the prompt and use the default values", false)
+  .option("-f, --force", "Force the generator actions to override existing files", false)
+  .action(function (opts) {
+    console.log("Initializing a new Webpack project");
+    const { skip, force } = opts;
+    const initGenerator = plop.getGenerator("init");
+    if (skip) {
+      console.log("Skipping the prompt and using the default values");
+      initGenerator.runActions(defaultValues.init);
+    } else {
+      initGenerator.runPrompts([]).then((answers) => {
+        initGenerator.runActions(answers);
+      });
+    }
+  });
 
-program.parse(process.argv);
-
-console.log(program.opts());
+program.parse();
