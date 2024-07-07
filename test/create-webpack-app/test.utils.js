ea92e56708b98755fb12fd670f1f3eccb929bfd1
Uzair <uzairkhanuk3215@gmail.com>
2024-07-07 14:04:00 +0530

    refactor: remove redundant code from test.utils and extend from utils/test-utils.js
    - earlier this file was an exact copy with a line changed
    - now it imports all the utility function and only extends run**()
    functions with path value pointing to create-webpack-cli

--- test/create-webpack-app/test.utils.js
+++ test/create-webpack-app/test.utils.js
@@ -1,41 +1,17 @@
 /* eslint-disable node/no-unpublished-require */
-
 "use strict";
 
-const os = require("os");
-const stripAnsi = require("strip-ansi");
+const testUtilsPkg = require("../utils/test-utils.js");
+const { processKill } = testUtilsPkg;
 const path = require("path");
-const fs = require("fs");
 const execa = require("execa");
-const internalIp = require("internal-ip");
-const { exec } = require("child_process");
 const { node: execaNode } = execa;
-const { Writable } = require("readable-stream");
+const stripAnsi = require("strip-ansi");
 const concat = require("concat-stream");
-const { cli } = require("webpack");
+const { Writable } = require("readable-stream");
 
 const CREATE_WEBPACK_PATH = path.resolve(__dirname, "../../packages/create-webpack-app/bin/cli.js");
 const ENABLE_LOG_COMPILATION = process.env.ENABLE_PIPE || false;
-const isWindows = process.platform === "win32";
-
-const hyphenToUpperCase = (name) => {
-  if (!name) {
-    return name;
-  }
-
-  return name.replace(/-([a-z])/g, function (g) {
-    return g[1].toUpperCase();
-  });
-};
-
-const processKill = (process) => {
-  if (isWindows) {
-    exec("taskkill /pid " + process.pid + " /T /F");
-  } else {
-    process.kill();
-  }
-};
-
 /**
  * Webpack CLI test runner.
  *
@@ -149,8 +125,7 @@ const runWatch = (cwd, args = [], options = {}) => {
       });
   });
 };
-
-/**
+/*
  * runPromptWithAnswers
  * @param {string} location location of current working directory
  * @param {string[]} args CLI args to pass in
@@ -238,189 +213,11 @@ const runPromptWithAnswers = (location, args, answers) => {
   });
 };
 
-const normalizeVersions = (output) => {
-  return output.replace(
-    /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/gi,
-    "x.x.x",
-  );
-};
-
-const normalizeCwd = (output) => {
-  return output
-    .replace(/\\/g, "/")
-    .replace(new RegExp(process.cwd().replace(/\\/g, "/"), "g"), "<cwd>");
-};
-
-const normalizeError = (output) => {
-  return output
-    .replace(/SyntaxError: .+/, "SyntaxError: <error-message>")
-    .replace(/\s+at .+(}|\)|\d)/gs, "\n    at stack");
-};
-
-const normalizeStdout = (stdout) => {
-  if (typeof stdout !== "string") {
-    return stdout;
-  }
-
-  if (stdout.length === 0) {
-    return stdout;
-  }
-
-  let normalizedStdout = stripAnsi(stdout);
-  normalizedStdout = normalizeCwd(normalizedStdout);
-  normalizedStdout = normalizeVersions(normalizedStdout);
-  normalizedStdout = normalizeError(normalizedStdout);
-
-  return normalizedStdout;
-};
-
-const normalizeStderr = (stderr) => {
-  if (typeof stderr !== "string") {
-    return stderr;
-  }
-
-  if (stderr.length === 0) {
-    return stderr;
-  }
-
-  let normalizedStderr = stripAnsi(stderr);
-  normalizedStderr = normalizeCwd(normalizedStderr);
-
-  const networkIPv4 = internalIp.v4.sync();
-
-  if (networkIPv4) {
-    normalizedStderr = normalizedStderr.replace(new RegExp(networkIPv4, "g"), "<network-ip-v4>");
-  }
-
-  const networkIPv6 = internalIp.v6.sync();
-
-  if (networkIPv6) {
-    normalizedStderr = normalizedStderr.replace(new RegExp(networkIPv6, "g"), "<network-ip-v6>");
-  }
-
-  normalizedStderr = normalizedStderr.replace(/:[0-9]+\//g, ":<port>/");
-
-  if (!/On Your Network \(IPv6\)/.test(stderr)) {
-    // Github Actions doesn't' support IPv6 on ubuntu in some cases
-    normalizedStderr = normalizedStderr.split("\n");
-
-    const ipv4MessageIndex = normalizedStderr.findIndex((item) =>
-      /On Your Network \(IPv4\)/.test(item),
-    );
-
-    if (ipv4MessageIndex !== -1) {
-      normalizedStderr.splice(
-        ipv4MessageIndex + 1,
-        0,
-        "<i> [webpack-dev-server] On Your Network (IPv6): http://[<network-ip-v6>]:<port>/",
-      );
-    }
-
-    normalizedStderr = normalizedStderr.join("\n");
-  }
-
-  // TODO remove me after drop old Node.js versions and update deps
-  // Suppress warnings for Node.js version >= v22
-  // [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
-  if (process.version.startsWith("v22")) {
-    normalizedStderr = normalizedStderr
-      .split("\n")
-      .filter((line) => {
-        return (
-          !line.includes("DeprecationWarning: The `punycode` module is deprecated.") &&
-          !line.includes("Use `node --trace-deprecation ...`")
-        );
-      })
-      .join("\n");
-  }
-
-  // the warning below is causing CI failure on some jobs
-  if (/Gracefully shutting down/.test(stderr)) {
-    normalizedStderr = normalizedStderr.replace(
-      "\n<i> [webpack-dev-server] Gracefully shutting down. To force exit, press ^C again. Please wait...",
-      "",
-    );
-  }
-
-  normalizedStderr = normalizeVersions(normalizedStderr);
-  normalizedStderr = normalizeError(normalizedStderr);
-
-  return normalizedStderr;
-};
-
-const getWebpackCliArguments = (startWith) => {
-  if (typeof startWith === "undefined") {
-    return cli.getArguments();
-  }
-
-  const result = {};
-
-  for (const [name, value] of Object.entries(cli.getArguments())) {
-    if (name.startsWith(startWith)) {
-      result[name] = value;
-    }
-  }
-
-  return result;
-};
-
-const readFile = (path, options = {}) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(path, options, (err, stats) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(stats);
-    });
-  });
-
-const readdir = (path) =>
-  new Promise((resolve, reject) => {
-    fs.readdir(path, (err, stats) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(stats);
-    });
-  });
-
-// cSpell:ignore Symbhas, ABCDEFGHNR, Vfgcti
-const urlAlphabet = "ModuleSymbhasOwnPr-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW";
-
-const uuid = (size = 21) => {
-  let id = "";
-  let i = size;
-
-  while (i--) {
-    // `| 0` is more compact and faster than `Math.floor()`.
-    id += urlAlphabet[(Math.random() * 64) | 0];
-  }
-
-  return id;
-};
-
-const uniqueDirectoryForTest = async () => {
-  const result = path.resolve(os.tmpdir(), uuid());
-
-  if (!fs.existsSync(result)) {
-    fs.mkdirSync(result);
-  }
-
-  return result;
-};
-
 module.exports = {
+  ...testUtilsPkg,
+  createProcess,
   run,
   runAndGetProcess,
   runWatch,
   runPromptWithAnswers,
-  isWindows,
-  normalizeStderr,
-  normalizeStdout,
-  uniqueDirectoryForTest,
-  readFile,
-  readdir,
-  hyphenToUpperCase,
-  processKill,
-  getWebpackCliArguments,
 };
