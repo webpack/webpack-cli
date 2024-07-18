8bc0d5f798242c286bfeedfa3dda0e8d86a93d9c
Uzair <uzairkhanuk3215@gmail.com>
2024-07-18 17:34:09 +0530

    feat: add loader generator, default-template

--- packages/create-webpack-app/src/plopfile.ts
+++ packages/create-webpack-app/src/plopfile.ts
@@ -3,4 +3,5 @@ import { NodePlopAPI } from "./types";
 export default async function (plop: NodePlopAPI) {
   await plop.load("./generators/init/default.js", {}, true);
   await plop.load("./generators/init/react.js", {}, true);
+  await plop.load("./generators/loader/default.js", {}, true);
 }
