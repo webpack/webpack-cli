1f891f137ffb91c06e6c2e46afe06d972eaa325c
alexander.akait <sheo13666q@gmail.com>
2024-09-03 21:40:35 +0300

    test: debug

--- test/serve/serve-variable/serve-variable.test.js
+++ test/serve/serve-variable/serve-variable.test.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const getPort = require("get-port");
 const { runWatch, normalizeStderr } = require("../../utils/test-utils");
+const internalIp = require("internal-ip");
 
 const testPath = path.resolve(__dirname);
 
@@ -18,6 +19,9 @@ describe("serve variable", () => {
       stdoutKillStr: /webpack \d+\.\d+\.\d/,
       stderrKillStr: /Content not from webpack is served from/,
     });
+
+    console.log(internalIp.v4.sync());
+    console.log(stderr);
 
     expect(normalizeStderr(stderr)).toMatchSnapshot();
     expect(stdout).toContain("main.js");
