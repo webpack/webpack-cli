19311f46131a1bab971989902709f2f1c0c1f7ab
Sakibul Islam <sakibulislam25800@gmail.com>
2025-02-10 14:55:45 +0000

    feat(webpack-cli): add --env-file flag support
    Add support for import.meta.env and introduce --env-file flag to load
    environment variables from .env files into the bundled files.
    
    - Support for import.meta.env in webpack builds
    - New --env-file CLI flag for specifying whether to load .env files for configuration
    - Renamed dotenv plugin for clarity

--- packages/webpack-cli/src/bootstrap.ts
+++ packages/webpack-cli/src/bootstrap.ts
@@ -1,16 +1,8 @@
 import { type IWebpackCLI } from "./types";
-import { EnvLoader } from "./utils/env-loader";
 
 const WebpackCLI = require("./webpack-cli");
 
 const runCLI = async (args: Parameters<IWebpackCLI["run"]>[0]) => {
-  // Load environment variables from .env files
-  try {
-    EnvLoader.loadEnvFiles();
-  } catch (error) {
-    console.warn("Warning: Error loading environment variables:", error);
-  }
-
   // Create a new instance of the CLI object
   const cli: IWebpackCLI = new WebpackCLI();
 
