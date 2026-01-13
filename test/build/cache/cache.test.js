419383ed81790093867ae2c61de9596617234583
alexander-akait <sheo13666q@gmail.com>
2026-01-13 16:10:41 +0300

    fix: use graceful shutdown when file system cache is enabled

--- test/build/cache/cache.test.js
+++ test/build/cache/cache.test.js
@@ -2,7 +2,7 @@
 
 const fs = require("node:fs");
 const path = require("node:path");
-const { run } = require("../../utils/test-utils");
+const { processKill, run, runWatch } = require("../../utils/test-utils");
 
 describe("cache", () => {
   it("should work", async () => {
@@ -217,5 +217,47 @@ describe("cache", () => {
     expect(stderr.match(/restore cache content \d+ \(.+\):/g)).toHaveLength(1);
     expect(stderr).toBeTruthy();
     expect(stdout).toBeTruthy();
+  });
+
+  it("should graceful shutdown", async () => {
+    fs.rmSync(
+      path.join(
+        __dirname,
+        "../../../node_modules/.cache/webpack/cache-graceful-shutdown-development",
+      ),
+      {
+        recursive: true,
+        force: true,
+      },
+    );
+
+    let stdout = "";
+    let stderr = "";
+
+    await runWatch(__dirname, ["--config", "./graceful-exit.webpack.config.js", "--watch"], {
+      handler: (proc) => {
+        proc.stdout.on("data", (chunk) => {
+          const data = chunk.toString();
+
+          stdout += data;
+
+          if (data.includes("app.bundle.js")) {
+            processKill(proc);
+          }
+        });
+
+        proc.stderr.on("data", (chunk) => {
+          const data = chunk.toString();
+
+          stderr += data;
+        });
+      },
+    });
+
+    expect(stderr.match(/No pack exists at/g)).toHaveLength(1);
+    expect(stderr.match(/Stored pack/g)).toHaveLength(1);
+    expect(stdout).toContain(
+      "Gracefully shutting down. To force exit, press ^C again. Please wait...",
+    );
   });
 });
