b57e88d45d47a14400ee73020fef4b99daa106df
alexander-akait <sheo13666q@gmail.com>
2026-02-24 16:47:13 +0300

    refactor: fix

--- packages/webpack-cli/src/webpack-cli.ts
+++ packages/webpack-cli/src/webpack-cli.ts
@@ -562,9 +562,9 @@ class WebpackCLI implements IWebpackCLI {
 
     defaultInformation.npmPackages = `{${defaultPackages.map((item) => `*${item}*`).join(",")}}`;
 
-    const envinfo = await import("envinfo");
+    const { run } = await import("envinfo");
 
-    let info = await envinfo.run(defaultInformation, envinfoConfig);
+    let info = await run(defaultInformation, envinfoConfig);
 
     info = info.replace("npmPackages", "Packages");
     info = info.replace("npmGlobalPackages", "Global Packages");
