8390cc935706a77785066118be38c82d69362425
alexander-akait <sheo13666q@gmail.com>
2026-03-10 20:45:45 +0300

    test: fix

--- test/api/capitalizeFirstLetter.test.js
+++ test/api/capitalizeFirstLetter.test.js
@@ -7,9 +7,9 @@ describe("capitalizeFirstLetter", () => {
     expect(cli.capitalizeFirstLetter("webpack")).toBe("Webpack");
   });
 
-  it("should return an empty string on passing a non-string value", () => {
+  it("should return an empty string on passing a empty string value", () => {
     const cli = new CLI();
 
-    expect(cli.capitalizeFirstLetter(true)).toBe("");
+    expect(cli.capitalizeFirstLetter("")).toBe("");
   });
 });
