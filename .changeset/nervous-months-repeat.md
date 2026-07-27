b0a2024a5790446ea07610ad27fd5abc65f5b875
Sebastian Beltran <bjohansebas@gmail.com>
2026-07-26 22:21:34 -0500

    perf: enable the Node.js compile cache (#4818)
    * perf: enable the Node.js compile cache
    
    * fixup!

---
+++ .changeset/nervous-months-repeat.md
@@ -0,0 +1,5 @@
+---
+"webpack-cli": patch
+---
+
+perf: enable the Node.js compile cache (`module.enableCompileCache`, available on Node.js >= 22.8.0) to speed up CLI startup
--- packages/webpack-cli/bin/cli.js
+++ packages/webpack-cli/bin/cli.js
@@ -2,6 +2,18 @@
 
 "use strict";
 
+// `module.enableCompileCache` is only available on Node.js >= 22.8.0
+// eslint-disable-next-line n/no-unsupported-features/node-builtins
+const { enableCompileCache } = require("node:module");
+
+if (enableCompileCache) {
+  try {
+    enableCompileCache();
+  } catch {
+    // Ignore errors
+  }
+}
+
 // Prefer the local installation of `webpack-cli` when one exists. Run this
 // before requiring the (heavier) CLI implementation: a delegated run then never
 // loads it, and `WEBPACK_CLI_SKIP_IMPORT_LOCAL` skips loading `import-local` too.
